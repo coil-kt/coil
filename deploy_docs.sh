83d160ad3a16f8ec4464c22d2d7ba5193af9380f
Colin White <colin@colinwhite.me>
2019-08-11 00:12:21 -0700

    Copy files when deploying docs.

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -6,5 +6,14 @@ rm -rf docs/api
 # Build the Dokka docs.
 ./gradlew clean :coil-base:dokka :coil-default:dokka :coil-gif:dokka
 
+# Copy outside files into the docs folder.
+sed -e '/full documentation here/ { N; d; }' < README.md > docs/index.md
+cp CONTRIBUTING.md docs/contributing.md
+cp CHANGELOG.md docs/changelog.md
+cp logo.svg docs/logo.svg
+
 # Deploy to Github pages.
 mkdocs gh-deploy
+
+# Clean up.
+rm docs/index.md docs/contributing.md docs/changelog.md docs/logo.svg
