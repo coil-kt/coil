32d56dd4b79dbd70428d235784b61b2ad097a480
Zac Sweers <zac.sweers@gmail.com>
2019-08-13 00:54:38 -0400

    Simplify sample

--- coil-sample/src/main/java/coil/sample/Application.kt
+++ coil-sample/src/main/java/coil/sample/Application.kt
@@ -9,24 +9,9 @@ import coil.Coil
 import coil.ImageLoader
 import coil.ImageLoaderBuilder.Companion.applyCoilOptimizations
 import coil.util.CoilLogger
-import okhttp3.Call
 import okhttp3.OkHttpClient
 
 class Application : MultiDexApplication() {
-
-    /**
-     * Clever technique with Call.Factory. By lazily constructing our OkHttpClient instance,
-     * we can defer its instantiation until the first network request and off the main thread.
-     */
-    private val okHttpClient by lazy {
-        OkHttpClient.Builder()
-            .applyCoilOptimizations(this@Application)
-            .apply {
-                // The Unsplash API requires TLS 1.2, which isn't enabled by default before Lollipop.
-                if (SDK_INT < LOLLIPOP) forceTls12()
-            }
-            .build()
-    }
 
     override fun onCreate() {
         super.onCreate()
@@ -39,7 +24,15 @@ class Application : MultiDexApplication() {
             availableMemoryPercentage(0.5)
             bitmapPoolPercentage(0.5)
             crossfade(true)
-            callFactory(Call.Factory { request -> okHttpClient.newCall(request) })
+            callFactory(
+                OkHttpClient.Builder()
+                    .applyCoilOptimizations(this@Application)
+                    .apply {
+                        // The Unsplash API requires TLS 1.2, which isn't enabled by default before Lollipop.
+                        if (SDK_INT < LOLLIPOP) forceTls12()
+                    }
+                    .build()
+            )
         }
     }
 }
