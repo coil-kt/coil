4bdecf800d9e7e390d6e0ec5e59b46b4ae0dcb72
Zac Sweers <zac.sweers@gmail.com>
2019-08-13 00:54:18 -0400

    Update sample demonstrating deferred init

--- coil-sample/src/main/java/coil/sample/Application.kt
+++ coil-sample/src/main/java/coil/sample/Application.kt
@@ -7,9 +7,26 @@ import android.os.Build.VERSION_CODES.LOLLIPOP
 import androidx.multidex.MultiDexApplication
 import coil.Coil
 import coil.ImageLoader
+import coil.ImageLoaderBuilder.Companion.applyCoilOptimizations
 import coil.util.CoilLogger
+import okhttp3.Call
+import okhttp3.OkHttpClient
 
 class Application : MultiDexApplication() {
+
+    /**
+     * Clever technique with Call.Factory. By lazily constructing our OkHttpClient instance,
+     * we can defer its instantiation until the first network request and off the main thread.
+     */
+    private val okHttpClient by lazy {
+        OkHttpClient.Builder()
+            .applyCoilOptimizations(this@Application)
+            .apply {
+                // The Unsplash API requires TLS 1.2, which isn't enabled by default before Lollipop.
+                if (SDK_INT < LOLLIPOP) forceTls12()
+            }
+            .build()
+    }
 
     override fun onCreate() {
         super.onCreate()
@@ -22,10 +39,7 @@ class Application : MultiDexApplication() {
             availableMemoryPercentage(0.5)
             bitmapPoolPercentage(0.5)
             crossfade(true)
-            okHttpClient {
-                // The Unsplash API requires TLS 1.2, which isn't enabled by default before Lollipop.
-                if (SDK_INT < LOLLIPOP) forceTls12()
-            }
+            callFactory(Call.Factory { request -> okHttpClient.newCall(request) })
         }
     }
 }
