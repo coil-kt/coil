661f97be9ff5f1d439f54ec28726fb0b079e166a
Colin White <colin@colinwhite.me>
2019-08-14 23:24:31 -0700

    Add test for file Uri fetching. (#30)

--- coil-base/src/androidTest/java/coil/fetch/UriFetcherTest.kt
+++ coil-base/src/androidTest/java/coil/fetch/UriFetcherTest.kt
@@ -4,14 +4,19 @@ package coil.fetch
 
 import android.content.Context
 import android.net.Uri
+import androidx.core.net.toUri
 import androidx.test.core.app.ApplicationProvider
 import coil.bitmappool.BitmapPool
 import coil.bitmappool.FakeBitmapPool
 import coil.size.PixelSize
 import coil.util.createOptions
 import kotlinx.coroutines.runBlocking
+import okio.buffer
+import okio.sink
+import okio.source
 import org.junit.Before
 import org.junit.Test
+import java.io.File
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
 import kotlin.test.assertTrue
@@ -30,7 +35,27 @@ class UriFetcherTest {
     }
 
     @Test
-    fun basicAssetLoad() {
+    fun basicFileFetch() {
+        // Copy the asset to filesDir.
+        val source = context.assets.open("normal.jpg").source().buffer()
+        val file = File(context.filesDir.absolutePath + File.separator + "normal.jpg")
+        val sink = file.sink().buffer()
+        sink.writeAll(source)
+
+        val uri = file.toUri()
+        assertTrue(loader.handles(uri))
+        assertEquals(uri.toString(), loader.key(uri))
+
+        val result = runBlocking {
+            loader.fetch(pool, uri, PixelSize(100, 100), createOptions())
+        }
+
+        assertTrue(result is SourceResult)
+        assertFalse(result.source.exhausted())
+    }
+
+    @Test
+    fun basicAssetFetch() {
         val uri = Uri.parse("file:///android_asset/normal.jpg")
         assertTrue(loader.handles(uri))
         assertEquals(uri.toString(), loader.key(uri))
