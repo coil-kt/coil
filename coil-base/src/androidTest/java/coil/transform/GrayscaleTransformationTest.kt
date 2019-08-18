176bfc8e38f93c19429324160779e53a3c19fce2
Jeziel <jeziellago@gm>
2019-08-18 02:36:52 -0300

    Change gray scale transformation test and add new asset

--- coil-base/src/androidTest/java/coil/transform/GrayscaleTransformationTest.kt
+++ coil-base/src/androidTest/java/coil/transform/GrayscaleTransformationTest.kt
@@ -10,18 +10,24 @@ import coil.decode.BitmapFactoryDecoder
 import coil.size.PixelSize
 import coil.util.createOptions
 import coil.util.getPixels
+import coil.util.sameAs
 import kotlinx.coroutines.runBlocking
 import okio.buffer
 import okio.source
 import org.junit.Before
 import org.junit.Test
-import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 class GrayscaleTransformationTest {
 
+    companion object {
+        const val THRESHOLD = 0.99
+    }
+
     private val context: Context = ApplicationProvider.getApplicationContext()
 
+    private lateinit var normalBitmap: Bitmap
+    private lateinit var normalGrayscaleBitmap: Bitmap
     private lateinit var pool: BitmapPool
     private lateinit var decoder: BitmapFactoryDecoder
     private lateinit var grayscaleTransformation: GrayscaleTransformation
@@ -31,25 +37,27 @@ class GrayscaleTransformationTest {
         pool = FakeBitmapPool()
         decoder = BitmapFactoryDecoder(context)
         grayscaleTransformation = GrayscaleTransformation()
+        normalBitmap = getBitmapFromAssets("normal.jpg")
+        normalGrayscaleBitmap = getBitmapFromAssets("normal_grayscale.jpg")
     }
 
     @Test
     fun withRGBBitmap_assertThatTransformToNewGrayscaleBitmap() {
 
-        val coloredBitmap = getColoredBitmap()
-
         val grayscaleBitmap = runBlocking {
-            grayscaleTransformation.transform(pool, coloredBitmap)
+            grayscaleTransformation.transform(pool, normalBitmap)
         }
 
         val (_, red, green, blue) = grayscaleBitmap.getPixels()
 
         assertTrue(red.contentEquals(green) && green.contentEquals(blue))
-        assertFalse(grayscaleBitmap.sameAs(coloredBitmap))
+        assertTrue(grayscaleBitmap.sameAs(normalGrayscaleBitmap, THRESHOLD))
     }
 
-    private fun getColoredBitmap(): Bitmap {
-        val source = context.assets.open("normal.jpg").source().buffer()
+    private fun getBitmapFromAssets(
+        fileName: String
+    ): Bitmap {
+        val source = context.assets.open(fileName).source().buffer()
         val (drawable, _) = runBlocking {
             decoder.decode(
                 pool = pool,
