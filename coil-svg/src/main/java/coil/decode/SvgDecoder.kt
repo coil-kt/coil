2b08ef23b99150629e886a9a3436f8717f2008e2
Colin White <colin@colinwhite.me>
2019-08-20 13:49:22 -0700

    Formatting changes.

--- coil-svg/src/main/java/coil/decode/SvgDecoder.kt
+++ coil-svg/src/main/java/coil/decode/SvgDecoder.kt
@@ -1,4 +1,4 @@
-@file:Suppress("DEPRECATION", "unused")
+@file:Suppress("unused")
 
 package coil.decode
 
@@ -13,9 +13,18 @@ import okio.BufferedSource
  */
 class SvgDecoder : Decoder {
 
-    override fun handles(source: BufferedSource, mimeType: String?) = mimeType == "image/svg+xml"
+    companion object {
+        private const val SVG_MIME_TYPE = "image/svg+xml"
+    }
 
-    override suspend fun decode(pool: BitmapPool, source: BufferedSource, size: Size, options: Options): DecodeResult {
+    override fun handles(source: BufferedSource, mimeType: String?) = mimeType == SVG_MIME_TYPE
+
+    override suspend fun decode(
+        pool: BitmapPool,
+        source: BufferedSource,
+        size: Size,
+        options: Options
+    ): DecodeResult {
         val svg = source.use { SVG.getFromInputStream(it.inputStream()) }
         return DecodeResult(
             drawable = PictureDrawable(svg.renderToPicture()),
