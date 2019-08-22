c77a29b8f00301c3db086cd4c3a680c072e85658
John Carlson <jawnnypoo@gmail.com>
2019-08-22 17:28:19 -0500

    Add readme files for coil-gif and coil-svg. (#62)
    * Add README blurb to the two decoders
    
    * Add SVGs docs to doc site
    
    * PR feedback
    
    * rm

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -10,10 +10,12 @@ rm -rf docs/api
 sed -e '/full documentation here/ { N; d; }' < README.md > docs/index.md
 cp CONTRIBUTING.md docs/contributing.md
 cp CHANGELOG.md docs/changelog.md
+cp coil-gif/README.md docs/gifs.md
+cp coil-svg/README.md docs/svgs.md
 cp logo.svg docs/logo.svg
 
 # Deploy to Github pages.
 mkdocs gh-deploy
 
 # Clean up.
-rm docs/index.md docs/contributing.md docs/changelog.md docs/logo.svg
+rm docs/index.md docs/contributing.md docs/changelog.md docs/logo.svg docs/gifs.md docs/svgs.md
