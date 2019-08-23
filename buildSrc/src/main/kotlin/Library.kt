f1d9be183282e0f2598d5a7adb782f120caa17d3
Colin White <colin@colinwhite.me>
2019-08-23 11:46:25 -0700

    Update Coroutines to 1.3.0.

--- buildSrc/src/main/kotlin/Library.kt
+++ buildSrc/src/main/kotlin/Library.kt
@@ -6,7 +6,7 @@ object Library {
 
     // CORE
 
-    private const val COROUTINES_VERSION = "1.2.2"
+    private const val COROUTINES_VERSION = "1.3.0"
     const val KOTLINX_COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$COROUTINES_VERSION"
 
     const val ANDROIDX_ANNOTATION = "androidx.annotation:annotation:1.1.0"
