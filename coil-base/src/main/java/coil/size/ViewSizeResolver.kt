5196e26dab0754777d3d28fbb5470c0773eb800c
Colin White <colin@colinwhite.me>
2019-09-08 00:49:38 -0700

    Treat WRAP_CONTENT as the largest display dimension.

--- coil-base/src/main/java/coil/size/ViewSizeResolver.kt
+++ coil-base/src/main/java/coil/size/ViewSizeResolver.kt
@@ -1,7 +1,10 @@
 package coil.size
 
+import android.util.Log
 import android.view.View
+import android.view.ViewGroup
 import android.view.ViewTreeObserver
+import coil.util.log
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 import kotlin.math.max
@@ -12,6 +15,8 @@ import kotlin.math.max
 interface ViewSizeResolver<T : View> : SizeResolver {
 
     companion object {
+        private const val TAG = "ViewSizeResolver"
+
         /**
          * Construct a [ViewSizeResolver] instance using the default [View] measurement implementation.
          */
@@ -25,13 +30,12 @@ interface ViewSizeResolver<T : View> : SizeResolver {
     val view: T
 
     override suspend fun size(): Size {
-        // Fast path: assume the View's height will match the data in its layout params.
-        view.layoutParams?.let { layoutParams ->
-            val width = layoutParams.width - view.paddingLeft - view.paddingRight
-            val height = layoutParams.height - view.paddingTop - view.paddingBottom
-            if (width > 0 && height > 0) {
-                return PixelSize(width, height)
-            }
+        // Fast path: we don't need to wait for the view to be measured.
+        val isLayoutRequested = view.isLayoutRequested
+        val width = getWidth(isLayoutRequested)
+        val height = getHeight(isLayoutRequested)
+        if (width > 0 && height > 0) {
+            return PixelSize(width, height)
         }
 
         // Wait for the view to be measured.
@@ -46,9 +50,11 @@ interface ViewSizeResolver<T : View> : SizeResolver {
                         isResumed = true
 
                         viewTreeObserver.removePreDrawListenerSafe(this)
-                        val width = max(1, view.width - view.paddingLeft - view.paddingRight)
-                        val height = max(1, view.height - view.paddingTop - view.paddingBottom)
-                        continuation.resume(PixelSize(width, height))
+                        val size = PixelSize(
+                            width = getWidth(false).coerceAtLeast(1),
+                            height = getHeight(false).coerceAtLeast(1)
+                        )
+                        continuation.resume(size)
                     }
                     return true
                 }
@@ -62,10 +68,58 @@ interface ViewSizeResolver<T : View> : SizeResolver {
         }
     }
 
+    private fun getWidth(isLayoutRequested: Boolean): Int {
+        return getDimension(
+            paramSize = view.layoutParams?.width ?: -1,
+            viewSize = view.width,
+            paddingSize = view.paddingLeft + view.paddingRight,
+            isLayoutRequested = isLayoutRequested
+        )
+    }
+
+    private fun getHeight(isLayoutRequested: Boolean): Int {
+        return getDimension(
+            paramSize = view.layoutParams?.height ?: -1,
+            viewSize = view.height,
+            paddingSize = view.paddingTop + view.paddingBottom,
+            isLayoutRequested = isLayoutRequested
+        )
+    }
+
+    /** Modified from Glide. */
+    private fun getDimension(
+        paramSize: Int,
+        viewSize: Int,
+        paddingSize: Int,
+        isLayoutRequested: Boolean
+    ): Int {
+        // Assume the dimension will match the value in the View's layout params.
+        val insetParamSize = paramSize - paddingSize
+        if (insetParamSize > 0) {
+            return insetParamSize
+        }
+
+        // Fallback to the View's current size.
+        val insetViewSize = viewSize - paddingSize
+        if (insetViewSize > 0) {
+            return insetViewSize
+        }
+
+        // If the dimension is set to WRAP_CONTENT and is fully laid out, fallback to the largest dimension of the display.
+        if (!isLayoutRequested && paramSize == ViewGroup.LayoutParams.WRAP_CONTENT) {
+            log(TAG, Log.INFO) { "A View's width and/or height is set to WRAP_CONTENT. Falling back to the size of the display." }
+            return view.context.resources.displayMetrics.run { max(widthPixels, heightPixels) }
+        }
+
+        // Unable to resolve the dimension's size.
+        return -1
+    }
+
     private fun ViewTreeObserver.removePreDrawListenerSafe(victim: ViewTreeObserver.OnPreDrawListener) {
-        when {
-            isAlive -> removeOnPreDrawListener(victim)
-            else -> view.viewTreeObserver.removeOnPreDrawListener(victim)
+        if (isAlive) {
+            removeOnPreDrawListener(victim)
+        } else {
+            view.viewTreeObserver.removeOnPreDrawListener(victim)
         }
     }
 }
