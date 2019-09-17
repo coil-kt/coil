77e33dfebc418b4604c4ef0496449cede3543156
khatv911 <khatv911@gmail.com>
2019-09-17 11:11:27 +0800

    Support individual corner radii in RoundedCornersTransformation. (#103)
    * RoundedCornersTransformation can support 4 various sizes in clockwise order
    
    * Fix lint issue
    
    * trigger a CI rebuild
    
    * code review changes
    
    * fix wrong corner order
    
    * remove reduntdant brace

--- coil-base/src/main/java/coil/transform/RoundedCornersTransformation.kt
+++ coil-base/src/main/java/coil/transform/RoundedCornersTransformation.kt
@@ -4,24 +4,32 @@ package coil.transform
 
 import android.graphics.Bitmap
 import android.graphics.BitmapShader
-import android.graphics.Color
 import android.graphics.Paint
-import android.graphics.PorterDuff
-import android.graphics.RectF
 import android.graphics.Shader
+import android.graphics.RectF
+import android.graphics.Color
+import android.graphics.PorterDuff
+import android.graphics.Path
 import androidx.core.graphics.applyCanvas
 import coil.bitmappool.BitmapPool
 
 /**
  * A [Transformation] that rounds the corners of an image.
  */
-class RoundedCornersTransformation(private val radius: Float) : Transformation {
+class RoundedCornersTransformation(
+    private val topLeft: Float,
+    private val topRight: Float,
+    private val bottomRight: Float,
+    private val bottomLeft: Float
+) : Transformation {
+
+    constructor(radius: Float) : this(radius, radius, radius, radius)
 
     init {
-        require(radius >= 0) { "Radius must be >= 0." }
+        require(topLeft >= 0 && topRight >= 0 && bottomRight >= 0 && bottomLeft >= 0) { "All radii must be >= 0." }
     }
 
-    override fun key() = "${RoundedCornersTransformation::class.java}-$radius"
+    override fun key() = "${RoundedCornersTransformation::class.java}-$topLeft,$topRight,$bottomLeft,$bottomRight"
 
     override suspend fun transform(pool: BitmapPool, input: Bitmap): Bitmap {
         val paint = Paint(Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG)
@@ -31,8 +39,17 @@ class RoundedCornersTransformation(private val radius: Float) : Transformation {
         val rect = RectF(0f, 0f, output.width.toFloat(), output.height.toFloat())
         output.applyCanvas {
             drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR)
-            drawRoundRect(rect, radius, radius, paint)
+            drawPath(Path().apply {
+                addRoundRect(
+                    rect,
+                    floatArrayOf(
+                        topLeft, topLeft, topRight, topRight, bottomRight, bottomRight, bottomLeft, bottomLeft
+                    ),
+                    Path.Direction.CW
+                )
+            }, paint)
         }
+
         pool.put(input)
         return output
     }
