f9446928f61e3b0f2081ce9d3abff331993feae9
Colin White <colin@colinwhite.me>
2019-09-19 15:00:51 -0700

    Work around ImageDecoder native crash.

--- coil-gif/src/main/java/coil/decode/ImageDecoderDecoder.kt
+++ coil-gif/src/main/java/coil/decode/ImageDecoderDecoder.kt
@@ -2,6 +2,7 @@
 
 package coil.decode
 
+import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.ImageDecoder
 import android.os.Build.VERSION_CODES.P
@@ -11,13 +12,14 @@ import coil.bitmappool.BitmapPool
 import coil.size.PixelSize
 import coil.size.Size
 import okio.BufferedSource
-import java.nio.ByteBuffer
+import okio.buffer
+import okio.sink
 
 /**
  * A [Decoder] that uses [ImageDecoder] to decode GIFs and animated WebPs on Android P and above.
  */
 @RequiresApi(P)
-class ImageDecoderDecoder : Decoder {
+class ImageDecoderDecoder(private val context: Context) : Decoder {
 
     override fun handles(source: BufferedSource, mimeType: String?): Boolean {
         return DecodeUtils.isGif(source) || DecodeUtils.isAnimatedWebP(source)
@@ -29,34 +31,46 @@ class ImageDecoderDecoder : Decoder {
         size: Size,
         options: Options
     ): DecodeResult {
-        var isSampled = false
-        val decoderSource = source.use {
-            ImageDecoder.createSource(ByteBuffer.wrap(it.readByteArray()))
+        val tempFile = createTempFile(directory = context.cacheDir)
+
+        try {
+            var isSampled = false
+
+            // Work around https://issuetracker.google.com/issues/139371066 by copying the source to a temp file.
+            source.use { tempFile.sink().buffer().writeAll(it) }
+            val decoderSource = ImageDecoder.createSource(tempFile)
+
+            val drawable = decoderSource.decodeDrawable { info, _ ->
+                // It's safe to delete the temp file here.
+                tempFile.delete()
+
+                // Set the target size if the source image is larger than the target.
+                if (size is PixelSize && info.size.run { width > size.width || height > size.height }) {
+                    isSampled = true
+                    setTargetSize(size.width, size.height)
+                }
+
+                if (options.config != Bitmap.Config.HARDWARE) {
+                    allocator = ImageDecoder.ALLOCATOR_SOFTWARE
+                }
+
+                if (options.colorSpace != null) {
+                    setTargetColorSpace(options.colorSpace)
+                }
+
+                memorySizePolicy = if (options.allowRgb565) {
+                    ImageDecoder.MEMORY_POLICY_LOW_RAM
+                } else {
+                    ImageDecoder.MEMORY_POLICY_DEFAULT
+                }
+            }
+
+            return DecodeResult(
+                drawable = drawable,
+                isSampled = isSampled
+            )
+        } finally {
+            tempFile.delete()
         }
-        val drawable = decoderSource.decodeDrawable { info, _ ->
-            // Set the target size if the source image is larger than the target.
-            if (size is PixelSize && info.size.run { width > size.width || height > size.height }) {
-                isSampled = true
-                setTargetSize(size.width, size.height)
-            }
-
-            if (options.config != Bitmap.Config.HARDWARE) {
-                allocator = ImageDecoder.ALLOCATOR_SOFTWARE
-            }
-
-            if (options.colorSpace != null) {
-                setTargetColorSpace(options.colorSpace)
-            }
-
-            memorySizePolicy = if (options.allowRgb565) {
-                ImageDecoder.MEMORY_POLICY_LOW_RAM
-            } else {
-                ImageDecoder.MEMORY_POLICY_DEFAULT
-            }
-        }
-        return DecodeResult(
-            drawable = drawable,
-            isSampled = isSampled
-        )
     }
 }
