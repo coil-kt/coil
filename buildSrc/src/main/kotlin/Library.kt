d1891cc3a4d24750e3745f20b9e16ede8a67d54e
Colin White <colin@colinwhite.me>
2019-10-07 23:15:08 -0700

    Support dark mode and rendering behind the nav bar on Android Q in the sample app. (#138)

--- buildSrc/src/main/kotlin/Library.kt
+++ buildSrc/src/main/kotlin/Library.kt
@@ -26,7 +26,7 @@ object Library {
     const val ANDROIDX_LIFECYCLE_LIVE_DATA = "androidx.lifecycle:lifecycle-livedata:$LIFECYCLE_VERSION"
     const val ANDROIDX_LIFECYCLE_VIEW_MODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:$LIFECYCLE_VERSION"
 
-    const val MATERIAL = "com.google.android.material:material:1.0.0"
+    const val MATERIAL = "com.google.android.material:material:1.1.0-beta01"
 
     const val ANDROID_SVG = "com.caverock:androidsvg-aar:1.4"
 
