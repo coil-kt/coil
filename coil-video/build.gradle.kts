9367f36ba2dc8fc8f1c90638e94bb549e6707b18
Colin White <colin@colinwhite.me>
2019-10-12 14:50:03 -0700

    Use regular Dokka plugin.

--- coil-video/build.gradle.kts
+++ coil-video/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
     id("com.android.library")
     id("com.vanniktech.maven.publish")
     id("kotlin-android")
-    id("org.jetbrains.dokka-android")
+    id("org.jetbrains.dokka")
 }
 
 android {
