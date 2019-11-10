b0f161624bfc8a2027f57bfdc9b3e780cca0e430
Colin White <colin@colinwhite.me>
2019-11-09 20:54:07 -0800

    Preserve aspect ratio when converting a Drawable to a Bitmap. (#166)
    * Preserve aspect ratio when converting a Drawable to a Bitmap.
    
    * Fix tests.

--- coil-base/src/test/java/coil/decode/DrawableDecoderServiceTest.kt
+++ coil-base/src/test/java/coil/decode/DrawableDecoderServiceTest.kt
@@ -30,7 +30,10 @@ class DrawableDecoderServiceTest {
 
     @Test
     fun `vector with hardware config is converted correctly`() {
-        val input = VectorDrawable()
+        val input = object : VectorDrawable() {
+            override fun getIntrinsicWidth() = 100
+            override fun getIntrinsicHeight() = 100
+        }
         val output = service.convertIfNecessary(
             drawable = input,
             size = PixelSize(200, 200),
@@ -38,7 +41,41 @@ class DrawableDecoderServiceTest {
         )
 
         assertTrue(output is BitmapDrawable)
+        assertEquals(Bitmap.Config.ARGB_8888, output.bitmap.config)
         assertTrue(output.bitmap.run { width == 200 && height == 200 })
+    }
+
+    @Test
+    fun `unimplemented intrinsic size does not crash`() {
+        val input = object : VectorDrawable() {
+            override fun getIntrinsicWidth() = -1
+            override fun getIntrinsicHeight() = -1
+        }
+        val output = service.convertIfNecessary(
+            drawable = input,
+            size = PixelSize(200, 200),
+            config = Bitmap.Config.HARDWARE
+        )
+
+        assertTrue(output is BitmapDrawable)
+        assertEquals(Bitmap.Config.ARGB_8888, output.bitmap.config)
+        assertTrue(output.bitmap.run { width == 200 && height == 200 })
+    }
+
+    @Test
+    fun `aspect ratio is preserved`() {
+        val input = object : VectorDrawable() {
+            override fun getIntrinsicWidth() = 125
+            override fun getIntrinsicHeight() = 250
+        }
+        val output = service.convert(
+            drawable = input,
+            size = PixelSize(200, 200),
+            config = Bitmap.Config.ARGB_8888
+        )
+
+        assertEquals(Bitmap.Config.ARGB_8888, output.config)
+        assertTrue(output.width == 100 && output.height == 200)
     }
 
     @Test
