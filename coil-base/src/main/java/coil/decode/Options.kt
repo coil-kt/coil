4cd40cb04e387e2e9a9bd58fc5935599f0d8e6ce
Colin White <colin@colinwhite.me>
2019-12-10 20:30:01 -0800

    Revise Options documentation.

--- coil-base/src/main/java/coil/decode/Options.kt
+++ coil-base/src/main/java/coil/decode/Options.kt
@@ -9,20 +9,21 @@ import coil.size.Scale
 import okhttp3.Headers
 
 /**
- * A set of configuration options for loading and decoding an image.
+ * A set of configuration options for fetching and decoding an image.
  *
  * [Fetcher]s and [Decoder]s should respect these options as best as possible.
  *
  * @param config The requested config for any [Bitmap]s.
  * @param colorSpace The preferred color space for any [Bitmap]s.
- * @param scale Determines if the image should be loaded to fit or fill the target's dimensions.
- * @param allowInexactSize True if the image should be loaded to the exact dimensions given by the size + scale.
- *  This will be false if the target supports scaling the image to its required dimensions.
- * @param allowRgb565 True if the [Fetcher] is allowed to use [Bitmap.Config.RGB_565] as an optimization.
- * @param headers The headers for any network operations.
- * @param parameters A map of custom parameters. These are used to pass custom data to [Fetcher]s and [Decoder]s.
- * @param networkCachePolicy Used to determine if this request is allowed to read from the network.
- * @param diskCachePolicy Used to determine if this request is allowed to read/write from/to disk.
+ *  If null, components should typically default to [ColorSpace.Named.SRGB].
+ * @param scale The scaling algorithm for how to fit the source image's dimensions into the target's dimensions.
+ * @param allowInexactSize True if the output image does not need to fit/fill the target's dimensions exactly. For instance,
+ *  if true [BitmapFactoryDecoder] will not decode an image at a larger size than its source dimensions as an optimization.
+ * @param allowRgb565 True if a component is allowed to use [Bitmap.Config.RGB_565] as an optimization.
+ * @param headers The header fields to use for any network requests.
+ * @param parameters A map of custom parameters. These are used to pass custom data to a component.
+ * @param networkCachePolicy Determines if this request is allowed to read from the network.
+ * @param diskCachePolicy Determines if this request is allowed to read/write from/to disk.
  */
 data class Options(
     val config: Bitmap.Config,
