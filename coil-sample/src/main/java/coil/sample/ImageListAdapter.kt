ec9da758da44843aee627fd1bc72c48a37e3a05b
Colin White <colin@colinwhite.me>
2020-02-06 21:12:54 -0800

    Re-arrange.

--- coil-sample/src/main/java/coil/sample/ImageListAdapter.kt
+++ coil-sample/src/main/java/coil/sample/ImageListAdapter.kt
@@ -39,8 +39,8 @@ class ImageListAdapter(
             }
 
             load(item.uri) {
-                placeholder(ColorDrawable(item.color))
                 parameters(item.parameters)
+                placeholder(ColorDrawable(item.color))
             }
 
             setOnClickListener {
