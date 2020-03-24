e375c31d1d06689e98e42364de403886277da77e
Colin White <colin@colinwhite.me>
2020-03-24 12:38:24 -0700

    Docs.

--- coil-base/src/main/java/coil/request/NullRequestDataException.kt
+++ coil-base/src/main/java/coil/request/NullRequestDataException.kt
@@ -5,7 +5,5 @@ import kotlin.coroutines.CoroutineContext
 
 /**
  * An exception thrown by [ImageLoader.launch] (inside the [CoroutineContext]) when [Request.data] is null.
- *
- * @see Request.fallback
  */
 class NullRequestDataException : RuntimeException("The request's data is null.")
