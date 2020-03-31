784f4eb84fbe8252eb8058acaaccd8eee3b40f85
Colin White <colin@colinwhite.me>
2020-03-30 23:05:55 -0700

    Fix incompatibility with Android R SDK. (#337)

--- coil-base/src/main/java/coil/util/ComponentCallbacks.kt
+++ coil-base/src/main/java/coil/util/ComponentCallbacks.kt
@@ -5,7 +5,7 @@ import android.content.res.Configuration
 
 internal interface ComponentCallbacks : ComponentCallbacks2 {
 
-    override fun onConfigurationChanged(newConfig: Configuration?) {}
+    override fun onConfigurationChanged(newConfig: Configuration) {}
 
     override fun onLowMemory() = onTrimMemory(ComponentCallbacks2.TRIM_MEMORY_COMPLETE)
 
