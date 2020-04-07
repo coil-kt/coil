6fa5fd71743b509e48a8de3179228d90392ce382
Colin White <colin@colinwhite.me>
2020-04-06 20:41:13 -0700

    Move computeCacheKey to RealImageLoader. Partially outline LazySizeResolver.size(). (#352)

--- coil-base/src/test/java/coil/memory/MemoryCacheServiceTest.kt
+++ coil-base/src/test/java/coil/memory/MemoryCacheServiceTest.kt
@@ -5,37 +5,23 @@ import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import androidx.test.core.app.ApplicationProvider
 import coil.DefaultRequestOptions
-import coil.EventListener
-import coil.RealImageLoader
 import coil.annotation.ExperimentalCoilApi
-import coil.bitmappool.BitmapPool
-import coil.decode.Options
-import coil.fetch.Fetcher
-import coil.request.Parameters
 import coil.request.Request
-import coil.size.OriginalSize
 import coil.size.PixelSize
 import coil.size.Precision
 import coil.size.Scale
 import coil.size.Size
 import coil.size.SizeResolver
-import coil.transform.Transformation
 import coil.util.createBitmap
 import coil.util.createGetRequest
 import coil.util.createLoadRequest
 import coil.util.toDrawable
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
-import kotlin.test.assertEquals
 import kotlin.test.assertFalse
-import kotlin.test.assertNull
 import kotlin.test.assertTrue
-import kotlin.test.fail
 
 @RunWith(RobolectricTestRunner::class)
 @OptIn(ExperimentalCoilApi::class)
@@ -50,65 +36,6 @@ class MemoryCacheServiceTest {
         val defaults = DefaultRequestOptions()
         val requestService = RequestService(defaults, null)
         service = MemoryCacheService(requestService, defaults, null)
-    }
-
-    @Test
-    fun `computeCacheKey - null key`() {
-        val fetcher = createFakeFetcher(key = null)
-        val size = createFakeLazySizeResolver()
-        val key = runBlocking {
-            service.computeCacheKey(fetcher, Unit, Parameters.EMPTY, emptyList(), size)
-        }
-
-        assertNull(key)
-    }
-
-    @Test
-    fun `computeCacheKey - basic key`() {
-        val fetcher = createFakeFetcher()
-        val size = createFakeLazySizeResolver()
-        val result = runBlocking {
-            service.computeCacheKey(fetcher, Unit, Parameters.EMPTY, emptyList(), size)
-        }
-
-        assertEquals("base_key", result)
-    }
-
-    @Test
-    fun `computeCacheKey - params only`() {
-        val fetcher = createFakeFetcher()
-        val parameters = createFakeParameters()
-        val size = createFakeLazySizeResolver()
-        val result = runBlocking {
-            service.computeCacheKey(fetcher, Unit, parameters, emptyList(), size)
-        }
-
-        assertEquals("base_key#key2=cached2#key3=cached3", result)
-    }
-
-    @Test
-    fun `computeCacheKey - transformations only`() {
-        val fetcher = createFakeFetcher()
-        val transformations = createFakeTransformations()
-        val size = createFakeLazySizeResolver { PixelSize(123, 332) }
-        val result = runBlocking {
-            service.computeCacheKey(fetcher, Unit, Parameters.EMPTY, transformations, size)
-        }
-
-        assertEquals("base_key#key1#key2#PixelSize(width=123, height=332)", result)
-    }
-
-    @Test
-    fun `computeCacheKey - complex key`() {
-        val fetcher = createFakeFetcher()
-        val parameters = createFakeParameters()
-        val transformations = createFakeTransformations()
-        val size = createFakeLazySizeResolver { OriginalSize }
-        val result = runBlocking {
-            service.computeCacheKey(fetcher, Unit, parameters, transformations, size)
-        }
-
-        assertEquals("base_key#key2=cached2#key3=cached3#key1#key2#coil.size.OriginalSize", result)
     }
 
     @Test
@@ -366,72 +293,6 @@ class MemoryCacheServiceTest {
             size = PixelSize(400, 800),
             scale = Scale.FIT
         ))
-    }
-
-    @Test
-    fun `lazySizeResolver - resolves at most once`() {
-        var isFirstResolve = true
-        val lazySizeResolver = createFakeLazySizeResolver {
-            if (isFirstResolve) {
-                isFirstResolve = false
-                PixelSize(100, 100)
-            } else {
-                throw IllegalStateException()
-            }
-        }
-
-        runBlocking {
-            assertEquals(lazySizeResolver.size(), lazySizeResolver.size())
-        }
-    }
-
-    private fun createFakeTransformations(): List<Transformation> {
-        return listOf(
-            object : Transformation {
-                override fun key() = "key1"
-                override suspend fun transform(pool: BitmapPool, input: Bitmap, size: Size) = fail()
-            },
-            object : Transformation {
-                override fun key() = "key2"
-                override suspend fun transform(pool: BitmapPool, input: Bitmap, size: Size) = fail()
-            }
-        )
-    }
-
-    private fun createFakeParameters(): Parameters {
-        return Parameters.Builder()
-            .set("key1", "no_cache", cacheKey = null)
-            .set("key2", "cached2")
-            .set("key3", "cached3")
-            .build()
-    }
-
-    private fun createFakeFetcher(key: String? = "base_key"): Fetcher<Any> {
-        return object : Fetcher<Any> {
-            override fun key(data: Any) = key
-
-            override suspend fun fetch(
-                pool: BitmapPool,
-                data: Any,
-                size: Size,
-                options: Options
-            ) = fail()
-        }
-    }
-
-    private fun createFakeLazySizeResolver(
-        block: suspend () -> Size = { fail() }
-    ): RealImageLoader.LazySizeResolver {
-        return RealImageLoader.LazySizeResolver(
-            scope = CoroutineScope(Job()), // Pass a fake scope.
-            sizeResolver = object : SizeResolver {
-                override suspend fun size() = block()
-            },
-            targetDelegate = EmptyTargetDelegate,
-            request = createLoadRequest(context),
-            defaults = DefaultRequestOptions(),
-            eventListener = EventListener.NONE
-        )
     }
 
     /** Convenience function to avoid having to specify the [SizeResolver] explicitly. */
