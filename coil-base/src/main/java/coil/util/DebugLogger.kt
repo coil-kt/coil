eaca0d7210a489b7e5fbcc65a5bbd7398ee4b535
Colin White <colin@colinwhite.me>
2020-04-08 20:55:03 -0700

    Support passing a log level to DebugLogger's constructor. (#355)
    * Support passing a log level to DebugLogger's constructor.
    
    * Update API.

--- coil-base/src/main/java/coil/util/DebugLogger.kt
+++ coil-base/src/main/java/coil/util/DebugLogger.kt
@@ -13,13 +13,17 @@ import java.io.StringWriter
  * NOTE: You **should not** enable this in release builds. Adding this to your [ImageLoader] reduces performance.
  * Additionally, this will log URLs which can contain [PII](https://en.wikipedia.org/wiki/Personal_data).
  */
-class DebugLogger : Logger {
+class DebugLogger @JvmOverloads constructor(level: Int = Log.DEBUG) : Logger {
 
-    override var level: Int = Log.DEBUG
+    override var level: Int = level
         set(value) {
-            require(level in Log.VERBOSE..Log.ASSERT) { "Invalid log level." }
+            assertValidLevel(value)
             field = value
         }
+
+    init {
+        assertValidLevel(level)
+    }
 
     override fun log(tag: String, priority: Int, message: String?, throwable: Throwable?) {
         if (message != null) {
@@ -31,5 +35,9 @@ class DebugLogger : Logger {
             throwable.printStackTrace(PrintWriter(writer))
             Log.println(priority, tag, writer.toString())
         }
+    }
+
+    private fun assertValidLevel(value: Int) {
+        require(value in Log.VERBOSE..Log.ASSERT) { "Invalid log level: $value" }
     }
 }
