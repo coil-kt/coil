b9613974d34300f859661add48e5aac558414555
Colin White <colin@colinwhite.me>
2020-04-17 12:29:04 -0700

    Update AGP (3.6.3).

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.6.2")
+        classpath("com.android.tools.build:gradle:3.6.3")
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.11.1")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.10.1")
         classpath("org.jetbrains.kotlinx:binary-compatibility-validator:0.2.3")
