b2600e8d32f9d1e00fa43d6c9ee9382b8a964ea7
Colin White <colin@colinwhite.me>
2020-05-02 16:42:40 -0700

    Initial commit.

---
+++ coil-base/src/main/java/coil/util/SystemCallbacks.kt
@@ -0,0 +1,65 @@
+package coil.util
+
+import android.content.ComponentCallbacks2
+import android.content.Context
+import android.content.res.Configuration
+import android.util.Log
+import coil.ImageLoader
+import coil.RealImageLoader
+import java.lang.ref.WeakReference
+
+/**
+ * Proxies [ComponentCallbacks2] and [NetworkObserver.Listener] calls to a weakly referenced [imageLoader].
+ *
+ * This prevents the system from having a strong reference to the [imageLoader], which allows it be freed
+ * naturally even if [ImageLoader.shutdown] is not called. If the [imageLoader] is freed, it unregisters
+ * its callbacks.
+ */
+internal class SystemCallbacks(
+    imageLoader: RealImageLoader,
+    private val context: Context
+) : ComponentCallbacks2, NetworkObserver.Listener {
+
+    companion object {
+        private const val TAG = "NetworkObserver"
+        private const val ONLINE = "ONLINE"
+        private const val OFFLINE = "OFFLINE"
+    }
+
+    private val imageLoader = WeakReference(imageLoader)
+    private val networkObserver = NetworkObserver(context, this, imageLoader.logger)
+
+    private var _isOnline = networkObserver.isOnline
+
+    val isOnline get() = _isOnline
+
+    init {
+        context.registerComponentCallbacks(this)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        imageLoader.get() ?: shutdown()
+    }
+
+    override fun onTrimMemory(level: Int) {
+        imageLoader.get()?.onTrimMemory(level) ?: shutdown()
+    }
+
+    override fun onLowMemory() = onTrimMemory(ComponentCallbacks2.TRIM_MEMORY_COMPLETE)
+
+    override fun onConnectivityChange(isOnline: Boolean) {
+        val imageLoader = imageLoader.get()
+        if (imageLoader == null) {
+            shutdown()
+            return
+        }
+
+        _isOnline = isOnline
+        imageLoader.logger?.log(TAG, Log.INFO) { if (isOnline) ONLINE else OFFLINE }
+    }
+
+    fun shutdown() {
+        context.unregisterComponentCallbacks(this)
+        networkObserver.shutdown()
+    }
+}
