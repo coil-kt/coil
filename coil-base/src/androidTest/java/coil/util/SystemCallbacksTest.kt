d117f198f176f455e2b5b602f4d553ba04688d74
Colin White <colin@colinwhite.me>
2020-05-05 21:37:30 -0700

    Use androidx method.

--- coil-base/src/androidTest/java/coil/util/SystemCallbacksTest.kt
+++ coil-base/src/androidTest/java/coil/util/SystemCallbacksTest.kt
@@ -3,6 +3,7 @@ package coil.util
 import android.content.ComponentCallbacks2.TRIM_MEMORY_BACKGROUND
 import android.content.Context
 import android.graphics.Bitmap
+import androidx.core.graphics.createBitmap
 import androidx.test.core.app.ApplicationProvider
 import coil.ImageLoader
 import coil.RealImageLoader
@@ -26,7 +27,7 @@ class SystemCallbacksTest {
         // Keep allocating bitmaps until either the image loader is freed or we run out of memory.
         val bitmaps = mutableListOf<Bitmap>()
         while (systemCallbacks.imageLoader.get() != null) {
-            bitmaps += Bitmap.createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
+            bitmaps += createBitmap(1000, 1000, Bitmap.Config.ARGB_8888)
         }
 
         // Ensure that the next system callback is called.
