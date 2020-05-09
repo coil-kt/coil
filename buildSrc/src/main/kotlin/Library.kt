0a33bee5453d078a5553491ee6e00081d361c35a
Colin White <colin@colinwhite.me>
2020-05-08 21:35:45 -0700

    Update Coroutines (1.3.6).

--- buildSrc/src/main/kotlin/Library.kt
+++ buildSrc/src/main/kotlin/Library.kt
@@ -6,7 +6,7 @@ object Library {
 
     // CORE
 
-    private const val COROUTINES_VERSION = "1.3.5"
+    private const val COROUTINES_VERSION = "1.3.6"
     const val KOTLINX_COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$COROUTINES_VERSION"
 
     const val ANDROIDX_ACTIVITY = "androidx.activity:activity-ktx:1.1.0"
