5b44dd4e06882b0819c20764200136cd7cc95a3b
Colin White <colin@colinwhite.me>
2020-06-06 17:42:27 -0700

    Ensure cancellation is handled on the main thread. (#426)

--- coil-base/src/main/java/coil/util/Lifecycles.kt
+++ coil-base/src/main/java/coil/util/Lifecycles.kt
@@ -7,27 +7,32 @@ import androidx.annotation.MainThread
 import androidx.lifecycle.DefaultLifecycleObserver
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Lifecycle.State.STARTED
+import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.LifecycleOwner
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 
-/** Suspend until [this] is at least [STARTED] */
+/** Suspend until [Lifecycle.getCurrentState] is at least [STARTED] */
 @MainThread
 internal suspend inline fun Lifecycle.awaitStarted() {
     // Fast path: we're already started.
     if (currentState.isAtLeast(STARTED)) return
 
     // Slow path: observe the lifecycle until we're started.
-    suspendCancellableCoroutine<Unit> { continuation ->
-        val observer = object : DefaultLifecycleObserver {
-            override fun onStart(owner: LifecycleOwner) {
-                removeObserver(this)
-                continuation.resume(Unit)
+    var observer: LifecycleObserver? = null
+    try {
+        suspendCancellableCoroutine<Unit> { continuation ->
+            observer = object : DefaultLifecycleObserver {
+                override fun onStart(owner: LifecycleOwner) {
+                    removeObserver(this)
+                    continuation.resume(Unit)
+                }
             }
+            addObserver(observer!!)
         }
-        continuation.invokeOnCancellation {
-            removeObserver(observer)
-        }
-        addObserver(observer)
+    } catch (throwable: Throwable) {
+        // Ensure exceptions are handled on the main thread.
+        observer?.let(::removeObserver)
+        throw throwable
     }
 }
