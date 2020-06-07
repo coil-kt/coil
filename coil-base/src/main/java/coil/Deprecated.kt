20339ec2ee5d13148bcf49e67f8987db539ff68d
Colin White <colin@colinwhite.me>
2020-06-06 19:22:04 -0700

    Add deprecated typealias for ImageLoaderBuilder.

---
+++ coil-base/src/main/java/coil/Deprecated.kt
@@ -0,0 +1,9 @@
+@file:Suppress("unused")
+
+package coil
+
+@Deprecated(
+    message = "Replace with ImageLoader.Builder.",
+    replaceWith = ReplaceWith("ImageLoader.Builder", "coil.ImageLoader")
+)
+typealias ImageLoaderBuilder = ImageLoader.Builder
