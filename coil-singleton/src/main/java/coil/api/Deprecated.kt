fb06897d8d3bde81a0bdc955684b4cf766a1f668
Colin White <colin@colinwhite.me>
2020-07-06 17:27:35 -0700

    Add deprecated placeholders.

--- coil-singleton/src/main/java/coil/api/Deprecated.kt
+++ coil-singleton/src/main/java/coil/api/Deprecated.kt
@@ -12,109 +12,75 @@ import coil.Coil
 import coil.ImageLoader
 import coil.request.ImageRequest
 import coil.request.RequestDisposable
-import coil.util.CoilUtils
 import okhttp3.HttpUrl
 import java.io.File
+import coil.clear as _clear
+import coil.load as _load
+import coil.loadAny as _loadAny
 
-/** @see ImageView.loadAny */
 @Deprecated("Replace `coil.api.load` with `coil.load`.")
 @JvmSynthetic
 inline fun ImageView.load(
     uri: String?,
     imageLoader: ImageLoader = Coil.imageLoader(context),
     builder: ImageRequest.Builder.() -> Unit = {}
-): RequestDisposable = loadAny(uri, imageLoader, builder)
+): RequestDisposable = _load(uri, imageLoader, builder)
 
-/** @see ImageView.loadAny */
 @Deprecated("Replace `coil.api.load` with `coil.load`.")
 @JvmSynthetic
 inline fun ImageView.load(
     url: HttpUrl?,
     imageLoader: ImageLoader = Coil.imageLoader(context),
     builder: ImageRequest.Builder.() -> Unit = {}
-): RequestDisposable = loadAny(url, imageLoader, builder)
+): RequestDisposable = _load(url, imageLoader, builder)
 
-/** @see ImageView.loadAny */
 @Deprecated("Replace `coil.api.load` with `coil.load`.")
 @JvmSynthetic
 inline fun ImageView.load(
     uri: Uri?,
     imageLoader: ImageLoader = Coil.imageLoader(context),
     builder: ImageRequest.Builder.() -> Unit = {}
-): RequestDisposable = loadAny(uri, imageLoader, builder)
+): RequestDisposable = _load(uri, imageLoader, builder)
 
-/** @see ImageView.loadAny */
 @Deprecated("Replace `coil.api.load` with `coil.load`.")
 @JvmSynthetic
 inline fun ImageView.load(
     file: File?,
     imageLoader: ImageLoader = Coil.imageLoader(context),
     builder: ImageRequest.Builder.() -> Unit = {}
-): RequestDisposable = loadAny(file, imageLoader, builder)
+): RequestDisposable = _load(file, imageLoader, builder)
 
-/** @see ImageView.loadAny */
 @Deprecated("Replace `coil.api.load` with `coil.load`.")
 @JvmSynthetic
 inline fun ImageView.load(
     @DrawableRes drawableResId: Int,
     imageLoader: ImageLoader = Coil.imageLoader(context),
     builder: ImageRequest.Builder.() -> Unit = {}
-): RequestDisposable = loadAny(drawableResId, imageLoader, builder)
+): RequestDisposable = _load(drawableResId, imageLoader, builder)
 
-/** @see ImageView.loadAny */
 @Deprecated("Replace `coil.api.load` with `coil.load`.")
 @JvmSynthetic
 inline fun ImageView.load(
     drawable: Drawable?,
     imageLoader: ImageLoader = Coil.imageLoader(context),
     builder: ImageRequest.Builder.() -> Unit = {}
-): RequestDisposable = loadAny(drawable, imageLoader, builder)
+): RequestDisposable = _load(drawable, imageLoader, builder)
 
-/** @see ImageView.loadAny */
 @Deprecated("Replace `coil.api.load` with `coil.load`.")
 @JvmSynthetic
 inline fun ImageView.load(
     bitmap: Bitmap?,
     imageLoader: ImageLoader = Coil.imageLoader(context),
     builder: ImageRequest.Builder.() -> Unit = {}
-): RequestDisposable = loadAny(bitmap, imageLoader, builder)
+): RequestDisposable = _load(bitmap, imageLoader, builder)
 
-/**
- * Load the image referenced by [data] and set it on this [ImageView].
- *
- * This is the type-unsafe version of [ImageView.load].
- *
- * Example:
- * ```
- * imageView.load("https://www.example.com/image.jpg") {
- *     crossfade(true)
- *     transformations(CircleCropTransformation())
- * }
- * ```
- *
- * @param data The data to load.
- * @param imageLoader The [ImageLoader] that will be used to enqueue the [ImageRequest].
- * @param builder An optional lambda to configure the request before it is enqueued.
- */
 @Deprecated("Replace `coil.api.load` with `coil.load`.")
 @JvmSynthetic
 inline fun ImageView.loadAny(
     data: Any?,
     imageLoader: ImageLoader = Coil.imageLoader(context),
     builder: ImageRequest.Builder.() -> Unit = {}
-): RequestDisposable {
-    val request = ImageRequest.Builder(context)
-        .data(data)
-        .target(this)
-        .apply(builder)
-        .build()
-    return imageLoader.enqueue(request)
-}
+): RequestDisposable = _loadAny(data, imageLoader, builder)
 
-/**
- * Cancel any in progress requests and clear all resources associated with this [ImageView].
- */
 @Deprecated("Replace `coil.api.clear` with `coil.clear`.")
-fun ImageView.clear() {
-    CoilUtils.clear(this)
-}
+fun ImageView.clear() = _clear()
---
+++ coil-video/src/main/java/coil/extension/Deprecated.kt
@@ -0,0 +1,25 @@
+@file:Suppress("unused")
+@file:JvmName("Videos")
+
+package coil.extension
+
+import coil.request.ImageRequest
+import coil.request.Parameters
+import coil.request.videoFrameMicros as _videoFrameMicros
+import coil.request.videoFrameMillis as _videoFrameMillis
+import coil.request.videoFrameOption as _videoFrameOption
+
+@Deprecated("Replace `coil.extension.videoFrameMillis` with `coil.request.videoFrameMillis`.")
+fun ImageRequest.Builder.videoFrameMillis(frameMillis: Long) = _videoFrameMillis(frameMillis)
+
+@Deprecated("Replace `coil.extension.videoFrameMicros` with `coil.request.videoFrameMicros`.")
+fun ImageRequest.Builder.videoFrameMicros(frameMicros: Long) = _videoFrameMicros(frameMicros)
+
+@Deprecated("Replace `coil.extension.videoFrameOption` with `coil.request.videoFrameOption`.")
+fun ImageRequest.Builder.videoFrameOption(option: Int) = _videoFrameOption(option)
+
+@Deprecated("Replace `coil.extension.videoFrameMicros` with `coil.request.videoFrameMicros`.")
+fun Parameters.videoFrameMicros(): Long? = _videoFrameMicros()
+
+@Deprecated("Replace `coil.extension.videoFrameOption` with `coil.request.videoFrameOption`.")
+fun Parameters.videoFrameOption(): Int? = _videoFrameOption()
