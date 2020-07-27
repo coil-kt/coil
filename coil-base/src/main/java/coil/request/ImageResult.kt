114a874cec2d49e08dcb6a0a2f2547fa334c5e79
Colin White <colin@colinwhite.me>
2020-07-26 22:54:30 -0400

    Tweaks.

--- coil-base/src/main/java/coil/request/ImageResult.kt
+++ coil-base/src/main/java/coil/request/ImageResult.kt
@@ -20,7 +20,7 @@ sealed class ImageResult {
      *
      * @param memoryCacheKey The cache key for the image in the memory cache.
      *  It is null if the image was not written to the memory cache.
-     * @param isSampled True if [drawable] is sampled (i.e. loaded into memory at less than its original size).
+     * @param isSampled True if the image is sampled (i.e. loaded into memory at less than its original size).
      * @param dataSource The data source that the image was loaded from.
      * @param isPlaceholderMemoryCacheKeyPresent True if the request's [ImageRequest.placeholderMemoryCacheKey] was
      *  present in the memory cache and was set as the placeholder.
