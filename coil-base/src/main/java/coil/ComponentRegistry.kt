66a8ab94db2efd42ba5a65e11b557b65c50185c5
Colin White <colin@colinwhite.me>
2020-08-18 16:34:27 -0400

    Remove all existing deprecated APIs. (#498)

--- coil-base/src/main/java/coil/ComponentRegistry.kt
+++ coil-base/src/main/java/coil/ComponentRegistry.kt
@@ -8,7 +8,6 @@ import coil.decode.Decoder
 import coil.fetch.Fetcher
 import coil.intercept.Interceptor
 import coil.map.Mapper
-import coil.map.MeasuredMapper
 import coil.util.MultiList
 import coil.util.MultiMutableList
 
@@ -20,12 +19,11 @@ import coil.util.MultiMutableList
 class ComponentRegistry private constructor(
     internal val interceptors: List<Interceptor>,
     internal val mappers: MultiList<Mapper<out Any, *>, Class<out Any>>,
-    internal val measuredMappers: MultiList<MeasuredMapper<out Any, *>, Class<out Any>>,
     internal val fetchers: MultiList<Fetcher<out Any>, Class<out Any>>,
     internal val decoders: List<Decoder>
 ) {
 
-    constructor() : this(emptyList(), emptyList(), emptyList(), emptyList(), emptyList())
+    constructor() : this(emptyList(), emptyList(), emptyList(), emptyList())
 
     fun newBuilder() = Builder(this)
 
@@ -33,14 +31,12 @@ class ComponentRegistry private constructor(
 
         private val interceptors: MutableList<Interceptor>
         private val mappers: MultiMutableList<Mapper<out Any, *>, Class<out Any>>
-        private val measuredMappers: MultiMutableList<MeasuredMapper<out Any, *>, Class<out Any>>
         private val fetchers: MultiMutableList<Fetcher<out Any>, Class<out Any>>
         private val decoders: MutableList<Decoder>
 
         constructor() {
             interceptors = mutableListOf()
             mappers = mutableListOf()
-            measuredMappers = mutableListOf()
             fetchers = mutableListOf()
             decoders = mutableListOf()
         }
@@ -48,7 +44,6 @@ class ComponentRegistry private constructor(
         constructor(registry: ComponentRegistry) {
             interceptors = registry.interceptors.toMutableList()
             mappers = registry.mappers.toMutableList()
-            measuredMappers = registry.measuredMappers.toMutableList()
             fetchers = registry.fetchers.toMutableList()
             decoders = registry.decoders.toMutableList()
         }
@@ -66,28 +61,6 @@ class ComponentRegistry private constructor(
             mappers += mapper to type
         }
 
-        @Deprecated(
-            message = "Parameter order is reversed.",
-            replaceWith = ReplaceWith("add(mapper, type)"),
-            level = DeprecationLevel.ERROR
-        )
-        @PublishedApi
-        internal fun <T : Any> add(type: Class<T>, mapper: Mapper<T, *>) = add(mapper, type)
-
-        /** Register a [MeasuredMapper]. */
-        @Deprecated("MeasuredMappers are deprecated. Replace with an Interceptor.")
-        inline fun <reified T : Any> add(measuredMapper: MeasuredMapper<T, *>) = add(T::class.java, measuredMapper)
-
-        @Deprecated("MeasuredMappers are deprecated. Replace with an Interceptor.")
-        @PublishedApi
-        internal fun <T : Any> add(measuredMapper: MeasuredMapper<T, *>, type: Class<T>) = apply {
-            measuredMappers += measuredMapper to type
-        }
-
-        @Deprecated("MeasuredMappers are deprecated. Replace with an Interceptor.")
-        @PublishedApi
-        internal fun <T : Any> add(type: Class<T>, measuredMapper: MeasuredMapper<T, *>) = add(measuredMapper, type)
-
         /** Register a [Fetcher]. */
         inline fun <reified T : Any> add(fetcher: Fetcher<T>) = add(fetcher, T::class.java)
 
@@ -95,14 +68,6 @@ class ComponentRegistry private constructor(
         internal fun <T : Any> add(fetcher: Fetcher<T>, type: Class<T>) = apply {
             fetchers += fetcher to type
         }
-
-        @Deprecated(
-            message = "Parameter order is reversed.",
-            replaceWith = ReplaceWith("add(fetcher, type)"),
-            level = DeprecationLevel.ERROR
-        )
-        @PublishedApi
-        internal fun <T : Any> add(type: Class<T>, fetcher: Fetcher<T>) = add(fetcher, type)
 
         /** Register a [Decoder]. */
         fun add(decoder: Decoder) = apply {
@@ -113,7 +78,6 @@ class ComponentRegistry private constructor(
             return ComponentRegistry(
                 interceptors.toList(),
                 mappers.toList(),
-                measuredMappers.toList(),
                 fetchers.toList(),
                 decoders.toList()
             )
