1555a2ef654311ad2ba4c4407cb31300867ef9c7
Colin White <colin@colinwhite.me>
2020-09-06 21:34:19 -0400

    Fix invalid Dokka links. (#524)
    * Centralize Dokka configuration.
    
    * Fix script.

--- deploy_docs.sh
+++ deploy_docs.sh
@@ -3,8 +3,19 @@
 # Clean any previous Dokka docs.
 rm -rf docs/api
 
-# Build the Dokka docs.
-./gradlew clean :coil-base:dokka :coil-gif:dokka :coil-svg:dokka :coil-video:dokka :coil-singleton:dokka
+# Work around Dokka failing to link against external links generated from 'gfm' sources.
+wget -O package-list-okio https://square.github.io/okio/2.x/okio/package-list
+sed -i '' 's/$dokka.linkExtension:md/$dokka.linkExtension:html/g' package-list-okio
+
+# Build the coil-base docs.
+./gradlew clean :coil-base:dokka
+
+# Work around Dokka failing to link against external links generated from 'gfm' sources.
+cp docs/api/coil-base/package-list package-list-coil-base
+sed -i '' 's/$dokka.linkExtension:md/$dokka.linkExtension:html/g' package-list-coil-base
+
+# Build the remaining docs.
+./gradlew :coil-gif:dokka :coil-svg:dokka :coil-video:dokka :coil-singleton:dokka
 
 # Copy outside files into the docs folder.
 sed -e '/full documentation here/ { N; d; }' < README.md > docs/index.md
@@ -21,4 +32,4 @@ cp README-zh.md docs/README-zh.md
 mkdocs gh-deploy
 
 # Clean up.
-rm docs/index.md docs/contributing.md docs/changelog.md docs/logo.svg docs/gifs.md docs/svgs.md docs/videos.md docs/README-ko.md docs/README-zh.md
+rm package-list-coil-base package-list-okio docs/index.md docs/contributing.md docs/changelog.md docs/logo.svg docs/gifs.md docs/svgs.md docs/videos.md docs/README-ko.md docs/README-zh.md
