529f0bf27e83c051173e1185c972b32f642f972e
Colin White <colin@colinwhite.me>
2020-09-07 14:39:58 -0400

    Update command.

--- assemble_docs.sh
+++ assemble_docs.sh
@@ -8,14 +8,14 @@ wget -O package-list-okio https://square.github.io/okio/2.x/okio/package-list
 sed -i '' 's/$dokka.linkExtension:md/$dokka.linkExtension:html/g' package-list-okio
 
 # Build the coil-base docs.
-./gradlew clean :coil-base:dokka
+./gradlew clean :coil-base:dokkaGfm
 
 # Work around Dokka failing to link against external links generated from 'gfm' sources.
 cp docs/api/coil-base/package-list package-list-coil-base
 sed -i '' 's/$dokka.linkExtension:md/$dokka.linkExtension:html/g' package-list-coil-base
 
 # Build the remaining docs.
-./gradlew :coil-gif:dokka :coil-svg:dokka :coil-video:dokka :coil-singleton:dokka
+./gradlew :coil-gif:dokkaGfm :coil-svg:dokkaGfm :coil-video:dokkaGfm :coil-singleton:dokkaGfm
 
 # Clean up.
 rm package-list-coil-base package-list-okio
