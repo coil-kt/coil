63f428351d45d440aa5e165cfeaa2458eaeaa9a3
Colin White <colin@colinwhite.me>
2020-09-16 19:28:28 -0400

    Move Parameter extension functions.

--- coil-base/src/main/java/coil/request/Parameters.kt
+++ coil-base/src/main/java/coil/request/Parameters.kt
@@ -1,4 +1,5 @@
-@file:Suppress("unused")
+@file:JvmName("-Parameters")
+@file:Suppress("NOTHING_TO_INLINE", "unused")
 
 package coil.request
 
@@ -108,3 +109,12 @@ class Parameters private constructor(
         @JvmField val EMPTY = Parameters()
     }
 }
+
+/** Returns the number of parameters in this object. */
+inline fun Parameters.count(): Int = size
+
+/** Return true when the set contains elements. */
+inline fun Parameters.isNotEmpty(): Boolean = !isEmpty()
+
+/** Returns the value associated with [key] or null if [key] has no mapping. */
+inline operator fun Parameters.get(key: String): Any? = value(key)
