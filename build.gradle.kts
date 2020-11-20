aa1775ce90cc819d9e676d68731833482c2d524e
Colin White <colin@colinwhite.me>
2020-11-19 22:51:44 -0600

    Update AGP (4.1.1) and Gradle (6.7.1).

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:4.1.0")
+        classpath("com.android.tools.build:gradle:4.1.1")
         classpath("com.vanniktech:gradle-maven-publish-plugin:0.13.0")
         classpath("org.jetbrains.dokka:dokka-gradle-plugin:0.10.1")
         classpath("org.jetbrains.kotlinx:binary-compatibility-validator:0.2.3")
