9f8f239a22180fec5b400a99558cddc07e9577c2
Goooler <wangzongler@gmail.com>
2021-02-02 09:47:42 +0800

    Remove unused import

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,6 @@
 import coil.by
 import coil.groupId
 import coil.versionName
-import com.android.build.gradle.BaseExtension
 import kotlinx.validation.ApiValidationExtension
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
