ec7ec2fe37f8e30017479ae431ba21b7d9e87a41
Colin White <colin@colinwhite.me>
2021-02-03 13:13:55 -0600

    Revert.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,13 @@
 import coil.by
 import coil.groupId
 import coil.versionName
-import java.net.URL
 import kotlinx.validation.ApiValidationExtension
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jlleitschuh.gradle.ktlint.KtlintExtension
+import java.net.URL
 
 buildscript {
     apply(from = "buildSrc/extra.gradle.kts")
