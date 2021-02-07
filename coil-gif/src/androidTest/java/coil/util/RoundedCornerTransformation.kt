860a7f32436962efb6d51fa8763b1607d53f3a09
Sagar Viradiya <sagarviradiya4444@gmail.com>
2021-02-08 00:36:37 +0530

    Fix the instrumentation test failure
    - Fix class not found error for rounded rectangle below API level 21 in RoundedCornerTransformation.kt

--- coil-gif/src/androidTest/java/coil/util/RoundedCornerTransformation.kt
+++ coil-gif/src/androidTest/java/coil/util/RoundedCornerTransformation.kt
@@ -6,6 +6,8 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.graphics.PorterDuff
 import android.graphics.PorterDuffXfermode
+import android.graphics.RectF
+import android.os.Build.VERSION.SDK_INT
 import coil.size.Size
 import coil.transform.AnimatedTransformation
 
@@ -15,7 +17,11 @@ class RoundedCornerTransformation : AnimatedTransformation {
         path.fillType = Path.FillType.INVERSE_EVEN_ODD
         val width = canvas.width
         val height = canvas.height
-        path.addRoundRect(0f, 0f, width.toFloat(), height.toFloat(), 20f, 20f, Path.Direction.CW)
+        if (SDK_INT >= 21) {
+            path.addRoundRect(0f, 0f, width.toFloat(), height.toFloat(), 20f, 20f, Path.Direction.CW)
+        } else {
+            path.addRoundRect(RectF(0f, 0f, width.toFloat(), height.toFloat()), 20f, 20f, Path.Direction.CW)
+        }
         val paint = Paint()
         paint.isAntiAlias = true
         paint.color = Color.TRANSPARENT
