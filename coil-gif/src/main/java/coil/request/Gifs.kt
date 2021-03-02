526ce617fd3d0cdbe6446149660ad06e42bbaa2e
Colin White <colin@colinwhite.me>
2021-03-01 21:33:39 -0600

    Fix case where MovieDrawable.getOpacity would incorrectly return transparent. Assorted code cleanup. (#682)

--- coil-gif/src/main/java/coil/request/Gifs.kt
+++ coil-gif/src/main/java/coil/request/Gifs.kt
@@ -3,6 +3,7 @@
 
 package coil.request
 
+import android.graphics.ImageDecoder
 import android.graphics.drawable.AnimatedImageDrawable
 import android.graphics.drawable.Drawable
 import coil.decode.GifDecoder.Companion.ANIMATED_TRANSFORMATION_KEY
@@ -25,35 +26,54 @@ fun ImageRequest.Builder.repeatCount(repeatCount: Int): ImageRequest.Builder {
     return setParameter(REPEAT_COUNT_KEY, repeatCount)
 }
 
-/** Get the number of times to repeat the animation if the result is an animated [Drawable]. */
+/**
+ * Get the number of times to repeat the animation if the result is an animated [Drawable].
+ */
 fun Parameters.repeatCount(): Int? = value(REPEAT_COUNT_KEY) as Int?
 
-/** Set the [AnimatedTransformation] that will be applied to the result if it is an animated [Drawable]. */
+/**
+ * Set the [AnimatedTransformation] that will be applied to the result if it is an animated [Drawable].
+ *
+ * Default: `null`
+ *
+ * @see MovieDrawable.setAnimatedTransformation
+ * @see ImageDecoder.setPostProcessor
+ */
 fun ImageRequest.Builder.animatedTransformation(animatedTransformation: AnimatedTransformation): ImageRequest.Builder {
     return setParameter(ANIMATED_TRANSFORMATION_KEY, animatedTransformation)
 }
 
-/** Get the [AnimatedTransformation] applied to the result if it is an animated [Drawable]. */
+/**
+ * Get the [AnimatedTransformation] that will be applied to the result if it is an animated [Drawable].
+ */
 fun Parameters.animatedTransformation(): AnimatedTransformation? {
     return value(ANIMATED_TRANSFORMATION_KEY) as AnimatedTransformation?
 }
 
-/** Set the callback to be invoked at the start of the animation if the result is an animated [Drawable]. */
+/**
+ * Set the callback to be invoked at the start of the animation if the result is an animated [Drawable].
+ */
 fun ImageRequest.Builder.onAnimationStart(callback: (() -> Unit)?): ImageRequest.Builder {
     return setParameter(ANIMATION_START_CALLBACK_KEY, callback)
 }
 
-/** Get the callback to be invoked at the start of the animation if the result is an animated [Drawable]. */
+/**
+ * Get the callback to be invoked at the start of the animation if the result is an animated [Drawable].
+ */
 fun Parameters.animationStartCallback(): (() -> Unit)? {
     return value(ANIMATION_START_CALLBACK_KEY) as (() -> Unit)?
 }
 
-/** Set the callback to be invoked at the end of the animation if the result is an animated [Drawable]. */
+/**
+ * Set the callback to be invoked at the end of the animation if the result is an animated [Drawable].
+ */
 fun ImageRequest.Builder.onAnimationEnd(callback: (() -> Unit)?): ImageRequest.Builder {
     return setParameter(ANIMATION_END_CALLBACK_KEY, callback)
 }
 
-/** Get the callback to be invoked at the end of the animation if the result is an animated [Drawable]. */
+/**
+ * Get the callback to be invoked at the end of the animation if the result is an animated [Drawable].
+ */
 fun Parameters.animationEndCallback(): (() -> Unit)? {
     return value(ANIMATION_END_CALLBACK_KEY) as (() -> Unit)?
 }
