24bdab733cf8f73d0ea8b283fb0ba3e2f657d91b
Colin White <colin@colinwhite.me>
2021-03-04 13:20:42 -0600

    Use the view bounds as the intrinsic size of an SVG in SvgDecoder. (#688)
    * Prefer view bounds as size in SvgDecoder.
    
    * Adjust algorithm.
    
    * Tweak test SVG.

--- coil-svg/src/main/java/coil/util/Extensions.kt
+++ coil-svg/src/main/java/coil/util/Extensions.kt
@@ -2,6 +2,8 @@
 
 package coil.util
 
+import android.graphics.Bitmap
+import android.os.Build.VERSION.SDK_INT
 import okio.BufferedSource
 import okio.ByteString
 
@@ -19,4 +21,12 @@ internal fun BufferedSource.indexOf(bytes: ByteString, fromIndex: Long, toIndex:
         currentIndex++
     }
     return -1
+}
+
+internal val Bitmap.Config.isHardware: Boolean
+    get() = SDK_INT >= 26 && this == Bitmap.Config.HARDWARE
+
+/** Convert null and [Bitmap.Config.HARDWARE] configs to [Bitmap.Config.ARGB_8888]. */
+internal fun Bitmap.Config?.toSoftware(): Bitmap.Config {
+    return if (this == null || isHardware) Bitmap.Config.ARGB_8888 else this
 }
