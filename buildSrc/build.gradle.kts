a3e6a47460e268caeb54066db2c19da8d0cd918a
Colin White <colin@colinwhite.me>
2021-03-15 23:18:00 -0500

    Migrate another callsite.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -5,7 +5,6 @@ plugins {
 repositories {
     google()
     mavenCentral()
-    jcenter()
 }
 
 apply(from = "extra.gradle.kts")
@@ -17,4 +16,13 @@ dependencies {
 
 kotlinDslPluginOptions {
     experimentalWarning.set(false)
+}
+
+// https://issuetracker.google.com/issues/179291081
+configurations.configureEach {
+    resolutionStrategy.eachDependency {
+        if (requested.group == "org.jetbrains.trove4j" && requested.name == "trove4j" && requested.version == "20160824") {
+            useTarget("org.jetbrains.intellij.deps:trove4j:1.0.20181211")
+        }
+    }
 }
