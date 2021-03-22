809c45e7c064897100c65aed1feff46e4d790acd
Goooler <wangzongler@gmail.com>
2021-03-23 02:46:46 +0800

    Update AGP (4.1.3). (#705)

--- buildSrc/extra.gradle.kts
+++ buildSrc/extra.gradle.kts
@@ -1,4 +1,4 @@
 rootProject.extra.apply {
-    set("androidPlugin", "com.android.tools.build:gradle:4.1.2")
+    set("androidPlugin", "com.android.tools.build:gradle:4.1.3")
     set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
 }
