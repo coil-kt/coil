7d333d4a25b9debcf2b45e7e8ba89df4ad78143d
Goooler <wangzongler@gmail.com>
2021-03-31 01:48:52 +0800

    Update kotlin (1.4.32) (#707)

--- buildSrc/extra.gradle.kts
+++ buildSrc/extra.gradle.kts
@@ -1,4 +1,4 @@
 rootProject.extra.apply {
     set("androidPlugin", "com.android.tools.build:gradle:4.1.3")
-    set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.31")
+    set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
 }
