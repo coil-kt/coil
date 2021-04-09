3aa5fd69aa000b895ecce9008aa38ca254aaae0d
Colin White <colin@colinwhite.me>
2021-04-09 14:36:48 -0500

    Update AGP (4.2.0-rc01).

--- buildSrc/extra.gradle.kts
+++ buildSrc/extra.gradle.kts
@@ -1,4 +1,4 @@
 rootProject.extra.apply {
-    set("androidPlugin", "com.android.tools.build:gradle:4.1.3")
+    set("androidPlugin", "com.android.tools.build:gradle:4.2.0-rc01")
     set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
 }
