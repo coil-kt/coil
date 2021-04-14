7980e61a7c60967e7a2416425994ddf2d55ac6e0
Colin White <colin@colinwhite.me>
2021-04-13 20:38:09 -0500

    Remove work-around.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -13,16 +13,3 @@ dependencies {
     implementation(rootProject.extra["androidPlugin"].toString())
     implementation(rootProject.extra["kotlinPlugin"].toString())
 }
-
-kotlinDslPluginOptions {
-    experimentalWarning.set(false)
-}
-
-// https://issuetracker.google.com/issues/179291081
-configurations.configureEach {
-    resolutionStrategy.eachDependency {
-        if (requested.group == "org.jetbrains.trove4j" && requested.name == "trove4j" && requested.version == "20160824") {
-            useTarget("org.jetbrains.intellij.deps:trove4j:1.0.20181211")
-        }
-    }
-}
