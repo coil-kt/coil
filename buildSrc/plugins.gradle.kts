4ed8e463c533870e1340d2242f5cacf7097fa001
Colin White <colin@colinwhite.me>
2021-04-26 13:42:54 -0500

    Update to release version.

--- buildSrc/plugins.gradle.kts
+++ buildSrc/plugins.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.extra.apply {
     set("androidPlugin", "com.android.tools.build:gradle:4.2.0-rc01")
-    set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0-RC")
+    set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
     set("mavenPublishPlugin", "com.vanniktech:gradle-maven-publish-plugin:0.14.2")
     set("dokkaPlugin", "org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
     set("dokkaAndroidPlugin", "org.jetbrains.dokka:android-documentation-plugin:1.4.32")
