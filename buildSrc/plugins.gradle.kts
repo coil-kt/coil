ea3e6a6e2678467dafd4cbf173e4b249ed58a387
Goooler <wangzongler@gmail.com>
2021-05-06 02:00:30 +0800

    Bump AGP to 4.2.0 (#742)

--- buildSrc/plugins.gradle.kts
+++ buildSrc/plugins.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.extra.apply {
-    set("androidPlugin", "com.android.tools.build:gradle:4.2.0-rc01")
+    set("androidPlugin", "com.android.tools.build:gradle:4.2.0")
     set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
     set("mavenPublishPlugin", "com.vanniktech:gradle-maven-publish-plugin:0.14.2")
     set("dokkaPlugin", "org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
