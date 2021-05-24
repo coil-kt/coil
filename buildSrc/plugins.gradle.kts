692b5e0bbf9dc0f912e8222bd3d085668e13462f
Goooler <wangzongler@gmail.com>
2021-05-25 02:27:03 +0800

    Bump kotlin to 1.5.10 (#765)

--- buildSrc/plugins.gradle.kts
+++ buildSrc/plugins.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.extra.apply {
     set("androidPlugin", "com.android.tools.build:gradle:4.2.1")
-    set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.0")
+    set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
     set("mavenPublishPlugin", "com.vanniktech:gradle-maven-publish-plugin:0.15.1")
     set("dokkaPlugin", "org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
     set("dokkaAndroidPlugin", "org.jetbrains.dokka:android-documentation-plugin:1.4.32")
