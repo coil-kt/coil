8ef18dfe34fb70e5e9266378d49e9d3ff7987d53
Goooler <wangzongler@gmail.com>
2021-06-16 17:57:08 +0800

    Bump binary-compatibility-validator to 0.6.0

--- buildSrc/plugins.gradle.kts
+++ buildSrc/plugins.gradle.kts
@@ -4,6 +4,6 @@ rootProject.extra.apply {
     set("mavenPublishPlugin", "com.vanniktech:gradle-maven-publish-plugin:0.15.1")
     set("dokkaPlugin", "org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
     set("dokkaAndroidPlugin", "org.jetbrains.dokka:android-documentation-plugin:1.4.32")
-    set("binaryCompatibilityPlugin", "org.jetbrains.kotlinx:binary-compatibility-validator:0.5.0")
+    set("binaryCompatibilityPlugin", "org.jetbrains.kotlinx:binary-compatibility-validator:0.6.0")
     set("ktlintPlugin", "org.jlleitschuh.gradle:ktlint-gradle:10.1.0")
 }
