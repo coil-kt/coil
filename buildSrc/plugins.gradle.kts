a1ca1594b8c505dec6915668214d5a0a05a70c4c
Colin White <colin@colinwhite.me>
2021-06-26 17:20:23 -0500

    Revert Kotlin version.

--- buildSrc/plugins.gradle.kts
+++ buildSrc/plugins.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.extra.apply {
     set("androidPlugin", "com.android.tools.build:gradle:7.1.0-alpha02")
-    set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.20")
+    set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
     set("mavenPublishPlugin", "com.vanniktech:gradle-maven-publish-plugin:0.16.0")
     set("dokkaPlugin", "org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
     set("dokkaAndroidPlugin", "org.jetbrains.dokka:android-documentation-plugin:1.4.32")
