c022ce0fa6bacd001cafe1c0a541d3155c3bae12
Colin White <colin@colinwhite.me>
2021-07-01 01:40:16 -0500

    Fix tests.

--- coil-compose-base/src/androidTest/java/coil/compose/ImagePainterTest.kt
+++ coil-compose-base/src/androidTest/java/coil/compose/ImagePainterTest.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.ColorPainter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
@@ -243,6 +244,7 @@ class ImagePainterTest {
                     modifier = Modifier
                         .size(size)
                         .testTag(Image),
+                    contentScale = ContentScale.Crop
                 )
 
                 LaunchedEffect(painter) {
