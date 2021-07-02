218cc41a878604a64f5af3e55e3ae09ac75e78aa
Colin White <colin@colinwhite.me>
2021-07-02 17:03:03 -0500

    Break emulator snapshot cache.

--- buildSrc/src/main/kotlin/Projects.kt
+++ buildSrc/src/main/kotlin/Projects.kt
@@ -66,12 +66,7 @@ private inline fun <reified T : BaseExtension> Project.setupBaseModule(crossinli
                 }
             }
         }
-        packagingOptions.resources.excludes += setOf(
-            "META-INF/licenses/**",
-            "META-INF/AL2.0",
-            "META-INF/LGPL2.1",
-            "META-INF/*kotlin_module"
-        )
+        packagingOptions.resources.pickFirsts += "META-INF/*kotlin_module"
         (this as T).block()
     }
 }
