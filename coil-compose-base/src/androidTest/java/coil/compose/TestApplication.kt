30135a7838809b12f38d6b8f72363fa8a0ac1f0e
Colin White <colin@colinwhite.me>
2021-07-07 17:01:32 -0500

    Simplify.

--- coil-compose-base/src/androidTest/java/coil/compose/TestApplication.kt
+++ coil-compose-base/src/androidTest/java/coil/compose/TestApplication.kt
@@ -1,8 +1,6 @@
 package coil.compose
 
-import android.app.Activity
 import android.app.Application
-import android.os.Bundle
 import android.os.StrictMode
 
 class TestApplication : Application() {
@@ -12,26 +10,10 @@ class TestApplication : Application() {
 
         // 'MockWebServer.url()' does a network check internally and triggers strict mode.
         // To work around that in the tests, we allow network on main thread.
-        registerActivityLifecycleCallbacks(
-            object : DefaultActivityLifecycleCallbacks {
-                override fun onActivityCreated(activity: Activity, savedState: Bundle?) {
-                    val threadPolicy = StrictMode.ThreadPolicy.Builder()
-                        .detectAll()
-                        .permitNetwork()
-                        .build()
-                    StrictMode.setThreadPolicy(threadPolicy)
-                }
-            }
-        )
-    }
-
-    private interface DefaultActivityLifecycleCallbacks : ActivityLifecycleCallbacks {
-        override fun onActivityCreated(activity: Activity, savedState: Bundle?) {}
-        override fun onActivityStarted(activity: Activity) {}
-        override fun onActivityResumed(activity: Activity) {}
-        override fun onActivityPaused(activity: Activity) {}
-        override fun onActivityStopped(activity: Activity) {}
-        override fun onActivitySaveInstanceState(activity: Activity, savedState: Bundle) {}
-        override fun onActivityDestroyed(activity: Activity) {}
+        val threadPolicy = StrictMode.ThreadPolicy.Builder()
+            .detectNetwork()
+            .permitNetwork()
+            .build()
+        StrictMode.setThreadPolicy(threadPolicy)
     }
 }
