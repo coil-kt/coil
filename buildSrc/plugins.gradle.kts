d9ada3a545d2bcfecc8872bbfe68c402673c9c54
Colin White <colin@colinwhite.me>
2021-07-09 01:38:32 -0500

    Update Dokka (1.5.0).

--- buildSrc/plugins.gradle.kts
+++ buildSrc/plugins.gradle.kts
@@ -2,8 +2,8 @@ rootProject.extra.apply {
     set("androidPlugin", "com.android.tools.build:gradle:7.0.0-beta05")
     set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
     set("mavenPublishPlugin", "com.vanniktech:gradle-maven-publish-plugin:0.16.0")
-    set("dokkaPlugin", "org.jetbrains.dokka:dokka-gradle-plugin:1.4.32")
-    set("dokkaAndroidPlugin", "org.jetbrains.dokka:android-documentation-plugin:1.4.32")
+    set("dokkaPlugin", "org.jetbrains.dokka:dokka-gradle-plugin:1.5.0")
+    set("dokkaAndroidPlugin", "org.jetbrains.dokka:android-documentation-plugin:1.5.0")
     set("binaryCompatibilityPlugin", "org.jetbrains.kotlinx:binary-compatibility-validator:0.6.0")
     set("ktlintPlugin", "org.jlleitschuh.gradle:ktlint-gradle:10.1.0")
 }
