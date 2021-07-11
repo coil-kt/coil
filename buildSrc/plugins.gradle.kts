5a628639d58d5382fb91a8f6af22a008587ebb0b
Goooler <wangzongler@gmail.com>
2021-07-11 11:15:39 +0800

    Bump gradle-maven-publish-plugin to 0.17.0 (#798)
    * Update dependencies
    
    * Revert coroutines to 1.5.1

--- buildSrc/plugins.gradle.kts
+++ buildSrc/plugins.gradle.kts
@@ -1,7 +1,7 @@
 rootProject.extra.apply {
     set("androidPlugin", "com.android.tools.build:gradle:7.0.0-beta05")
     set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
-    set("mavenPublishPlugin", "com.vanniktech:gradle-maven-publish-plugin:0.16.0")
+    set("mavenPublishPlugin", "com.vanniktech:gradle-maven-publish-plugin:0.17.0")
     set("dokkaPlugin", "org.jetbrains.dokka:dokka-gradle-plugin:1.5.0")
     set("dokkaAndroidPlugin", "org.jetbrains.dokka:android-documentation-plugin:1.5.0")
     set("binaryCompatibilityPlugin", "org.jetbrains.kotlinx:binary-compatibility-validator:0.6.0")
