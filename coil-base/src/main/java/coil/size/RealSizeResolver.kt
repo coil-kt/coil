5fed10cea95c98bd4c3c8b9cc73f122d9818f9bd
Colin White <colin@colinwhite.me>
2021-07-22 23:28:52 -0500

    Consistent formatting.

--- coil-base/src/main/java/coil/size/RealSizeResolver.kt
+++ coil-base/src/main/java/coil/size/RealSizeResolver.kt
@@ -5,7 +5,8 @@ internal class RealSizeResolver(private val size: Size) : SizeResolver {
     override suspend fun size() = size
 
     override fun equals(other: Any?): Boolean {
-        return (this === other) || (other is RealSizeResolver && size == other.size)
+        if (this === other) return true
+        return other is RealSizeResolver && size == other.size
     }
 
     override fun hashCode() = size.hashCode()
