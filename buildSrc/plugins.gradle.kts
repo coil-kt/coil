ad68538dd4c5d192f79021932de46956d9ce03dc
Colin White <colin@colinwhite.me>
2021-08-04 14:46:17 -0500

    Update Compose (1.0.1), Kotlin (1.5.21), and androidx dependencies.

--- buildSrc/plugins.gradle.kts
+++ buildSrc/plugins.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.extra.apply {
     set("androidPlugin", "com.android.tools.build:gradle:7.0.0")
-    set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.10")
+    set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
     set("mavenPublishPlugin", "com.vanniktech:gradle-maven-publish-plugin:0.17.0")
     set("dokkaPlugin", "org.jetbrains.dokka:dokka-gradle-plugin:1.5.0")
     set("dokkaAndroidPlugin", "org.jetbrains.dokka:android-documentation-plugin:1.5.0")
