851fdbb991ceb64b553b80fa34efb4680bd588f3
Goooler <wangzongler@gmail.com>
2021-08-25 02:13:30 +0800

    Update dependencies (#866)

--- buildSrc/plugins.gradle.kts
+++ buildSrc/plugins.gradle.kts
@@ -1,9 +1,9 @@
 rootProject.extra.apply {
-    set("androidPlugin", "com.android.tools.build:gradle:7.0.0")
+    set("androidPlugin", "com.android.tools.build:gradle:7.0.1")
     set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.21")
     set("mavenPublishPlugin", "com.vanniktech:gradle-maven-publish-plugin:0.17.0")
     set("dokkaPlugin", "org.jetbrains.dokka:dokka-gradle-plugin:1.5.0")
     set("dokkaAndroidPlugin", "org.jetbrains.dokka:android-documentation-plugin:1.5.0")
-    set("binaryCompatibilityPlugin", "org.jetbrains.kotlinx:binary-compatibility-validator:0.6.0")
+    set("binaryCompatibilityPlugin", "org.jetbrains.kotlinx:binary-compatibility-validator:0.7.0")
     set("ktlintPlugin", "org.jlleitschuh.gradle:ktlint-gradle:10.1.0")
 }
