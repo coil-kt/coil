342c417a268d7952bad50ecfb17dba0f0a56c1cc
Colin White <colin@colinwhite.me>
2021-09-01 15:43:46 -0400

    Update dependencies.

--- buildSrc/src/main/kotlin/Library.kt
+++ buildSrc/src/main/kotlin/Library.kt
@@ -28,7 +28,7 @@ object Library {
     const val ANDROIDX_LIFECYCLE_RUNTIME = "androidx.lifecycle:lifecycle-runtime:$LIFECYCLE_VERSION"
     const val ANDROIDX_LIFECYCLE_VIEW_MODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:$LIFECYCLE_VERSION"
 
-    const val COMPOSE_VERSION = "1.0.1"
+    const val COMPOSE_VERSION = "1.0.2"
     const val COMPOSE_FOUNDATION = "androidx.compose.foundation:foundation:$COMPOSE_VERSION"
     const val COMPOSE_UI = "androidx.compose.ui:ui:$COMPOSE_VERSION"
     const val COMPOSE_UI_TEST_JUNIT = "androidx.compose.ui:ui-test-junit4:$COMPOSE_VERSION"
