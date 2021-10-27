3369dc289174c4a4dcd6ee3af965a603aef603e2
Colin White <colin@colinwhite.me>
2021-10-27 14:08:28 -0500

    Update AndroidX libraries.

--- buildSrc/src/main/kotlin/Library.kt
+++ buildSrc/src/main/kotlin/Library.kt
@@ -7,11 +7,11 @@ object Library {
     private const val COROUTINES_VERSION = "1.5.2"
     const val KOTLINX_COROUTINES_ANDROID = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$COROUTINES_VERSION"
 
-    const val ANDROIDX_ACTIVITY = "androidx.activity:activity-ktx:1.3.1"
+    const val ANDROIDX_ACTIVITY = "androidx.activity:activity-ktx:1.4.0"
     const val ANDROIDX_ANNOTATION = "androidx.annotation:annotation:1.2.0"
     const val ANDROIDX_COLLECTION = "androidx.collection:collection-ktx:1.1.0"
     const val ANDROIDX_CONSTRAINT_LAYOUT = "androidx.constraintlayout:constraintlayout:2.1.1"
-    const val ANDROIDX_CORE = "androidx.core:core-ktx:1.6.0"
+    const val ANDROIDX_CORE = "androidx.core:core-ktx:1.7.0"
     const val ANDROIDX_EXIF_INTERFACE = "androidx.exifinterface:exifinterface:1.3.3"
     const val ANDROIDX_RECYCLER_VIEW = "androidx.recyclerview:recyclerview:1.2.1"
     const val ANDROIDX_VECTOR_DRAWABLE_ANIMATED = "androidx.vectordrawable:vectordrawable-animated:1.1.0"
@@ -20,7 +20,7 @@ object Library {
     const val ANDROIDX_APPCOMPAT = "androidx.appcompat:appcompat:$APPCOMPAT_VERSION"
     const val ANDROIDX_APPCOMPAT_RESOURCES = "androidx.appcompat:appcompat-resources:$APPCOMPAT_VERSION"
 
-    private const val LIFECYCLE_VERSION = "2.3.1"
+    private const val LIFECYCLE_VERSION = "2.4.0"
     const val ANDROIDX_LIFECYCLE_COMMON = "androidx.lifecycle:lifecycle-common-java8:$LIFECYCLE_VERSION"
     const val ANDROIDX_LIFECYCLE_RUNTIME = "androidx.lifecycle:lifecycle-runtime:$LIFECYCLE_VERSION"
     const val ANDROIDX_LIFECYCLE_VIEW_MODEL = "androidx.lifecycle:lifecycle-viewmodel-ktx:$LIFECYCLE_VERSION"
