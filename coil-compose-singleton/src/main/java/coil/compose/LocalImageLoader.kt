45fcf34a54c191b169f818056a68458cd828cdc7
Colin White <colin@colinwhite.me>
2021-11-02 03:32:07 -0500

    Add overloads for the singleton ImageLoader.

--- coil-compose-singleton/src/main/java/coil/compose/LocalImageLoader.kt
+++ coil-compose-singleton/src/main/java/coil/compose/LocalImageLoader.kt
@@ -6,11 +6,72 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocal
 import androidx.compose.runtime.ProvidableCompositionLocal
 import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.DefaultAlpha
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import coil.ImageLoader
+import coil.annotation.ExperimentalCoilApi
 import coil.compose.ImagePainter.ExecuteCallback
+import coil.compose.ImagePainter.State
 import coil.imageLoader
 import coil.request.ImageRequest
+
+@ExperimentalCoilApi
+@Composable
+fun AsyncImage(
+    data: Any?,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    loading: @Composable ((State.Loading) -> Unit)? = null,
+    success: @Composable ((State.Success) -> Unit)? = null,
+    error: @Composable ((State.Error) -> Unit)? = null,
+    alignment: Alignment = Alignment.Center,
+    contentScale: ContentScale = ContentScale.Fit,
+    alpha: Float = DefaultAlpha,
+    colorFilter: ColorFilter? = null,
+) = AsyncImage(
+    data = data,
+    contentDescription = contentDescription,
+    imageLoader = LocalImageLoader.current,
+    modifier = modifier,
+    loading = loading,
+    success = success,
+    error = error,
+    alignment = alignment,
+    contentScale = contentScale,
+    alpha = alpha,
+    colorFilter = colorFilter,
+)
+
+@ExperimentalCoilApi
+@Composable
+fun AsyncImage(
+    request: ImageRequest,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    loading: @Composable ((State.Loading) -> Unit)? = null,
+    success: @Composable ((State.Success) -> Unit)? = null,
+    error: @Composable ((State.Error) -> Unit)? = null,
+    alignment: Alignment = Alignment.Center,
+    contentScale: ContentScale = ContentScale.Fit,
+    alpha: Float = DefaultAlpha,
+    colorFilter: ColorFilter? = null,
+) = AsyncImage(
+    request = request,
+    contentDescription = contentDescription,
+    imageLoader = LocalImageLoader.current,
+    modifier = modifier,
+    loading = loading,
+    success = success,
+    error = error,
+    alignment = alignment,
+    contentScale = contentScale,
+    alpha = alpha,
+    colorFilter = colorFilter,
+)
 
 /**
  * Return an [ImagePainter] that will execute an [ImageRequest] using [LocalImageLoader].
