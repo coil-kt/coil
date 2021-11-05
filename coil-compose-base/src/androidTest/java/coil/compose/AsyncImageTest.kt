0dee1e44709d78b3e2aa8e25b715f508e14cbeba
Colin White <colin@colinwhite.me>
2021-11-05 02:13:05 -0500

    Add basic AsyncImage test.

---
+++ coil-compose-base/src/androidTest/java/coil/compose/AsyncImageTest.kt
@@ -0,0 +1,122 @@
+package coil.compose
+
+import android.os.Build.VERSION.SDK_INT
+import androidx.activity.ComponentActivity
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.test.assertHeightIsEqualTo
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertWidthIsEqualTo
+import androidx.compose.ui.test.captureToImage
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.unit.dp
+import coil.ImageLoader
+import coil.compose.base.test.R
+import coil.compose.utils.ImageLoaderIdlingResource
+import coil.compose.utils.ImageMockWebServer
+import coil.compose.utils.assertIsSimilarTo
+import coil.request.CachePolicy
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.After
+import org.junit.Assume.assumeTrue
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class AsyncImageTest {
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<ComponentActivity>()
+
+    private lateinit var server: MockWebServer
+    private lateinit var requestTracker: ImageLoaderIdlingResource
+    private lateinit var imageLoader: ImageLoader
+
+    @Before
+    fun before() {
+        server = ImageMockWebServer()
+        requestTracker = ImageLoaderIdlingResource()
+        imageLoader = ImageLoader.Builder(composeTestRule.activity)
+            .diskCachePolicy(CachePolicy.DISABLED)
+            .memoryCachePolicy(CachePolicy.DISABLED)
+            .networkObserverEnabled(false)
+            .eventListener(requestTracker)
+            .build()
+        composeTestRule.registerIdlingResource(requestTracker)
+        server.start()
+    }
+
+    @After
+    fun after() {
+        composeTestRule.unregisterIdlingResource(requestTracker)
+        server.shutdown()
+    }
+
+    @Test
+    fun basicLoad_http_fixedSize() {
+        // captureToImage is SDK_INT >= 26.
+        assumeTrue(SDK_INT >= 26)
+
+        composeTestRule.setContent {
+            AsyncImage(
+                data = server.url("/image"),
+                contentDescription = null,
+                imageLoader = imageLoader,
+                modifier = Modifier
+                    .size(128.dp, 166.dp)
+                    .testTag(Image),
+            )
+        }
+
+        waitForRequestComplete()
+
+        composeTestRule.onNodeWithTag(Image)
+            .assertIsDisplayed()
+            .assertWidthIsEqualTo(128.dp)
+            .assertHeightIsEqualTo(166.dp)
+            .captureToImage()
+            .assertIsSimilarTo(R.drawable.sample)
+    }
+
+    @Test
+    fun basicLoad_http_dynamicHeight() {
+        // captureToImage is SDK_INT >= 26.
+        assumeTrue(SDK_INT >= 26)
+
+        composeTestRule.setContent {
+            AsyncImage(
+                data = server.url("/image"),
+                contentDescription = null,
+                imageLoader = imageLoader,
+                modifier = Modifier.testTag(Image),
+            )
+        }
+
+        waitForRequestComplete()
+
+        val displayMetrics = composeTestRule.activity.resources.displayMetrics
+        val expectedWidthPx = displayMetrics.widthPixels.coerceAtMost(1024)
+        val expectedHeightPx = displayMetrics.heightPixels.coerceAtMost(1326)
+
+        composeTestRule.onNodeWithTag(Image)
+            .assertIsDisplayed()
+            .assertWidthIsEqualTo((expectedWidthPx / displayMetrics.density).dp)
+            .assertHeightIsEqualTo((expectedHeightPx / displayMetrics.density).dp)
+            .captureToImage()
+            .assertIsSimilarTo(R.drawable.sample)
+    }
+
+    private fun waitForRequestComplete(requestNumber: Int = 1) {
+        composeTestRule.waitForIdle()
+        composeTestRule.waitUntil(10_000) {
+            requestTracker.finishedRequests >= requestNumber
+        }
+        composeTestRule.waitForIdle()
+    }
+
+    companion object {
+        private const val Image = "image"
+    }
+}
