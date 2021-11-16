23629fb9b9e6af46aa1c0888c27761561a149847
Colin White <colin@colinwhite.me>
2021-11-15 23:23:38 -0600

    Fix imports.

--- coil-compose-base/src/main/java/coil/compose/AsyncImage.kt
+++ coil-compose-base/src/main/java/coil/compose/AsyncImage.kt
@@ -3,9 +3,6 @@ package coil.compose
 import android.content.Context
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
