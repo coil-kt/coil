64ccbadefa37fb547a9f24c9d12eb55401a5dead
Colin White <colin@colinwhite.me>
2021-11-16 04:42:34 -0600

    Tweak comment.

--- coil-compose-base/src/main/java/coil/compose/AsyncImage.kt
+++ coil-compose-base/src/main/java/coil/compose/AsyncImage.kt
@@ -87,7 +87,7 @@ fun AsyncImage(
             is State.Loading -> if (loading != null) loading(state).also { draw = false }
             is State.Success -> if (success != null) success(state).also { draw = false }
             is State.Error -> if (error != null) error(state).also { draw = false }
-            is State.Empty -> { /* This shouldn't happen. */ }
+            is State.Empty -> {} // This shouldn't happen.
         }
 
         // Draw the image.
