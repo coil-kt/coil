9cd4d432abc20db7437cb097a736a8343e30caa3
Colin White <colin@colinwhite.me>
2021-11-19 18:50:59 -0600

    Support overwriting AsyncImage's content. (#996)
    * Support overriding the AsyncImage's content.
    
    * Constrain.
    
    * Docs.
    
    * Still draw if empty.
    
    * Docs.
    
    * Move content to last parameter.
    
    * Change painter type.
    
    * Ignore.
    
    * Move DefaultContent to a companion object.
    
    * Docs.
    
    * Rename.
    
    * Fix size.
    
    * Add test.
    
    * Fix order.
    
    * Docs.

--- coil-compose-singleton/src/main/java/coil/compose/AsyncImage.kt
+++ coil-compose-singleton/src/main/java/coil/compose/AsyncImage.kt
@@ -3,7 +3,6 @@
 
 package coil.compose
 
-import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -13,6 +12,7 @@ import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.graphics.drawscope.DrawScope.Companion.DefaultFilterQuality
 import androidx.compose.ui.layout.ContentScale
 import coil.compose.AsyncImagePainter.State
+import coil.compose.AsyncImageScope.Companion.DefaultContent
 import coil.request.ImageRequest
 
 /**
@@ -42,9 +42,9 @@ fun AsyncImage(
     model: Any?,
     contentDescription: String?,
     modifier: Modifier = Modifier,
-    loading: @Composable (BoxScope.(State.Loading) -> Unit)? = null,
-    success: @Composable (BoxScope.(State.Success) -> Unit)? = null,
-    error: @Composable (BoxScope.(State.Error) -> Unit)? = null,
+    loading: @Composable (AsyncImageScope.(State.Loading) -> Unit)? = null,
+    success: @Composable (AsyncImageScope.(State.Success) -> Unit)? = null,
+    error: @Composable (AsyncImageScope.(State.Error) -> Unit)? = null,
     alignment: Alignment = Alignment.Center,
     contentScale: ContentScale = ContentScale.Fit,
     alpha: Float = DefaultAlpha,
@@ -62,5 +62,49 @@ fun AsyncImage(
     contentScale = contentScale,
     alpha = alpha,
     colorFilter = colorFilter,
-    filterQuality = filterQuality
+    filterQuality = filterQuality,
+)
+
+/**
+ * A composable that executes an [ImageRequest] asynchronously and renders the result.
+ *
+ * @param model Either an [ImageRequest] or the [ImageRequest.data] value.
+ * @param contentDescription Text used by accessibility services to describe what this image
+ *  represents. This should always be provided unless this image is used for decorative purposes,
+ *  and does not represent a meaningful action that a user can take.
+ * @param modifier Modifier used to adjust the layout algorithm or draw decoration content.
+ * @param alignment Optional alignment parameter used to place the [AsyncImagePainter] in the given
+ *  bounds defined by the width and height.
+ * @param contentScale Optional scale parameter used to determine the aspect ratio scaling to be
+ *  used if the bounds are a different size from the intrinsic size of the [AsyncImagePainter].
+ * @param alpha Optional opacity to be applied to the [AsyncImagePainter] when it is rendered
+ *  onscreen.
+ * @param colorFilter Optional [ColorFilter] to apply for the [AsyncImagePainter] when it is
+ *  rendered onscreen.
+ * @param filterQuality Sampling algorithm applied to a bitmap when it is scaled and drawn
+ *  into the destination.
+ * @param content A callback to draw the content for the current [AsyncImagePainter.State].
+ */
+@Composable
+fun AsyncImage(
+    model: Any?,
+    contentDescription: String?,
+    modifier: Modifier = Modifier,
+    alignment: Alignment = Alignment.Center,
+    contentScale: ContentScale = ContentScale.Fit,
+    alpha: Float = DefaultAlpha,
+    colorFilter: ColorFilter? = null,
+    filterQuality: FilterQuality = DefaultFilterQuality,
+    content: @Composable (AsyncImageScope.(State) -> Unit) = DefaultContent,
+) = AsyncImage(
+    model = model,
+    contentDescription = contentDescription,
+    imageLoader = LocalImageLoader.current,
+    modifier = modifier,
+    alignment = alignment,
+    contentScale = contentScale,
+    alpha = alpha,
+    colorFilter = colorFilter,
+    filterQuality = filterQuality,
+    content = content,
 )
