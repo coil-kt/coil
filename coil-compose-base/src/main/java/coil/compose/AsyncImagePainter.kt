de997bc59cc58a7298f26347105d4b79bf8a3d1f
Colin White <colin@colinwhite.me>
2021-11-22 12:51:36 -0600

    Move deprecated ImagePainter functions to a separate file. (#1001)
    * Move deprecated ImagePainter functions to a separate file.
    
    * Rename compose-singleton files.
    
    * Remove warnings.
    
    * Docs.
    
    * Add metadata migration aid.
    
    * Fix import.
    
    * Docs.

--- coil-compose-base/src/main/java/coil/compose/AsyncImagePainter.kt
+++ coil-compose-base/src/main/java/coil/compose/AsyncImagePainter.kt
@@ -1,4 +1,4 @@
-@file:Suppress("ComposableNaming", "DEPRECATION_ERROR", "unused", "UNUSED_PARAMETER")
+@file:Suppress("ComposableNaming", "unused")
 
 package coil.compose
 
@@ -32,7 +32,6 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalInspectionMode
 import coil.ImageLoader
-import coil.compose.AsyncImagePainter.ExecuteCallback
 import coil.compose.AsyncImagePainter.State
 import coil.decode.DataSource
 import coil.request.ErrorResult
@@ -221,33 +220,6 @@ class AsyncImagePainter internal constructor(
             .first()
     }
 
-    @Deprecated(
-        message = "ExecuteCallback no longer has any effect. To skip waiting for size resolution " +
-            "use ImageRequest.Builder.size(OriginalSize).",
-        level = DeprecationLevel.ERROR
-    )
-    fun interface ExecuteCallback {
-
-        operator fun invoke(previous: Snapshot?, current: Snapshot): Boolean
-
-        companion object {
-            @JvmField val Default = ExecuteCallback { previous, current ->
-                previous?.request != current.request
-            }
-        }
-    }
-
-    @Deprecated(
-        message = "ExecuteCallback no longer has any effect. To skip waiting for size resolution " +
-            "use ImageRequest.Builder.size(OriginalSize).",
-        level = DeprecationLevel.ERROR
-    )
-    data class Snapshot(
-        val state: State,
-        val request: ImageRequest,
-        val size: Size,
-    )
-
     /**
      * The current state of the [AsyncImagePainter].
      */
@@ -354,7 +326,7 @@ private val Size.isPositive get() = width >= 0.5 && height >= 0.5
 /** A simple mutable value holder that avoids recomposition. */
 private class ValueHolder<T>(@JvmField var value: T)
 
-/** Create an [ImageRequest] using the [model]. */
+/** Create an [ImageRequest] from the [model]. */
 @Composable
 @ReadOnlyComposable
 internal fun requestOf(model: Any?): ImageRequest {
@@ -364,53 +336,3 @@ internal fun requestOf(model: Any?): ImageRequest {
         return ImageRequest.Builder(LocalContext.current).data(model).build()
     }
 }
-
-@Deprecated(
-    message = "ImagePainter has been renamed to AsyncImagePainter.",
-    replaceWith = ReplaceWith(
-        expression = "AsyncImagePainter",
-        imports = ["coil.compose.AsyncImagePainter"]
-    )
-)
-typealias ImagePainter = AsyncImagePainter
-
-@Deprecated(
-    message = "ImagePainter has been renamed to AsyncImagePainter.",
-    replaceWith = ReplaceWith(
-        expression = "rememberAsyncImagePainter(" +
-            "ImageRequest.Builder(LocalContext.current).data(data).apply(builder).build(), " +
-            "imageLoader)",
-        imports = [
-            "androidx.compose.ui.platform.LocalContext",
-            "coil.compose.rememberAsyncImagePainter",
-            "coil.request.ImageRequest",
-        ]
-    )
-)
-@Composable
-inline fun rememberImagePainter(
-    data: Any?,
-    imageLoader: ImageLoader,
-    onExecute: ExecuteCallback = ExecuteCallback.Default,
-    builder: ImageRequest.Builder.() -> Unit = {},
-) = rememberAsyncImagePainter(
-    model = ImageRequest.Builder(LocalContext.current).data(data).apply(builder).build(),
-    imageLoader = imageLoader
-)
-
-@Deprecated(
-    message = "ImagePainter has been renamed to AsyncImagePainter.",
-    replaceWith = ReplaceWith(
-        expression = "rememberAsyncImagePainter(request, imageLoader)",
-        imports = ["coil.compose.rememberAsyncImagePainter"]
-    )
-)
-@Composable
-fun rememberImagePainter(
-    request: ImageRequest,
-    imageLoader: ImageLoader,
-    onExecute: ExecuteCallback = ExecuteCallback.Default
-) = rememberAsyncImagePainter(
-    model = request,
-    imageLoader = imageLoader
-)
---
+++ coil-compose-singleton/src/main/java/coil/compose/SingletonAsyncImagePainter.kt
@@ -0,0 +1,29 @@
+@file:Suppress("unused")
+
+package coil.compose
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.FilterQuality
+import androidx.compose.ui.graphics.drawscope.DrawScope.Companion.DefaultFilterQuality
+import androidx.compose.ui.graphics.painter.Painter
+import coil.request.ImageRequest
+
+/**
+ * Return an [AsyncImagePainter] that executes an [ImageRequest] asynchronously and
+ * renders the result.
+ *
+ * This is a lower-level API than [AsyncImage] and may not work as expected in all situations.
+ * Notably, it will not finish loading if [AsyncImagePainter.onDraw] is not called, which can occur
+ * for composables that don't have a fixed size (e.g. [LazyColumn]). It's recommended to use
+ * [AsyncImage] unless you need a reference to a [Painter].
+ *
+ * @param model Either an [ImageRequest] or the [ImageRequest.data] value.
+ * @param filterQuality Sampling algorithm applied to a bitmap when it is scaled and drawn
+ *  into the destination.
+ */
+@Composable
+fun rememberAsyncImagePainter(
+    model: Any?,
+    filterQuality: FilterQuality = DefaultFilterQuality,
+): AsyncImagePainter = rememberAsyncImagePainter(model, LocalImageLoader.current, filterQuality)
