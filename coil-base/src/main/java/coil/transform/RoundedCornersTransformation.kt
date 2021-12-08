ed7082c4a2f1749bcb9bf573e815239e74f20d57
Colin White <colin@colinwhite.me>
2021-12-07 19:09:57 -0600

    Encourage using clip instead of transformations in Jetpack Compose.

--- coil-base/src/main/java/coil/transform/RoundedCornersTransformation.kt
+++ coil-base/src/main/java/coil/transform/RoundedCornersTransformation.kt
@@ -22,8 +22,11 @@ import coil.util.safeConfig
 import kotlin.math.roundToInt
 
 /**
- * A [Transformation] that crops the image to fit the target's dimensions and rounds
- * the corners of the image.
+ * A [Transformation] that crops the image to fit the target's dimensions and rounds the corners of
+ * the image.
+ *
+ * If you're using Jetpack Compose, use `Modifier.clip(RoundedCornerShape(10.dp))` instead of this
+ * [Transformation] as it's more efficient.
  *
  * @param topLeft The radius for the top left corner.
  * @param topRight The radius for the top right corner.
