59bdcb072c6d8ff5f9de7cb35160c18b79db22cb
Colin White <colin@colinwhite.me>
2021-12-15 02:40:31 -0500

    Docs.

--- coil-compose-base/src/main/java/coil/compose/AsyncImage.kt
+++ coil-compose-base/src/main/java/coil/compose/AsyncImage.kt
@@ -132,7 +132,7 @@ fun AsyncImage(
 
     val sizeResolver = request.sizeResolver
     if (content === DefaultContent) {
-        // Fastest path: draw the content without parent composables or subcomposition.
+        // Fastest path: draw the content without a parent composable or subcomposition.
         InternalAsyncImageContent(
             modifier = if (sizeResolver is ConstraintsSizeResolver) {
                 modifier.then(sizeResolver)
