9866c7e1e22c9788a55223a81bae2c87e90b4d33
Colin White <colin@colinwhite.me>
2021-12-15 21:15:28 -0600

    Update dependencies. (#1039)

--- buildSrc/plugins.gradle.kts
+++ buildSrc/plugins.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.extra.apply {
-    set("androidPlugin", "com.android.tools.build:gradle:7.0.3")
+    set("androidPlugin", "com.android.tools.build:gradle:7.1.0-beta05")
     set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
     set("mavenPublishPlugin", "com.vanniktech:gradle-maven-publish-plugin:0.18.0")
     set("dokkaPlugin", "org.jetbrains.dokka:dokka-gradle-plugin:1.6.0")
