a1f0d83ce52b4a10ea73c46ef7ada65fc6e1d5a5
Colin White <colin@colinwhite.me>
2021-12-16 14:09:48 -0600

    Update Kotlin (1.6.10). (#1042)

--- buildSrc/plugins.gradle.kts
+++ buildSrc/plugins.gradle.kts
@@ -1,6 +1,6 @@
 rootProject.extra.apply {
     set("androidPlugin", "com.android.tools.build:gradle:7.1.0-beta05")
-    set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.0")
+    set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     set("dokkaPlugin", "org.jetbrains.dokka:dokka-gradle-plugin:1.6.0")
     set("dokkaAndroidPlugin", "org.jetbrains.dokka:android-documentation-plugin:1.6.0")
     set("mavenPublishPlugin", "com.vanniktech:gradle-maven-publish-plugin:0.18.0")
