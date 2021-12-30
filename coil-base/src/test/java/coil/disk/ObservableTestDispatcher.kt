9040912c619f6c8e9394e188bd8cfbe28d1fc37c
Colin White <colin@colinwhite.me>
2021-12-30 01:32:09 -0500

    Fixes.

--- coil-base/src/test/java/coil/disk/ObservableTestDispatcher.kt
+++ coil-base/src/test/java/coil/disk/ObservableTestDispatcher.kt
@@ -3,38 +3,29 @@ package coil.disk
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Runnable
-import kotlinx.coroutines.test.TestDispatcher
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import java.util.concurrent.ConcurrentLinkedQueue
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.coroutines.CoroutineContext
 
 @OptIn(ExperimentalCoroutinesApi::class)
-class ObservableTestDispatcher(
-    private val delegate: TestDispatcher = UnconfinedTestDispatcher()
-) : CoroutineDispatcher() {
+class ObservableTestDispatcher : CoroutineDispatcher() {
 
-    private val tasks = ArrayDeque<Pair<CoroutineContext, Runnable>>()
+    private val tasks = ConcurrentLinkedQueue<Runnable>()
     private val inProgressTasks = AtomicInteger()
 
-    fun isIdle() = sync { tasks.isEmpty() } && inProgressTasks.get() == 0
+    fun isIdle() = tasks.isEmpty() && inProgressTasks.get() == 0
 
     fun runNextTask() {
-        inProgressTasks.getAndIncrement()
-        val (context, block) = sync { tasks.removeFirst() }
-        delegate.dispatch(context) {
-            try {
-                block.run()
-            } finally {
-                inProgressTasks.getAndDecrement()
-            }
+        val block = tasks.remove()
+        try {
+            inProgressTasks.getAndIncrement()
+            block.run()
+        } finally {
+            inProgressTasks.getAndDecrement()
         }
     }
 
-    override fun isDispatchNeeded(context: CoroutineContext) = delegate.isDispatchNeeded(context)
-
     override fun dispatch(context: CoroutineContext, block: Runnable) {
-        sync { tasks += context to block }
+        tasks += block
     }
-
-    private inline fun <T> sync(block: () -> T): T = synchronized(this) { block() }
 }
