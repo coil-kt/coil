4e72b621f706a42420366460afdf0b915a68270d
Colin White <colin@colinwhite.me>
2022-01-05 13:47:20 -0500

    Update plugins. (#1075)
    * Update plugins.
    
    * Update Gradle.

--- buildSrc/plugins.gradle.kts
+++ buildSrc/plugins.gradle.kts
@@ -1,5 +1,5 @@
 rootProject.extra.apply {
-    set("androidPlugin", "com.android.tools.build:gradle:7.1.0-beta05")
+    set("androidPlugin", "com.android.tools.build:gradle:7.1.0-rc01")
     set("kotlinPlugin", "org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.10")
     set("dokkaPlugin", "org.jetbrains.dokka:dokka-gradle-plugin:1.6.10")
     set("mavenPublishPlugin", "com.vanniktech:gradle-maven-publish-plugin:0.18.0")
