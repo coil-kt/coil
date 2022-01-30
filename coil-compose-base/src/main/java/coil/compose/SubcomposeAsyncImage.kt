b241bbc891f918f3461b084a70e5ba493a88bec4
Colin White <colin@colinwhite.me>
2022-01-29 22:38:20 -0600

    Move SubcomposeAsyncImage into a separate file. (#1118)

--- coil-compose-base/src/main/java/coil/compose/SubcomposeAsyncImage.kt
+++ coil-compose-base/src/main/java/coil/compose/SubcomposeAsyncImage.kt
@@ -1,38 +1,24 @@
 package coil.compose
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.LayoutScopeMarker
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.Stable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.DefaultAlpha
 import androidx.compose.ui.graphics.FilterQuality
 import androidx.compose.ui.graphics.drawscope.DrawScope.Companion.DefaultFilterQuality
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.layout.Layout
-import androidx.compose.ui.layout.LayoutModifier
-import androidx.compose.ui.layout.Measurable
-import androidx.compose.ui.layout.MeasureResult
-import androidx.compose.ui.layout.MeasureScope
-import androidx.compose.ui.semantics.Role
-import androidx.compose.ui.semantics.contentDescription
-import androidx.compose.ui.semantics.role
-import androidx.compose.ui.semantics.semantics
-import androidx.compose.ui.unit.Constraints
 import coil.ImageLoader
 import coil.compose.AsyncImagePainter.Companion.DefaultTransform
 import coil.compose.AsyncImagePainter.State
 import coil.request.ImageRequest
-import coil.size.Dimension
-import coil.size.Scale
-import coil.size.SizeResolver
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.mapNotNull
-import coil.size.Size as CoilSize
 
 /**
  * A composable that executes an [ImageRequest] asynchronously and renders the result.
@@ -43,9 +29,9 @@ import coil.size.Size as CoilSize
  *  and does not represent a meaningful action that a user can take.
  * @param imageLoader The [ImageLoader] that will be used to execute the request.
  * @param modifier Modifier used to adjust the layout algorithm or draw decoration content.
- * @param placeholder A [Painter] that is displayed while the image is loading.
- * @param error A [Painter] that is displayed when the image request is unsuccessful.
- * @param fallback A [Painter] that is displayed when the request's [ImageRequest.data] is null.
+ * @param loading An optional callback to overwrite what's drawn while the image request is loading.
+ * @param success An optional callback to overwrite what's drawn when the image request succeeds.
+ * @param error An optional callback to overwrite what's drawn when the image request fails.
  * @param onLoading Called when the image request begins loading.
  * @param onSuccess Called when the image request completes successfully.
  * @param onError Called when the image request completes unsuccessfully.
@@ -61,14 +47,14 @@ import coil.size.Size as CoilSize
  *  into the destination.
  */
 @Composable
-fun AsyncImage(
+fun SubcomposeAsyncImage(
     model: Any?,
     contentDescription: String?,
     imageLoader: ImageLoader,
     modifier: Modifier = Modifier,
-    placeholder: Painter? = null,
-    error: Painter? = null,
-    fallback: Painter? = null,
+    loading: @Composable (SubcomposeAsyncImageScope.(State.Loading) -> Unit)? = null,
+    success: @Composable (SubcomposeAsyncImageScope.(State.Success) -> Unit)? = null,
+    error: @Composable (SubcomposeAsyncImageScope.(State.Error) -> Unit)? = null,
     onLoading: ((State.Loading) -> Unit)? = null,
     onSuccess: ((State.Success) -> Unit)? = null,
     onError: ((State.Error) -> Unit)? = null,
@@ -77,18 +63,18 @@ fun AsyncImage(
     alpha: Float = DefaultAlpha,
     colorFilter: ColorFilter? = null,
     filterQuality: FilterQuality = DefaultFilterQuality,
-) = AsyncImage(
+) = SubcomposeAsyncImage(
     model = model,
     contentDescription = contentDescription,
     imageLoader = imageLoader,
     modifier = modifier,
-    transform = transformOf(placeholder, error, fallback),
     onState = onStateOf(onLoading, onSuccess, onError),
     alignment = alignment,
     contentScale = contentScale,
     alpha = alpha,
     colorFilter = colorFilter,
-    filterQuality = filterQuality
+    filterQuality = filterQuality,
+    content = contentOf(loading, success, error)
 )
 
 /**
@@ -113,9 +99,10 @@ fun AsyncImage(
  *  rendered onscreen.
  * @param filterQuality Sampling algorithm applied to a bitmap when it is scaled and drawn
  *  into the destination.
+ * @param content A callback to draw the content inside an [SubcomposeAsyncImageScope].
  */
 @Composable
-fun AsyncImage(
+fun SubcomposeAsyncImage(
     model: Any?,
     contentDescription: String?,
     imageLoader: ImageLoader,
@@ -127,120 +114,135 @@ fun AsyncImage(
     alpha: Float = DefaultAlpha,
     colorFilter: ColorFilter? = null,
     filterQuality: FilterQuality = DefaultFilterQuality,
+    content: @Composable SubcomposeAsyncImageScope.() -> Unit,
 ) {
     // Create and execute the image request.
     val request = updateRequest(requestOf(model), contentScale)
     val painter = rememberAsyncImagePainter(request, imageLoader, transform, onState, filterQuality)
 
-    // Draw the content without a parent composable or subcomposition.
     val sizeResolver = request.sizeResolver
-    Content(
-        modifier = if (sizeResolver is ConstraintsSizeResolver) {
-            modifier.then(sizeResolver)
-        } else {
-            modifier
-        },
-        painter = painter,
-        contentDescription = contentDescription,
-        alignment = alignment,
-        contentScale = contentScale,
-        alpha = alpha,
-        colorFilter = colorFilter
-    )
-}
-
-/** Draws the current content without an [SubcomposeAsyncImageScope]. */
-@Composable
-internal fun Content(
-    modifier: Modifier,
-    painter: Painter,
-    contentDescription: String?,
-    alignment: Alignment,
-    contentScale: ContentScale,
-    alpha: Float,
-    colorFilter: ColorFilter?,
-) = Layout(
-    modifier = modifier
-        .contentDescription(contentDescription)
-        .clipToBounds()
-        .then(
-            ContentPainterModifier(
+    if (sizeResolver !is ConstraintsSizeResolver) {
+        // Fast path: draw the content without subcomposition as we've got a custom `SizeResolver`.
+        Box(
+            modifier = modifier,
+            contentAlignment = alignment,
+            propagateMinConstraints = true
+        ) {
+            RealSubcomposeAsyncImageScope(
+                parentScope = this,
                 painter = painter,
+                contentDescription = contentDescription,
                 alignment = alignment,
                 contentScale = contentScale,
                 alpha = alpha,
                 colorFilter = colorFilter
-            )
-        ),
-    measurePolicy = { _, constraints ->
-        layout(constraints.minWidth, constraints.minHeight) {}
-    }
-)
-
-@Composable
-internal fun updateRequest(
-    request: ImageRequest,
-    contentScale: ContentScale,
-) = request.newBuilder()
-    .apply {
-        if (request.defined.sizeResolver == null) {
-            size(remember { ConstraintsSizeResolver() })
-        }
-        if (request.defined.scale == null) {
-            scale(contentScale.toScale())
-        }
-    }
-    .build()
-
-/** A [SizeResolver] that computes the size from the constrains passed during the layout phase. */
-internal class ConstraintsSizeResolver : SizeResolver, LayoutModifier {
-
-    private val _constraints = MutableStateFlow(ZeroConstraints)
-
-    override suspend fun size() = _constraints.mapNotNull { it.toSizeOrNull() }.first()
-
-    override fun MeasureScope.measure(
-        measurable: Measurable,
-        constraints: Constraints
-    ): MeasureResult {
-        // Cache the current constraints.
-        _constraints.value = constraints
-
-        // Measure and layout the content.
-        val placeable = measurable.measure(constraints)
-        return layout(placeable.width, placeable.height) {
-            placeable.place(0, 0)
-        }
-    }
-
-    fun setConstraints(constraints: Constraints) {
-        _constraints.value = constraints
-    }
-}
-
-@Stable
-private fun Modifier.contentDescription(contentDescription: String?): Modifier {
-    return if (contentDescription != null) {
-        semantics {
-            this.contentDescription = contentDescription
-            this.role = Role.Image
+            ).content()
         }
     } else {
-        this
+        // Slow path: draw the content with subcomposition as we need to resolve the size before
+        // calling `content`.
+        BoxWithConstraints(
+            modifier = modifier,
+            contentAlignment = alignment,
+            propagateMinConstraints = true
+        ) {
+            // Ensure `painter.state` is up to date immediately. Resolving the constraints
+            // synchronously is necessary to ensure that images from the memory cache are resolved
+            // and `painter.state` is updated to `Success` before invoking `content`.
+            sizeResolver.setConstraints(constraints)
+
+            RealSubcomposeAsyncImageScope(
+                parentScope = this,
+                painter = painter,
+                contentDescription = contentDescription,
+                alignment = alignment,
+                contentScale = contentScale,
+                alpha = alpha,
+                colorFilter = colorFilter
+            ).content()
+        }
     }
 }
 
-@Stable
-private fun ContentScale.toScale() = when (this) {
-    ContentScale.Fit, ContentScale.Inside, ContentScale.None -> Scale.FIT
-    else -> Scale.FILL
+/**
+ * A scope for the children of [SubcomposeAsyncImage].
+ */
+@LayoutScopeMarker
+@Immutable
+interface SubcomposeAsyncImageScope : BoxScope {
+
+    /** The painter that is drawn by [SubcomposeAsyncImageContent]. */
+    val painter: AsyncImagePainter
+
+    /** The content description for [SubcomposeAsyncImageContent]. */
+    val contentDescription: String?
+
+    /** The default alignment for any composables drawn in this scope. */
+    val alignment: Alignment
+
+    /** The content scale for [SubcomposeAsyncImageContent]. */
+    val contentScale: ContentScale
+
+    /** The alpha for [SubcomposeAsyncImageContent]. */
+    val alpha: Float
+
+    /** The color filter for [SubcomposeAsyncImageContent]. */
+    val colorFilter: ColorFilter?
 }
 
+/**
+ * A composable that draws [SubcomposeAsyncImage]'s content with [SubcomposeAsyncImageScope]'s
+ * properties.
+ *
+ * @see SubcomposeAsyncImageScope
+ */
+@Composable
+fun SubcomposeAsyncImageScope.SubcomposeAsyncImageContent(
+    modifier: Modifier = Modifier,
+    painter: Painter = this.painter,
+    contentDescription: String? = this.contentDescription,
+    alignment: Alignment = this.alignment,
+    contentScale: ContentScale = this.contentScale,
+    alpha: Float = this.alpha,
+    colorFilter: ColorFilter? = this.colorFilter,
+) = Content(
+    modifier = modifier,
+    painter = painter,
+    contentDescription = contentDescription,
+    alignment = alignment,
+    contentScale = contentScale,
+    alpha = alpha,
+    colorFilter = colorFilter
+)
+
 @Stable
-private fun Constraints.toSizeOrNull() = when {
-    isZero -> null
-    else -> CoilSize(
-        width = if (hasBoundedWidth) Dimension(maxWidth) else Dimension.Original,
-        height = if (hasBoundedHeight) Dimension(maxHeight) else Dimension.Original
-    )
+private fun contentOf(
+    loading: @Composable (SubcomposeAsyncImageScope.(State.Loading) -> Unit)?,
+    success: @Composable (SubcomposeAsyncImageScope.(State.Success) -> Unit)?,
+    error: @Composable (SubcomposeAsyncImageScope.(State.Error) -> Unit)?,
+): @Composable SubcomposeAsyncImageScope.() -> Unit {
+    return if (loading != null || success != null || error != null) {
+        {
+            var draw = true
+            when (val state = painter.state) {
+                is State.Loading -> if (loading != null) loading(state).also { draw = false }
+                is State.Success -> if (success != null) success(state).also { draw = false }
+                is State.Error -> if (error != null) error(state).also { draw = false }
+                is State.Empty -> {} // Skipped if rendering on the main thread.
+            }
+            if (draw) SubcomposeAsyncImageContent()
+        }
+    } else {
+        { SubcomposeAsyncImageContent() }
+    }
 }
+
+private data class RealSubcomposeAsyncImageScope(
+    private val parentScope: BoxScope,
+    override val painter: AsyncImagePainter,
+    override val contentDescription: String?,
+    override val alignment: Alignment,
+    override val contentScale: ContentScale,
+    override val alpha: Float,
+    override val colorFilter: ColorFilter?,
+) : SubcomposeAsyncImageScope, BoxScope by parentScope
