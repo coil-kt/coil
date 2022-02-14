6eaa985118376c8aff10f389e943c021c4c55282
Colin White <colin@colinwhite.me>
2022-02-13 21:43:29 -0600

    Docs.

--- coil-base/src/main/java/coil/size/ScaleResolver.kt
+++ coil-base/src/main/java/coil/size/ScaleResolver.kt
@@ -12,13 +12,13 @@ import coil.request.ImageRequest
 fun ScaleResolver(scale: Scale): ScaleResolver = RealScaleResolver(scale)
 
 /**
- * An interface for measuring the target scale for an image request.
+ * An interface for determining the [Scale] for an image request.
  *
  * @see ImageRequest.Builder.scale
  */
 fun interface ScaleResolver {
 
-    /** Return the [Scale] that the image should be loaded with. */
+    /** Return the resolved [Scale]. */
     @MainThread
     suspend fun scale(): Scale
 }
