743a666acd5e17c814500a70afbaba4f5fe57337
Colin White <colin@colinwhite.me>
2022-02-15 20:15:51 -0600

    Remove ExperimentalCoilApi from ScaleResolver.

--- coil-base/src/main/java/coil/size/ScaleResolver.kt
+++ coil-base/src/main/java/coil/size/ScaleResolver.kt
@@ -3,22 +3,19 @@
 package coil.size
 
 import androidx.annotation.MainThread
-import coil.annotation.ExperimentalCoilApi
 import coil.request.ImageRequest
 
 /**
  * Create a [ScaleResolver] with a fixed [scale].
  */
-@ExperimentalCoilApi
 @JvmName("create")
 fun ScaleResolver(scale: Scale): ScaleResolver = RealScaleResolver(scale)
 
 /**
- * An interface for determining the [Scale] for an image request.
+ * An interface for lazily determining the [Scale] for an image request.
  *
  * @see ImageRequest.Builder.scale
  */
-@ExperimentalCoilApi
 fun interface ScaleResolver {
 
     /** Return the resolved [Scale]. */
