c1adab8123e24a44c33c8a5c166397f0e861cf43
Colin White <colin@colinwhite.me>
2022-05-11 16:27:35 -0400

    Support setting custom CSS rules for SVG. (#1210)
    * Support setting custom CSS rules for SVG.
    
    * Update API.
    
    * Remove.

---
+++ coil-svg/src/main/java/coil/request/Svgs.kt
@@ -0,0 +1,19 @@
+@file:Suppress("UNCHECKED_CAST")
+@file:JvmName("Svgs")
+
+package coil.request
+
+import coil.decode.SvgDecoder.Companion.CSS_KEY
+
+/**
+ * Specifies additional CSS rules that will be applied when rendering an SVG in addition to any
+ * rules specified in the SVG itself.
+ */
+fun ImageRequest.Builder.css(css: String): ImageRequest.Builder {
+    return setParameter(CSS_KEY, css)
+}
+
+/**
+ * Get the additional CSS rules.
+ */
+fun Parameters.css(): String? = value(CSS_KEY)
