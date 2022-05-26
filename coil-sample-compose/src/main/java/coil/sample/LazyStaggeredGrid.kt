f21c22d17656d9e5985011c9cfd67c73d13a5fd6
Colin White <colin@colinwhite.me>
2022-05-26 04:24:47 -0400

    Inline items in coil-sample-compose. (#1292)

--- coil-sample-compose/src/main/java/coil/sample/LazyStaggeredGrid.kt
+++ coil-sample-compose/src/main/java/coil/sample/LazyStaggeredGrid.kt
@@ -35,7 +35,6 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 /** Modified from: https://github.com/savvasdalkitsis/lazy-staggered-grid */
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun LazyStaggeredGrid(
     columnCount: Int,
@@ -66,6 +65,7 @@ fun LazyStaggeredGrid(
     val gridScope = RealLazyStaggeredGridScope(columnCount).apply(content)
 
     // Disable overscroll otherwise it'll only overscroll one column.
+    @OptIn(ExperimentalFoundationApi::class)
     CompositionLocalProvider(LocalOverScrollConfiguration provides null) {
         Row {
             for (index in 0 until columnCount) {
@@ -76,7 +76,7 @@ fun LazyStaggeredGrid(
                         .nestedScroll(scrollConnections[index])
                         .weight(1f)
                 ) {
-                    for ((key, itemContent) in gridScope.items[index]) {
+                    for ((key, itemContent) in gridScope.items(index)) {
                         item(key) { itemContent() }
                     }
                 }
@@ -93,39 +93,58 @@ interface LazyStaggeredGridScope {
         key: Any? = null,
         content: @Composable () -> Unit
     )
+}
 
-    /** Adds a [count] of items. */
-    fun items(
-        count: Int,
-        key: ((index: Int) -> Any)? = null,
-        itemContent: @Composable (index: Int) -> Unit
-    )
+/** Adds a [count] of items. */
+inline fun LazyStaggeredGridScope.items(
+    count: Int,
+    crossinline itemContent: @Composable (index: Int) -> Unit
+) {
+    for (index in 0 until count) {
+        item { itemContent(index) }
+    }
+}
+
+/** Adds a [count] of items. */
+inline fun LazyStaggeredGridScope.items(
+    count: Int,
+    key: (index: Int) -> Any,
+    crossinline itemContent: @Composable (index: Int) -> Unit
+) {
+    for (index in 0 until count) {
+        item(key(index)) { itemContent(index) }
+    }
 }
 
 /** Adds a list of items. */
 inline fun <T> LazyStaggeredGridScope.items(
     items: List<T>,
-    noinline key: ((item: T) -> Any)? = null,
     crossinline itemContent: @Composable (item: T) -> Unit
 ) = items(
     count = items.size,
-    key = if (key != null) { index -> key(items[index]) } else null,
+    itemContent = { itemContent(items[it]) }
+)
+
+/** Adds a list of items. */
+inline fun <T> LazyStaggeredGridScope.items(
+    items: List<T>,
+    key: (item: T) -> Any,
+    crossinline itemContent: @Composable (item: T) -> Unit
+) = items(
+    count = items.size,
+    key = { key(items[it]) },
     itemContent = { itemContent(items[it]) }
 )
 
 private class RealLazyStaggeredGridScope(private val columnCount: Int) : LazyStaggeredGridScope {
 
-    val items = Array(columnCount) { mutableListOf<Pair<Any?, @Composable () -> Unit>>() }
-    var currentIndex = 0
+    private val items = Array(columnCount) { ArrayList<Pair<Any?, @Composable () -> Unit>>() }
+    private var currentIndex = 0
 
     override fun item(key: Any?, content: @Composable () -> Unit) {
         items[currentIndex % columnCount] += key to content
         currentIndex += 1
     }
 
-    override fun items(count: Int, key: ((Int) -> Any)?, itemContent: @Composable (Int) -> Unit) {
-        for (index in 0 until count) {
-            item(key?.invoke(index)) { itemContent(index) }
-        }
-    }
+    fun items(index: Int): List<Pair<Any?, @Composable () -> Unit>> = items[index]
 }
