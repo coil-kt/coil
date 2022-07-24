a161c8452e35d92d5c201eb327027933f9d4475e
Colin White <colin@colinwhite.me>
2022-07-24 15:35:38 -0400

    Fix RoundedCornersTransformation throwing an exception if passed a size with an undefined dimension.

---
+++ coil-base/src/androidTest/java/coil/transform/RoundedCornerTransformationTest.kt
@@ -0,0 +1,58 @@
+package coil.transform
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import coil.size.Dimension
+import coil.size.Size
+import coil.util.decodeBitmapAsset
+import coil.util.size
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import kotlin.test.assertEquals
+
+@OptIn(ExperimentalCoroutinesApi::class)
+class RoundedCornerTransformationTest {
+
+    private lateinit var context: Context
+    private lateinit var transformation: RoundedCornersTransformation
+
+    @Before
+    fun before() {
+        context = ApplicationProvider.getApplicationContext()
+        transformation = RoundedCornersTransformation(20f)
+    }
+
+    @Test
+    fun defined() = runTest {
+        val input = context.decodeBitmapAsset("normal_small.jpg")
+        val actual = transformation.transform(input, Size(100, 100))
+
+        assertEquals(Size(100, 100), actual.size)
+    }
+
+    @Test
+    fun undefined_width() = runTest {
+        val input = context.decodeBitmapAsset("normal_small.jpg")
+        val actual = transformation.transform(input, Size(Dimension.Undefined, 100))
+
+        assertEquals(Size(80, 100), actual.size)
+    }
+
+    @Test
+    fun undefined_height() = runTest {
+        val input = context.decodeBitmapAsset("normal_small.jpg")
+        val actual = transformation.transform(input, Size(100, Dimension.Undefined))
+
+        assertEquals(Size(100, 125), actual.size)
+    }
+
+    @Test
+    fun undefined_size() = runTest {
+        val input = context.decodeBitmapAsset("normal_small.jpg")
+        val actual = transformation.transform(input, Size.ORIGINAL)
+
+        assertEquals(Size(103, 129), actual.size)
+    }
+}
