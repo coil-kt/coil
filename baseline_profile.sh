a38544b2ebd7234888b00fa37a8c80b4e43372f8
Sagar Viradiya <sagarviradiya4444@gmail.com>
2022-08-11 20:53:20 +0200

    Update the permission of the baseline_profile.sh - Attempt 1024

--- baseline_profile.sh
+++ baseline_profile.sh
@@ -12,5 +12,5 @@ adb pull storage/emulated/0/Android/media/coil.compose.benchmark/BaselineProfile
 # Rename the file
 mv BaselineProfileGenerator_startup-baseline-prof.txt baseline-prof.txt
 
-#Move file to sample app module
+# Move file to sample app module
 mv baseline-prof.txt coil-sample-compose/src/main/baseline-prof.txt
