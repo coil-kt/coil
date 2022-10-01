6a0fac7d4bc77a602d36406ef757cf2e06c26866
Colin White <colin@colinwhite.me>
2022-10-01 14:38:25 -0700

    Set the preferred bitmap config in VideoFrameDecoder on API 30+. (#1487)
    * Set the preferred bitmap config in VideoFrameDecoder on API 30+.
    
    * Fix tests.

--- coil-video/src/main/java/coil/util/Utils.kt
+++ coil-video/src/main/java/coil/util/Utils.kt
@@ -2,8 +2,11 @@
 
 package coil.util
 
+import android.graphics.Bitmap
 import android.media.MediaMetadataRetriever
+import android.media.MediaMetadataRetriever.BitmapParams
 import android.os.Build.VERSION.SDK_INT
+import androidx.annotation.RequiresApi
 import coil.size.Dimension
 import coil.size.Scale
 import coil.size.Size
@@ -22,6 +25,31 @@ internal inline fun <T> MediaMetadataRetriever.use(block: (MediaMetadataRetrieve
             release()
         }
     }
+}
+
+internal fun MediaMetadataRetriever.getFrameAtTime(
+    timeUs: Long,
+    option: Int,
+    config: Bitmap.Config,
+): Bitmap? = if (SDK_INT >= 30) {
+    val params = BitmapParams().apply { preferredConfig = config }
+    getFrameAtTime(timeUs, option, params)
+} else {
+    getFrameAtTime(timeUs, option)
+}
+
+@RequiresApi(27)
+internal fun MediaMetadataRetriever.getScaledFrameAtTime(
+    timeUs: Long,
+    option: Int,
+    dstWidth: Int,
+    dstHeight: Int,
+    config: Bitmap.Config,
+): Bitmap? = if (SDK_INT >= 30) {
+    val params = BitmapParams().apply { preferredConfig = config }
+    getScaledFrameAtTime(timeUs, option, dstWidth, dstHeight, params)
+} else {
+    getScaledFrameAtTime(timeUs, option, dstWidth, dstHeight)
 }
 
 internal inline fun Size.widthPx(scale: Scale, original: () -> Int): Int {
