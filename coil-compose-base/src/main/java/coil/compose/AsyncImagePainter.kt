26665b8bf8cd11025812fc95d9c67dbdb14ccf60
Colin White <colin@colinwhite.me>
2022-10-01 20:23:25 -0700

    Revert.

--- coil-compose-base/src/main/java/coil/compose/AsyncImagePainter.kt
+++ coil-compose-base/src/main/java/coil/compose/AsyncImagePainter.kt
@@ -260,8 +260,6 @@ class AsyncImagePainter internal constructor(
     private fun clear() {
         rememberScope?.cancel()
         rememberScope = null
-        object : coil.target.Target {
-        }
     }
 
     /** Update the [request] to work with [AsyncImagePainter]. */
