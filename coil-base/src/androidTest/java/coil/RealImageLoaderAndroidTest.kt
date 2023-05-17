2d193e18115459174a0b3a54c6dffe8260118187
Colin White <colin@colinwhite.me>
2023-05-16 21:57:59 -0700

    Fix remaining deprecated usages.

--- coil-base/src/androidTest/java/coil/RealImageLoaderAndroidTest.kt
+++ coil-base/src/androidTest/java/coil/RealImageLoaderAndroidTest.kt
@@ -388,7 +388,7 @@ class RealImageLoaderAndroidTest {
 
         assertEquals(DataSource.NETWORK, result.dataSource)
         assertEquals(key, result.diskCacheKey)
-        imageLoader.diskCache!![key]!!.use { assertNotNull(it) }
+        imageLoader.diskCache!!.read(key)!!.use { assertNotNull(it) }
     }
 
     @Test
