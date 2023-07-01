e6e3539d539219746273dc4503177b4d51f029d9
Colin White <colin@colinwhite.me>
2023-07-01 01:07:03 -0700

    Better define Image class.

--- coil-base/src/commonMain/kotlin/coil/Image.kt
+++ coil-base/src/commonMain/kotlin/coil/Image.kt
@@ -2,5 +2,20 @@ package coil
 
 interface Image {
 
+    /** The size of the image in memory in bytes. */
     val size: Long
+
+    /** The width of the image in pixels. */
+    val width: Int
+
+    /** The height of the image in pixels. */
+    val height: Int
+
+    /**
+     * True if the image can be shared between multiple [Target]s.
+     *
+     * For example, an Android bitmap can be shared between multiple targets if it's not mutated.
+     * Conversely, a crossfade drawable cannot be shared as its alpha is being mutated frequently.
+     */
+    val shareable: Boolean
 }
