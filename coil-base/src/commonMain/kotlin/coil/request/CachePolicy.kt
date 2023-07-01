b755e638a9af031337313d3ffead9d85108b9bfb
Colin White <colin@colinwhite.me>
2023-06-30 21:50:50 -0700

    Formatting.

--- coil-base/src/commonMain/kotlin/coil/request/CachePolicy.kt
+++ coil-base/src/commonMain/kotlin/coil/request/CachePolicy.kt
@@ -9,10 +9,10 @@ package coil.request
  */
 enum class CachePolicy(
     val readEnabled: Boolean,
-    val writeEnabled: Boolean
+    val writeEnabled: Boolean,
 ) {
     ENABLED(true, true),
     READ_ONLY(true, false),
     WRITE_ONLY(false, true),
-    DISABLED(false, false)
+    DISABLED(false, false),
 }
