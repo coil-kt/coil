7d80b246a9bef4745e7384acf8719d9ac1f545ae
Colin White <colin@colinwhite.me>
2023-07-04 16:55:09 -0700

    Add extensions for Ktor + Okio.

--- coil-base/src/commonMain/kotlin/coil/network/utils.kt
+++ coil-base/src/commonMain/kotlin/coil/network/utils.kt
@@ -1,40 +1,21 @@
 package coil.network
 
 import io.ktor.utils.io.ByteReadChannel
-import io.ktor.utils.io.ByteWriteChannel
 import io.ktor.utils.io.core.isEmpty
 import io.ktor.utils.io.core.readFully
-import okio.Sink
-import okio.Source
-import okio.buffer
-import okio.use
+import okio.BufferedSink
 
-internal suspend fun ByteReadChannel.readFully(sink: Sink) {
-    sink.buffer().use { bufferedSink ->
-        val buffer = ByteArray(OKIO_BUFFER_SIZE)
+/** Write a [ByteReadChannel] to [sink] using streaming. */
+internal suspend fun ByteReadChannel.readFully(sink: BufferedSink) {
+    val buffer = ByteArray(OKIO_BUFFER_SIZE)
 
-        while (!isClosedForRead) {
-            val packet = readRemaining(buffer.size.toLong())
-            if (packet.isEmpty) break
+    while (!isClosedForRead) {
+        val packet = readRemaining(buffer.size.toLong())
+        if (packet.isEmpty) break
 
-            val bytesRead = packet.remaining.toInt()
-            packet.readFully(buffer, 0, bytesRead)
-            bufferedSink.write(buffer, 0, bytesRead)
-        }
-    }
-}
-
-internal suspend fun ByteWriteChannel.writeAll(source: Source) {
-    source.buffer().use { bufferedSource ->
-        val buffer = ByteArray(OKIO_BUFFER_SIZE)
-
-        while (!isClosedForWrite) {
-            val bytesRead = bufferedSource.read(buffer)
-            if (bytesRead == -1) break
-
-            writeFully(buffer, 0, bytesRead)
-            flush()
-        }
+        val bytesRead = packet.remaining.toInt()
+        packet.readFully(buffer, 0, bytesRead)
+        sink.write(buffer, 0, bytesRead)
     }
 }
 
