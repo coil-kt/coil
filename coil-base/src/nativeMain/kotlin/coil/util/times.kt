0b3ba3cf8368fecdc814760fef07981ac0c0ba35
Colin White <colin@colinwhite.me>
2023-10-03 22:27:54 -0500

    Replace deprecated epoch time function.

--- coil-base/src/nativeMain/kotlin/coil/util/times.kt
+++ coil-base/src/nativeMain/kotlin/coil/util/times.kt
@@ -1,5 +1,17 @@
 package coil.util
 
-import kotlin.system.getTimeMillis
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.alloc
+import kotlinx.cinterop.convert
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.ptr
+import platform.posix.CLOCK_REALTIME
+import platform.posix.clock_gettime
+import platform.posix.timespec
 
-internal actual fun getTimeMillis() = getTimeMillis()
+@OptIn(ExperimentalForeignApi::class)
+internal actual fun getTimeMillis(): Long = memScoped {
+    val timeHolder = alloc<timespec>()
+    clock_gettime(CLOCK_REALTIME.convert(), timeHolder.ptr)
+    timeHolder.tv_sec * 1_000L + timeHolder.tv_nsec / 1_000_000L
+}
