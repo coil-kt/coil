f9028a556dadbca422ae267de01c3a9ff651ee18
Colin White <colin@colinwhite.me>
2023-11-15 23:29:11 -0800

    Add native Skia image impl.

--- coil-base/src/nonAndroidMain/kotlin/coil/Image.kt
+++ coil-base/src/nonAndroidMain/kotlin/coil/Image.kt
@@ -2,6 +2,31 @@ package coil
 
 import org.jetbrains.skia.Image as SkiaImage
 
-fun SkiaImage.asCoilImage(): Image = TODO()
+private class RealImage(
+    val image: SkiaImage,
+) : Image {
 
-fun Image.asSkiaImage(): SkiaImage = TODO()
+    override val size: Long
+        get() {
+            var bytesPerPixel = image.bytesPerPixel
+            if (bytesPerPixel <= 0) {
+                // Estimate a standard 4 bytes per pixel.
+                bytesPerPixel = 4
+            }
+            return bytesPerPixel.toLong() * image.width * image.height
+        }
+
+    override val width: Int
+        get() = image.width
+
+    override val height: Int
+        get() = image.height
+
+    // TODO: Return false if this is an animated image type.
+    override val shareable: Boolean
+        get() = true
+}
+
+fun SkiaImage.asCoilImage(): Image = RealImage(this)
+
+fun Image.asSkiaImage(): SkiaImage = (this as RealImage).image
