15c9618d3d314eec2414557ed43a3acc1645301d
Colin White <colin@colinwhite.me>
2023-11-28 13:19:48 -0800

    Install FileMapper.

--- coil-base/src/jvmCommon/kotlin/coil/RealImageLoader.jvm.kt
+++ coil-base/src/jvmCommon/kotlin/coil/RealImageLoader.jvm.kt
@@ -1,10 +1,14 @@
 package coil
 
 import coil.fetch.ByteBufferFetcher
+import coil.map.FileMapper
 
 internal actual fun ComponentRegistry.Builder.addJvmComponents(
     options: RealImageLoader.Options,
 ): ComponentRegistry.Builder {
     return this
+        // Mappers
+        .add(FileMapper())
+        // Fetchers
         .add(ByteBufferFetcher.Factory())
 }
