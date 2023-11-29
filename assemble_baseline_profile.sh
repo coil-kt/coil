91a379690f921c7dd4e4f94229ac105048ae9af4
Colin White <colin@colinwhite.me>
2023-11-28 19:43:44 -0800

    Update baseline profiles.

--- assemble_baseline_profile.sh
+++ assemble_baseline_profile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 ./gradlew :coil-benchmark:pixel7Api33BenchmarkAndroidTest -P android.testInstrumentationRunnerArguments.class=coil.benchmark.BaselineProfileGenerator#generate -Dproject=view
-mv coil-benchmark/build/outputs/managed_device_android_test_additional_output/benchmark/pixel7Api33/BaselineProfileGenerator_generate-baseline-prof.txt coil-base/src/main/baseline-prof.txt
+mv coil-benchmark/build/outputs/managed_device_android_test_additional_output/benchmark/pixel7Api33/BaselineProfileGenerator_generate-baseline-prof.txt coil-base/src/androidMain/baseline-prof.txt
+
 ./gradlew :coil-benchmark:pixel7Api33BenchmarkAndroidTest -P android.testInstrumentationRunnerArguments.class=coil.benchmark.BaselineProfileGenerator#generate -Dproject=compose
-mv coil-benchmark/build/outputs/managed_device_android_test_additional_output/benchmark/pixel7Api33/BaselineProfileGenerator_generate-baseline-prof.txt coil-compose-base/src/main/baseline-prof.txt
+mv coil-benchmark/build/outputs/managed_device_android_test_additional_output/benchmark/pixel7Api33/BaselineProfileGenerator_generate-baseline-prof.txt coil-compose-base/src/androidMain/baseline-prof.txt
