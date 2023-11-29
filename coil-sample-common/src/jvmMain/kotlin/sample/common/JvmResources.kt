ed6b1c90d7562fbf1ad0d09f34ab0ec858646875
Colin White <colin@colinwhite.me>
2023-11-29 12:43:16 -0800

    Create JvmResources.

---
+++ coil-sample-common/src/jvmMain/kotlin/sample/common/JvmResources.kt
@@ -0,0 +1,10 @@
+package sample.common
+
+import okio.Source
+import okio.source
+
+class JvmResources : Resources {
+    override fun open(path: String): Source {
+        return javaClass.classLoader.getResourceAsStream(path)!!.source()
+    }
+}
