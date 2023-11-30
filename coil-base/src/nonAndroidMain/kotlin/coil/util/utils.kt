d543aa09489dc85e64da9e99ee4a8799c421851c
Colin White <colin@colinwhite.me>
2023-11-29 19:40:26 -0800

    Fix Skia decoder sizing.

--- coil-base/src/nonAndroidMain/kotlin/coil/util/utils.kt
+++ coil-base/src/nonAndroidMain/kotlin/coil/util/utils.kt
@@ -6,10 +6,8 @@ import coil.request.ImageRequest
 import coil.size.Precision
 import coil.size.Scale
 import coil.size.Size
-import kotlin.math.roundToInt
 import org.jetbrains.skia.Bitmap
 import org.jetbrains.skia.Canvas
-import org.jetbrains.skia.Codec
 import org.jetbrains.skia.Image
 import org.jetbrains.skia.Rect
 import org.jetbrains.skia.impl.use
@@ -29,37 +27,31 @@ internal actual val ImageRequest.allowInexactSize: Boolean
         Precision.AUTOMATIC -> true
     }
 
-/** Returns true if the image is not animated. */
-internal val Codec.isStatic: Boolean
-    get() = frameCount <= 1
-
 /** Create a [Bitmap] from [image] that fits the given [size] and [scale]. */
 internal fun Bitmap.Companion.makeFromImage(
     image: Image,
     size: Size,
     scale: Scale,
 ): Bitmap {
-    val bitmap = Bitmap()
     val srcWidth = image.width
     val srcHeight = image.height
-    val targetWidth = size.widthPx(scale) { srcWidth }
-    val targetHeight = size.heightPx(scale) { srcHeight }
     val multiplier = DecodeUtils.computeSizeMultiplier(
         srcWidth = srcWidth,
         srcHeight = srcHeight,
-        dstWidth = targetWidth,
-        dstHeight = targetHeight,
+        dstWidth = size.widthPx(scale) { srcWidth },
+        dstHeight = size.heightPx(scale) { srcHeight },
         scale = scale,
     )
-    val dstWidth = (multiplier * targetWidth).roundToInt()
-    val dstHeight = (multiplier * targetHeight).roundToInt()
+    val dstWidth = (multiplier * srcWidth).toInt()
+    val dstHeight = (multiplier * srcHeight).toInt()
 
+    val bitmap = Bitmap()
     bitmap.allocN32Pixels(dstWidth, dstHeight)
     bitmap.applyCanvas {
         drawImageRect(
-            image,
-            Rect.makeWH(srcWidth.toFloat(), srcHeight.toFloat()),
-            Rect.makeWH(dstWidth.toFloat(), dstHeight.toFloat()),
+            image = image,
+            src = Rect.makeWH(srcWidth.toFloat(), srcHeight.toFloat()),
+            dst = Rect.makeWH(dstWidth.toFloat(), dstHeight.toFloat()),
         )
     }
     return bitmap
