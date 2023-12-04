a991e9a8b875e6860f2d030839a04cb39ef95671
Colin White <colin@colinwhite.me>
2023-12-04 01:55:16 -0500

    Fix incorrect crossfade configuration.

--- coil-base/src/androidMain/kotlin/coil3/request/imageRequests.kt
+++ coil-base/src/androidMain/kotlin/coil3/request/imageRequests.kt
@@ -73,14 +73,14 @@ private val transformationsKey = Extras.Key<List<Transformation>>(default = empt
  * Enable a crossfade animation when a request completes successfully.
  */
 actual fun ImageRequest.Builder.crossfade(enable: Boolean) =
-    crossfade(DEFAULT_CROSSFADE_MILLIS)
+    crossfade(if (enable) DEFAULT_CROSSFADE_MILLIS else 0)
 
 actual fun ImageRequest.Builder.crossfade(durationMillis: Int): ImageRequest.Builder {
     return transitionFactory(newCrossfadeTransitionFactory(durationMillis))
 }
 
 actual fun ImageLoader.Builder.crossfade(enable: Boolean) =
-    crossfade(DEFAULT_CROSSFADE_MILLIS)
+    crossfade(if (enable) DEFAULT_CROSSFADE_MILLIS else 0)
 
 actual fun ImageLoader.Builder.crossfade(durationMillis: Int): ImageLoader.Builder {
     return transitionFactory(newCrossfadeTransitionFactory(durationMillis))
