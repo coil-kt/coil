8dfaba264f161f4d7203d5b6243406628622a240
Colin White <colin@colinwhite.me>
2023-12-07 00:31:42 -0500

    Use atomic delegate.

--- coil-core/src/nonAndroidMain/kotlin/coil3/util/SystemCallbacks.kt
+++ coil-core/src/nonAndroidMain/kotlin/coil3/util/SystemCallbacks.kt
@@ -8,13 +8,11 @@ internal actual fun SystemCallbacks(): SystemCallbacks = NoopSystemCallbacks()
 // TODO: Listen for memory-pressure events to trim the memory cache on non-Android platforms.
 private class NoopSystemCallbacks : SystemCallbacks {
     override val isOnline get() = true
-
-    private val _isShutdown = atomic(false)
-    override val isShutdown: Boolean by _isShutdown
+    override var isShutdown by atomic(false)
 
     override fun register(imageLoader: RealImageLoader) {}
 
     override fun shutdown() {
-        _isShutdown.value = true
+        isShutdown = true
     }
 }
