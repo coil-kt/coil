d1d0e12ff220f80173dbcae39484a73ea8166025
Colin White <colin@colinwhite.me>
2023-12-06 23:51:45 -0500

    Use Compose viewModel function.

--- coil-sample-compose/src/androidMain/kotlin/sample/compose/MainActivity.kt
+++ coil-sample-compose/src/androidMain/kotlin/sample/compose/MainActivity.kt
@@ -3,14 +3,14 @@ package sample.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
+import androidx.lifecycle.viewmodel.compose.viewModel
 import sample.common.AndroidMainViewModel
 
 class MainActivity : ComponentActivity() {
-    private val viewModel: AndroidMainViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { App(viewModel) }
+        setContent {
+            App(viewModel<AndroidMainViewModel>())
+        }
     }
 }
