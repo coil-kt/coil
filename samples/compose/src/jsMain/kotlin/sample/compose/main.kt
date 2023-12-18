c0c15cc48a04482fafa8f2a1a4f31fd40d4848a8
Colin White <colin@colinwhite.me>
2023-12-18 03:50:35 -0500

    Simplify JS sample.

--- samples/compose/src/jsMain/kotlin/sample/compose/main.kt
+++ samples/compose/src/jsMain/kotlin/sample/compose/main.kt
@@ -3,24 +3,14 @@ package sample.compose
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
-import kotlinx.browser.window
 import org.jetbrains.skiko.wasm.onWasmReady
 import sample.common.JsResources
 
 @OptIn(ExperimentalComposeUiApi::class)
-fun main() = onReady {
+fun main() = onWasmReady {
     initializeSingletonImageLoader()
 
     CanvasBasedWindow(Title) {
         App(remember { JsResources() })
     }
-}
-
-private fun onReady(callback: () -> Unit) {
-    window.addEventListener(
-        type = "load",
-        callback = {
-            onWasmReady(callback)
-        },
-    )
 }
