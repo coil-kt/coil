2c23dd4eedab26fab006e46dd50dbef263363d41
Colin White <colin@colinwhite.me>
2023-12-18 03:51:35 -0500

    Simplify wasm sample.

--- samples/compose/src/wasmJsMain/kotlin/sample/compose/main.kt
+++ samples/compose/src/wasmJsMain/kotlin/sample/compose/main.kt
@@ -3,23 +3,13 @@ package sample.compose
 import androidx.compose.runtime.remember
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.CanvasBasedWindow
-import kotlinx.browser.window
 import sample.common.WasmJsResources
 
 @OptIn(ExperimentalComposeUiApi::class)
-fun main() = onReady {
+fun main() {
     initializeSingletonImageLoader()
 
     CanvasBasedWindow(Title) {
         App(remember { WasmJsResources() })
     }
-}
-
-private fun onReady(callback: () -> Unit) {
-    window.addEventListener(
-        type = "load",
-        callback = {
-            callback()
-        },
-    )
 }
