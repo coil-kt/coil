f09161d98012c6eaa3de91dd2ad692ba51a0e1dc
GG Bond <whatever@tagkong.com>
2023-12-23 16:52:36 +0800

    Fix build

--- coil-core/src/androidMain/kotlin/coil3/util/gifUtils.kt
+++ coil-core/src/androidMain/kotlin/coil3/util/gifUtils.kt
@@ -5,7 +5,6 @@ import android.graphics.PostProcessor
 import android.graphics.drawable.Animatable2
 import android.graphics.drawable.Drawable
 import androidx.annotation.RequiresApi
-import androidx.vectordrawable.graphics.drawable.Animatable2Compat
 import coil3.transform.AnimatedTransformation
 import coil3.transform.PixelOpacity
 
@@ -25,14 +24,6 @@ internal fun animatable2CallbackOf(
     onStart: (() -> Unit)?,
     onEnd: (() -> Unit)?
 ) = object : Animatable2.AnimationCallback() {
-    override fun onAnimationStart(drawable: Drawable?) { onStart?.invoke() }
-    override fun onAnimationEnd(drawable: Drawable?) { onEnd?.invoke() }
-}
-
-internal fun animatable2CompatCallbackOf(
-    onStart: (() -> Unit)?,
-    onEnd: (() -> Unit)?
-) = object : Animatable2Compat.AnimationCallback() {
     override fun onAnimationStart(drawable: Drawable?) { onStart?.invoke() }
     override fun onAnimationEnd(drawable: Drawable?) { onEnd?.invoke() }
 }
