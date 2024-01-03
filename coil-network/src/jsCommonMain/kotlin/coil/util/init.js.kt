c5ca5bddcfdbd378088d71830c288d2bbb70ced9
Colin White <colin@colinwhite.me>
2024-01-02 21:20:16 -0500

    Style.

--- coil-network/src/jsCommonMain/kotlin/coil/util/init.js.kt
+++ coil-network/src/jsCommonMain/kotlin/coil/util/init.js.kt
@@ -1,8 +1,8 @@
 package coil.util
 
 import coil3.annotation.InternalCoilApi
-import coil3.util.ServiceLoaderComponentRegistry
 import coil3.util.NetworkFetcherServiceLoaderTarget
+import coil3.util.ServiceLoaderComponentRegistry
 
 @Suppress("DEPRECATION")
 @OptIn(ExperimentalStdlibApi::class, ExperimentalJsExport::class)
