c371781c3b85add13fb63275a92f6981c964966a
Vladimir Dostoyevskiy <99822064+VDostoyevskiy@users.noreply.github.com>
2024-01-04 14:28:35 +0800

    Use DirectByteBuffer as AnimatedImageDecoderDecoder fallback source (#1986)
    * Use DirectByteBuffer as AnimatedImageDecoderDecoder fallback source
    
    * Optimize bytearray allocation
    
    * Add tests
    
    ---------
    
    Co-authored-by: GG Bond <whatever@tagkong.com>

--- coil-gif/src/main/java/coil3/decode/AnimatedImageDecoderDecoder.kt
+++ coil-gif/src/main/java/coil3/decode/AnimatedImageDecoderDecoder.kt
@@ -125,12 +125,8 @@ class AnimatedImageDecoderDecoder @JvmOverloads constructor(
             if (isDirect || SDK_INT >= 30) return ImageDecoder.createSource(metadata.byteBuffer)
         }
 
-        return when {
-            SDK_INT >= 31 -> ImageDecoder.createSource(source().readByteArray())
-            SDK_INT == 30 -> ImageDecoder.createSource(ByteBuffer.wrap(source().readByteArray()))
-            // https://issuetracker.google.com/issues/139371066
-            else -> ImageDecoder.createSource(file().toFile())
-        }
+        val bytebuffer = source.source().use { it.squashToDirectByteBuffer() }
+        return ImageDecoder.createSource(bytebuffer)
     }
 
     private fun ImageDecoder.configureImageDecoderProperties() {
@@ -189,5 +185,15 @@ class AnimatedImageDecoderDecoder @JvmOverloads constructor(
                 DecodeUtils.isAnimatedWebP(source) ||
                 (SDK_INT >= 30 && DecodeUtils.isAnimatedHeif(source))
         }
+    }
+}
+
+internal fun BufferedSource.squashToDirectByteBuffer(): ByteBuffer {
+    // Squash bytes to BufferedSource inner buffer then we know total byteCount
+    request(Long.MAX_VALUE)
+    return ByteBuffer.allocateDirect(buffer.size.toInt()).apply {
+        // Squash to DirectByteBuffer
+        while (!buffer.exhausted()) buffer.read(this)
+        flip()
     }
 }
