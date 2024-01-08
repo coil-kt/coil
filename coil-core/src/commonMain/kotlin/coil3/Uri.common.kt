7371268d15c02a2c5cca0675e4561ac65b622bb4
Colin White <colin@colinwhite.me>
2024-01-08 04:43:50 -0500

    Ensure Uri properly handles encoded text.

--- coil-core/src/commonMain/kotlin/coil3/Uri.common.kt
+++ coil-core/src/commonMain/kotlin/coil3/Uri.common.kt
@@ -34,7 +34,8 @@ val Uri.pathSegments: List<String>
 
 fun String.toUri(): Uri = parseUri(this)
 
-private fun parseUri(data: String): Uri {
+private fun parseUri(rawData: String): Uri {
+    val data = rawData.percentDecode()
     var authorityStartIndex = -1
     var pathStartIndex = -1
     var queryStartIndex = -1
@@ -111,5 +112,25 @@ private fun parseUri(data: String): Uri {
         fragment = data.substring(fragmentStartIndex, data.length)
     }
 
-    return Uri(data, scheme, authority, path, query, fragment)
+    return Uri(rawData, scheme, authority, path, query, fragment)
+}
+
+private fun String.percentDecode(): String {
+    val bytes = ByteArray(length)
+    var size = 0
+    var index = 0
+
+    while (index < length) {
+        if (get(index) == '%') {
+            val hex = substring(index + 1, index + 3)
+            bytes[size] = hex.toInt(16).toByte()
+            index += 3
+        } else {
+            bytes[size] = get(index).code.toByte()
+            index++
+        }
+        size++
+    }
+
+    return bytes.decodeToString(endIndex = size)
 }
--- coil-core/src/commonTest/kotlin/coil3/UriTest.kt
+++ coil-core/src/commonTest/kotlin/coil3/UriTest.kt
@@ -46,4 +46,16 @@ class UriTest {
         assertEquals("ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjU4MjM5fQ", uri.query)
         assertNull(uri.fragment)
     }
+
+    @Test
+    fun encoded() {
+        val string = "https://example.com/%E4%B8%8A%E6%B5%B7%2B%E4%B8%AD%E5%9C%8B"
+        val uri = string.toUri()
+        assertEquals("https", uri.scheme)
+        assertEquals("example.com", uri.authority)
+        assertEquals("/上海+中國", uri.path)
+        assertNull(uri.query)
+        assertNull(uri.fragment)
+        assertEquals(string, uri.toString())
+    }
 }
