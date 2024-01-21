66028a0fd4c3a7088f4f7785aacb5e28eb7e029f
Colin White <colin@colinwhite.me>
2024-01-21 15:13:27 -0800

    Expose Call.Factory instead of OkHttpClient in OkHttpNetworkFetcherFactory.

--- coil-network-okhttp/src/commonMain/kotlin/coil3/network/okhttp/OkHttpNetworkFetcher.kt
+++ coil-network-okhttp/src/commonMain/kotlin/coil3/network/okhttp/OkHttpNetworkFetcher.kt
@@ -5,27 +5,28 @@ package coil3.network.okhttp
 import coil3.network.CacheStrategy
 import coil3.network.NetworkClient
 import coil3.network.NetworkFetcher
-import coil3.network.okhttp.internal.OkHttpNetworkClient
+import coil3.network.okhttp.internal.CallFactoryNetworkClient
+import okhttp3.Call
 import okhttp3.OkHttpClient
 
 @JvmOverloads
 @JvmName("factory")
 fun OkHttpNetworkFetcherFactory(
-    httpClient: Lazy<OkHttpClient> = lazy { OkHttpClient() },
+    callFactory: Lazy<Call.Factory> = lazy { OkHttpClient() },
 ) = OkHttpNetworkFetcherFactory(
-    httpClient = httpClient,
+    callFactory = callFactory,
     cacheStrategy = lazy { CacheStrategy() },
 )
 
 @JvmName("factory")
 fun OkHttpNetworkFetcherFactory(
-    httpClient: Lazy<OkHttpClient>,
+    callFactory: Lazy<Call.Factory>,
     cacheStrategy: Lazy<CacheStrategy>,
 ) = NetworkFetcher.Factory(
-    networkClient = lazy { httpClient.value.asNetworkClient() },
+    networkClient = lazy { callFactory.value.asNetworkClient() },
     cacheStrategy = cacheStrategy,
 )
 
-fun OkHttpClient.asNetworkClient(): NetworkClient {
-    return OkHttpNetworkClient(this)
+fun Call.Factory.asNetworkClient(): NetworkClient {
+    return CallFactoryNetworkClient(this)
 }
