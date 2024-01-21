0974d46526a2de8810abcef04fbc0e1e7497fcde
MohamedRejeb <mohamedrejeb445@gmail.com>
2024-01-21 12:05:50 +0100

    Run apiDump

--- coil-test/src/nonAndroidMain/kotlin/coil3/test/FakeImage.nonAndroid.kt
+++ coil-test/src/nonAndroidMain/kotlin/coil3/test/FakeImage.nonAndroid.kt
@@ -5,11 +5,14 @@ package coil3.test
 import coil3.Image
 import coil3.annotation.Data
 import coil3.annotation.ExperimentalCoilApi
+import org.jetbrains.skia.Bitmap
 import org.jetbrains.skia.Canvas
 import org.jetbrains.skia.ColorAlphaType
 import org.jetbrains.skia.ColorInfo
 import org.jetbrains.skia.ColorType
+import org.jetbrains.skia.ImageInfo
 import org.jetbrains.skia.Paint
+import org.jetbrains.skia.impl.use
 
 @ExperimentalCoilApi
 @Data
@@ -20,10 +23,15 @@ actual class FakeImage actual constructor(
     override val shareable: Boolean,
     actual val color: Int,
 ) : Image {
-    override fun Canvas.onDraw() {
-        val paint = Paint()
-        paint.color = color
-        drawPaint(paint)
+    override fun toBitmap(): Bitmap {
+        val bitmap = Bitmap()
+        bitmap.setImageInfo(ImageInfo(colorInfo, width, height))
+        Canvas(bitmap).use { canvas ->
+            val paint = Paint()
+            paint.color = color
+            canvas.drawPaint(paint)
+        }
+        return bitmap
     }
 }
 
--- internal/test-utils/src/nonAndroidMain/kotlin/coil3/test/utils/FakeImage.nonAndroid.kt
+++ internal/test-utils/src/nonAndroidMain/kotlin/coil3/test/utils/FakeImage.nonAndroid.kt
@@ -1,6 +1,7 @@
 package coil3.test.utils
 
 import coil3.Image
+import org.jetbrains.skia.Bitmap
 import org.jetbrains.skia.Canvas
 
 actual class FakeImage actual constructor(
@@ -9,5 +10,7 @@ actual class FakeImage actual constructor(
     override val size: Long,
     override val shareable: Boolean,
 ) : Image {
-    override fun Canvas.onDraw() = Unit
+    override fun toBitmap(): Bitmap {
+        throw UnsupportedOperationException()
+    }
 }
