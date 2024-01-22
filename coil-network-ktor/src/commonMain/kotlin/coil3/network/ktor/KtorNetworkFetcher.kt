74e16d57f88b19bee8e83ef620badd75bd80e906
Colin White <colin@colinwhite.me>
2024-01-21 16:48:30 -0800

    Remove Lazy from OkHttpNetworkFetcherFactory and KtorNetworkFetcherFactory public API.

--- coil-network-ktor/src/commonMain/kotlin/coil3/network/ktor/KtorNetworkFetcher.kt
+++ coil-network-ktor/src/commonMain/kotlin/coil3/network/ktor/KtorNetworkFetcher.kt
@@ -8,24 +8,45 @@ import coil3.network.NetworkFetcher
 import coil3.network.ktor.internal.KtorNetworkClient
 import io.ktor.client.HttpClient
 import kotlin.jvm.JvmName
-import kotlin.jvm.JvmOverloads
 
-@JvmOverloads
 @JvmName("factory")
-fun KtorNetworkFetcherFactory(
-    httpClient: Lazy<HttpClient> = lazy { HttpClient() },
-) = KtorNetworkFetcherFactory(
-    httpClient = httpClient,
+fun KtorNetworkFetcherFactory() = NetworkFetcher.Factory(
+    networkClient = lazy { HttpClient().asNetworkClient() },
     cacheStrategy = lazy { CacheStrategy() },
 )
 
 @JvmName("factory")
 fun KtorNetworkFetcherFactory(
-    httpClient: Lazy<HttpClient>,
-    cacheStrategy: Lazy<CacheStrategy>,
+    httpClient: () -> HttpClient,
+) = KtorNetworkFetcherFactory(
+    httpClient = httpClient,
+    cacheStrategy = { CacheStrategy() },
+)
+
+@JvmName("factory")
+fun KtorNetworkFetcherFactory(
+    httpClient: () -> HttpClient,
+    cacheStrategy: () -> CacheStrategy,
 ) = NetworkFetcher.Factory(
-    networkClient = lazy { httpClient.value.asNetworkClient() },
-    cacheStrategy = cacheStrategy,
+    networkClient = lazy { httpClient().asNetworkClient() },
+    cacheStrategy = lazy(cacheStrategy),
+)
+
+@JvmName("factory")
+fun KtorNetworkFetcherFactory(
+    httpClient: HttpClient,
+) = KtorNetworkFetcherFactory(
+    httpClient = httpClient,
+    cacheStrategy = CacheStrategy(),
+)
+
+@JvmName("factory")
+fun KtorNetworkFetcherFactory(
+    httpClient: HttpClient,
+    cacheStrategy: CacheStrategy,
+) = NetworkFetcher.Factory(
+    networkClient = lazyOf(httpClient.asNetworkClient()),
+    cacheStrategy = lazyOf(cacheStrategy),
 )
 
 fun HttpClient.asNetworkClient(): NetworkClient {
