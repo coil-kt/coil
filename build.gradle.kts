8ad3facf770b385aa956c5d2788d7e85e03a373a
Colin White <colin@colinwhite.me>
2024-02-28 21:53:11 -0800

    Remove Compose dev maven repo and downgrade Skiko to 0.7.93. (#2151)

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,6 @@ buildscript {
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
     dependencies {
         classpath(libs.gradlePlugin.android)
@@ -55,7 +54,6 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
 
     // Necessary to publish to Maven.
