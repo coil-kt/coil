9bb4bcfe287681de58bb927afeebf5244de90685
Colin White <colin@colinwhite.me>
2024-05-19 12:58:14 -0500

    Implement SvgDecoder.renderToBitmap in nonAndroid sources. (#2275)

--- coil-svg/src/commonMain/kotlin/coil3/svg/SvgDecoder.kt
+++ coil-svg/src/commonMain/kotlin/coil3/svg/SvgDecoder.kt
@@ -6,6 +6,7 @@ import coil3.decode.Decoder
 import coil3.decode.ImageSource
 import coil3.fetch.SourceFetchResult
 import coil3.request.Options
+import kotlin.jvm.JvmOverloads
 
 /**
  * A [Decoder] that uses [AndroidSVG](https://bigbadaboom.github.io/androidsvg/)
@@ -14,16 +15,22 @@ import coil3.request.Options
  *
  * @param useViewBoundsAsIntrinsicSize If true, uses the SVG's view bounds as the intrinsic size for
  *  the SVG. If false, uses the SVG's width/height as the intrinsic size for the SVG.
+ * @param renderToBitmap If true, renders the SVG to a bitmap immediately after decoding. Else, the
+ *  SVG will be rendered at draw time. Rendering at draw time is more memory efficient, but
+ *  depending on the complexity of the SVG, can be slow.
  */
 expect class SvgDecoder(
     source: ImageSource,
     options: Options,
     useViewBoundsAsIntrinsicSize: Boolean = true,
+    renderToBitmap: Boolean = true,
 ) : Decoder {
+
     override suspend fun decode(): DecodeResult?
 
-    class Factory(
+    class Factory @JvmOverloads constructor(
         useViewBoundsAsIntrinsicSize: Boolean = true,
+        renderToBitmap: Boolean = true,
     ) : Decoder.Factory {
         override fun create(
             result: SourceFetchResult,
