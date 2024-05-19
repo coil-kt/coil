8498e3c84f2ad6affcef77d476468448cbbbcf77
Colin White <colin@colinwhite.me>
2024-05-18 19:02:36 -0500

    Add an option to continue to render as a bitmap in SvgDecoder.

--- coil-svg/src/androidMain/kotlin/coil3/svg/SvgDecoder.android.kt
+++ coil-svg/src/androidMain/kotlin/coil3/svg/SvgDecoder.android.kt
@@ -1,15 +1,20 @@
 package coil3.svg
 
+import android.graphics.Canvas
 import android.graphics.RectF
+import androidx.core.graphics.createBitmap
 import coil3.ImageLoader
+import coil3.asImage
 import coil3.decode.DecodeResult
 import coil3.decode.DecodeUtils
 import coil3.decode.Decoder
 import coil3.decode.ImageSource
 import coil3.fetch.SourceFetchResult
 import coil3.request.Options
+import coil3.request.bitmapConfig
 import coil3.svg.internal.MIME_TYPE_SVG
 import coil3.svg.internal.getDstSize
+import coil3.util.toSoftware
 import com.caverock.androidsvg.RenderOptions
 import com.caverock.androidsvg.SVG
 import kotlinx.coroutines.runInterruptible
@@ -21,11 +26,18 @@ import kotlinx.coroutines.runInterruptible
  * @param useViewBoundsAsIntrinsicSize If true, uses the SVG's view bounds as the intrinsic size for
  *  the SVG. If false, uses the SVG's width/height as the intrinsic size for the SVG.
  */
-actual class SvgDecoder @JvmOverloads actual constructor(
+actual class SvgDecoder(
     private val source: ImageSource,
     private val options: Options,
     val useViewBoundsAsIntrinsicSize: Boolean,
+    val renderToBitmap: Boolean,
 ) : Decoder {
+
+    actual constructor(
+        source: ImageSource,
+        options: Options,
+        useViewBoundsAsIntrinsicSize: Boolean,
+    ) : this(source, options, useViewBoundsAsIntrinsicSize, false)
 
     actual override suspend fun decode(): DecodeResult? = runInterruptible {
         val svg = source.source().use { SVG.getFromInputStream(it.inputStream()) }
@@ -68,16 +80,36 @@ actual class SvgDecoder @JvmOverloads actual constructor(
         svg.setDocumentHeight("100%")
 
         val renderOptions = options.css?.let { RenderOptions().css(it) }
+        val image = if (renderToBitmap) {
+            val bitmap = createBitmap(bitmapWidth, bitmapHeight, options.bitmapConfig.toSoftware())
+            svg.renderToCanvas(Canvas(bitmap), renderOptions)
+            bitmap.asImage(shareable = true)
+        } else {
+            SvgImage(svg, renderOptions, bitmapWidth, bitmapHeight)
+        }
 
         DecodeResult(
-            image = SvgImage(svg, renderOptions, bitmapWidth, bitmapHeight),
+            image = image,
             isSampled = true, // SVGs can always be re-decoded at a higher resolution.
         )
     }
 
-    actual class Factory @JvmOverloads actual constructor(
-        val useViewBoundsAsIntrinsicSize: Boolean,
+    actual class Factory(
+        val useViewBoundsAsIntrinsicSize: Boolean = true,
+        val renderToBitmap: Boolean = false,
     ) : Decoder.Factory {
+
+        constructor() : this(
+            useViewBoundsAsIntrinsicSize = true,
+            renderToBitmap = false,
+        )
+
+        actual constructor(
+            useViewBoundsAsIntrinsicSize: Boolean,
+        ) : this(
+            useViewBoundsAsIntrinsicSize = useViewBoundsAsIntrinsicSize,
+            renderToBitmap = false,
+        )
 
         actual override fun create(
             result: SourceFetchResult,
@@ -85,7 +117,12 @@ actual class SvgDecoder @JvmOverloads actual constructor(
             imageLoader: ImageLoader,
         ): Decoder? {
             if (!isApplicable(result)) return null
-            return SvgDecoder(result.source, options, useViewBoundsAsIntrinsicSize)
+            return SvgDecoder(
+                source = result.source,
+                options = options,
+                useViewBoundsAsIntrinsicSize = useViewBoundsAsIntrinsicSize,
+                renderToBitmap = renderToBitmap,
+            )
         }
 
         private fun isApplicable(result: SourceFetchResult): Boolean {
