c301f58e759cff726819f6e3fd5a16dcf1620288
Colin White <colin@colinwhite.me>
2024-05-22 16:11:56 -0700

    Move ScaleDrawable to coil-core.

--- coil-gif/src/main/java/coil3/gif/AnimatedImageDecoder.kt
+++ coil-gif/src/main/java/coil3/gif/AnimatedImageDecoder.kt
@@ -26,6 +26,7 @@ import coil3.request.Options
 import coil3.request.allowRgb565
 import coil3.request.bitmapConfig
 import coil3.request.colorSpace
+import coil3.size.ScaleDrawable
 import coil3.toAndroidUri
 import coil3.util.heightPx
 import coil3.util.isHardware
