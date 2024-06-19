29e750fd4cd655b30a8320555ef35ebf594fa62b
Colin White <colin@colinwhite.me>
2024-06-19 13:08:07 -0400

    Simplify.

--- coil-gif/src/main/java/coil3/gif/AnimatedImageDecoder.kt
+++ coil-gif/src/main/java/coil3/gif/AnimatedImageDecoder.kt
@@ -83,7 +83,7 @@ class AnimatedImageDecoder @JvmOverloads constructor(
                         // Set the target size if the image is larger than the requested dimensions
                         // or the request requires exact dimensions.
                         isSampled = multiplier < 1
-                        if (isSampled || !(options.precision == Precision.INEXACT)) {
+                        if (isSampled || options.precision == Precision.EXACT) {
                             val targetWidth = (multiplier * srcWidth).roundToInt()
                             val targetHeight = (multiplier * srcHeight).roundToInt()
                             setTargetSize(targetWidth, targetHeight)
