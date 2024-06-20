27cf39db0c446e277836d661505ca0edf21a1aee
Colin White <colin@colinwhite.me>
2024-06-20 13:57:04 -0700

    Revert.

--- coil-compose-core/src/commonMain/kotlin/coil3/compose/LocalAsyncImagePreviewHandler.kt
+++ coil-compose-core/src/commonMain/kotlin/coil3/compose/LocalAsyncImagePreviewHandler.kt
@@ -1,16 +1,12 @@
 package coil3.compose
 
 import androidx.compose.runtime.staticCompositionLocalOf
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalInspectionMode
-import coil3.Canvas
 import coil3.Image
 import coil3.ImageLoader
 import coil3.annotation.ExperimentalCoilApi
 import coil3.request.ImageRequest
-import coil3.request.SuccessResult
 import kotlin.jvm.JvmField
 
 @ExperimentalCoilApi
@@ -30,22 +26,8 @@ fun interface AsyncImagePreviewHandler {
     ): AsyncImagePainter.State
 
     companion object {
-        @JvmField val Default = AsyncImagePreviewHandler { _, request, _ ->
-            AsyncImagePainter.State.Success(
-                painter = ColorPainter(Color(0x88888888)),
-                result = SuccessResult(
-                    image = FakeImage,
-                    request = request,
-                ),
-            )
+        @JvmField val Default = AsyncImagePreviewHandler { _, request, toPainter ->
+            AsyncImagePainter.State.Loading(request.placeholder()?.toPainter())
         }
     }
-}
-
-private val FakeImage = object : Image {
-    override val size: Long get() = 0L
-    override val width: Int get() = -1
-    override val height: Int get() = -1
-    override val shareable: Boolean get() = true
-    override fun draw(canvas: Canvas) {}
 }
