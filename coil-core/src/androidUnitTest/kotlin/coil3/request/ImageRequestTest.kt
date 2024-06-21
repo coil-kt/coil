d7c49cdfebd1c4e2c4673ff5dc07bb54362abb78
Colin White <colin@colinwhite.me>
2024-06-21 06:08:52 -0700

    Remove use of Dispatchers.setMain in tests.

--- coil-core/src/androidUnitTest/kotlin/coil3/request/ImageRequestTest.kt
+++ coil-core/src/androidUnitTest/kotlin/coil3/request/ImageRequestTest.kt
@@ -20,31 +20,10 @@ import kotlin.test.assertIs
 import kotlin.test.assertNotEquals
 import kotlin.test.assertSame
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.test.TestDispatcher
-import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
-import kotlinx.coroutines.test.setMain
-import org.junit.After
-import org.junit.Before
 import org.junit.Test
 
-@OptIn(ExperimentalCoroutinesApi::class)
 class ImageRequestTest : RobolectricTest() {
-
-    private lateinit var mainDispatcher: TestDispatcher
-
-    @Before
-    fun before() {
-        mainDispatcher = UnconfinedTestDispatcher()
-        Dispatchers.setMain(mainDispatcher)
-    }
-
-    @After
-    fun after() {
-        Dispatchers.resetMain()
-    }
 
     /** Regression test: https://github.com/coil-kt/coil/issues/221 */
     @Test
