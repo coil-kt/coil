3a40c125ed90dc2881c2c6a756a669f761ff670e
Colin White <colin@colinwhite.me>
2024-07-19 01:09:29 -0400

    Use non-optimized methods for now.

--- coil-network-ktor3/src/jvmCommonMain/kotlin/coil3/network/ktor3/internal/utils.kt
+++ coil-network-ktor3/src/jvmCommonMain/kotlin/coil3/network/ktor3/internal/utils.kt
@@ -1,85 +1,37 @@
 package coil3.network.ktor3.internal
 
 import io.ktor.utils.io.ByteReadChannel
-import io.ktor.utils.io.InternalAPI
-import io.ktor.utils.io.core.read
-import java.io.RandomAccessFile
-import java.nio.ByteBuffer
-import java.nio.channels.SelectableChannel
-import java.nio.channels.WritableByteChannel
+import io.ktor.utils.io.core.readFully
+import io.ktor.utils.io.core.remaining
+import io.ktor.utils.io.readRemaining
 import okio.BufferedSink
 import okio.FileSystem
 import okio.Path
 
+// TODO: Use JVM-optimized implementations from Ktor 2 once
+//  https://youtrack.jetbrains.com/issue/KTOR-7220 is fixed.
+
 internal actual suspend fun ByteReadChannel.writeTo(sink: BufferedSink) {
-    copyTo(sink)
-}
+    val buffer = ByteArray(OKIO_BUFFER_SIZE)
 
-internal actual suspend fun ByteReadChannel.writeTo(fileSystem: FileSystem, path: Path) {
-    if (fileSystem === FileSystem.SYSTEM) {
-        // Fast path: normal jvm File, write to FileChannel directly.
-        RandomAccessFile(path.toFile(), "rw").use {
-            copyTo(it.channel)
-        }
-    } else {
-        // Slow path: cannot guarantee a "real" file.
-        fileSystem.write(path) {
-            copyTo(this)
-        }
-    }
-}
+    while (!isClosedForRead) {
+        val packet = readRemaining(buffer.size.toLong())
+        if (packet.exhausted()) break
 
-/**
- * Copied from https://github.com/ktorio/ktor/blob/main/ktor-io/jvm/src/io/ktor/utils/io/ByteReadChannelOperations.jvm.kt
- * to work around https://youtrack.jetbrains.com/issue/KTOR-7220.
- */
-@OptIn(InternalAPI::class)
-private suspend fun ByteReadChannel.copyTo(
-    channel: WritableByteChannel,
-    limit: Long = Long.MAX_VALUE,
-): Long {
-    require(limit >= 0L) { "Limit shouldn't be negative: $limit" }
-    if (channel is SelectableChannel && !channel.isBlocking) {
-        throw IllegalArgumentException("Non-blocking channels are not supported")
-    }
-
-    if (isClosedForRead) {
-        closedCause?.let { throw it }
-        return 0
-    }
-
-    var copied = 0L
-    val copy = { bb: ByteBuffer ->
-        val rem = limit - copied
-
-        if (rem < bb.remaining()) {
-            val l = bb.limit()
-            bb.limit(bb.position() + rem.toInt())
-
-            while (bb.hasRemaining()) {
-                channel.write(bb)
-            }
-
-            bb.limit(l)
-            copied += rem
-        } else {
-            var written = 0L
-            while (bb.hasRemaining()) {
-                written += channel.write(bb)
-            }
-
-            copied += written
-        }
-    }
-
-    while (copied < limit) {
-        if (isClosedForRead || !awaitContent()) break
-        if (readBuffer.request(1)) {
-            readBuffer.read(copy)
-        }
+        // TODO: Figure out how to remove 'buffer' and read directly into 'sink'.
+        val bytesRead = packet.remaining.toInt()
+        packet.readFully(buffer, 0, bytesRead)
+        sink.write(buffer, 0, bytesRead)
     }
 
     closedCause?.let { throw it }
-
-    return copied
 }
+
+internal actual suspend fun ByteReadChannel.writeTo(fileSystem: FileSystem, path: Path) {
+    fileSystem.write(path) {
+        writeTo(this)
+    }
+}
+
+// Okio uses 8 KB internally.
+private const val OKIO_BUFFER_SIZE = 8 * 1024
