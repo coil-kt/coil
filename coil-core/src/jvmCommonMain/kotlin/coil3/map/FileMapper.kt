7df13f6981a3283ab7d7fea1f720fff0ae0e5364
Colin White <colin@colinwhite.me>
2024-07-29 23:58:05 -0700

    Refactor FileMapper.

--- coil-core/src/jvmCommonMain/kotlin/coil3/map/FileMapper.kt
+++ coil-core/src/jvmCommonMain/kotlin/coil3/map/FileMapper.kt
@@ -3,11 +3,10 @@ package coil3.map
 import coil3.Uri
 import coil3.request.Options
 import coil3.toUri
-import coil3.util.SCHEME_FILE
 import java.io.File
 
 class FileMapper : Mapper<File, Uri> {
     override fun map(data: File, options: Options): Uri {
-        return "$SCHEME_FILE://$data".toUri()
+        return data.path.toUri()
     }
 }
