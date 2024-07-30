d8dcf5671d1a05b517ea8ea232639c5f48d3f528
Colin White <colin@colinwhite.me>
2024-07-30 00:14:38 -0700

    Use filePath in keyer.

--- coil-core/src/commonMain/kotlin/coil3/key/FileUriKeyer.kt
+++ coil-core/src/commonMain/kotlin/coil3/key/FileUriKeyer.kt
@@ -1,6 +1,7 @@
 package coil3.key
 
 import coil3.Uri
+import coil3.filePath
 import coil3.request.Options
 import coil3.util.isFileUri
 import okio.Path.Companion.toPath
@@ -11,12 +12,14 @@ class FileUriKeyer(
 
     override fun key(data: Uri, options: Options): String? {
         if (addLastModifiedToFileCacheKey && isFileUri(data)) {
-            val path = data.path
+            val path = data.filePath
             if (path != null) {
                 val timestamp = options.fileSystem.metadata(path.toPath()).lastModifiedAtMillis
                 return "$data-$timestamp"
             }
         }
+
+        // Fall back to the standard UriKeyer.
         return null
     }
 }
