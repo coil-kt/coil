817566c580833ec0810be8361d927852c762fd82
Colin White <colin@colinwhite.me>
2024-09-28 14:46:45 -0500

    Add scaleToDensity to SvgDecoder. (#2502)
    * Add scaleToDensity to SvgDecoder.
    
    * Add test.
    
    * Fix test on CI.

--- coil-svg/src/androidInstrumentedTest/kotlin/coil3/svg/SvgDecoderTest.android.kt
+++ coil-svg/src/androidInstrumentedTest/kotlin/coil3/svg/SvgDecoderTest.android.kt
@@ -1,5 +1,34 @@
 package coil3.svg
 
+import coil3.ImageLoader
+import coil3.request.Options
+import coil3.size.Size
+import coil3.svg.internal.density
 import coil3.test.utils.AbstractSvgDecoderTest
+import coil3.test.utils.context
+import kotlin.math.roundToInt
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlinx.coroutines.test.runTest
+import okio.FileSystem
+import okio.Path.Companion.toPath
+import okio.buffer
 
-class SvgDecoderTestAndroid : AbstractSvgDecoderTest(SvgDecoder.Factory())
+class SvgDecoderTestAndroid : AbstractSvgDecoderTest(SvgDecoder.Factory()) {
+
+    @Test
+    fun scaleToDensity_true() = runTest {
+        val source = FileSystem.RESOURCES.source("coil_logo.svg".toPath()).buffer()
+            .asSourceResult(mimeType = "image/svg+xml")
+        val result = SvgDecoder.Factory(scaleToDensity = true).create(
+            result = source,
+            options = Options(context, size = Size.ORIGINAL),
+            imageLoader = ImageLoader(context),
+        )?.decode()
+        assertNotNull(result)
+        val size = (context.density * 140).roundToInt()
+        assertEquals(size, result.image.width)
+        assertEquals(size, result.image.height)
+    }
+}
