b2402b05f8e9f5fc3be76da284f87be7b7db8b3e
Colin White <colin@colinwhite.me>
2024-10-01 16:08:15 -0700

    Make ConstraintsSizeResolver public.

--- coil-compose-core/src/commonMain/kotlin/coil3/compose/AsyncImage.kt
+++ coil-compose-core/src/commonMain/kotlin/coil3/compose/AsyncImage.kt
@@ -16,7 +16,6 @@ import coil3.ImageLoader
 import coil3.compose.AsyncImagePainter.Companion.DefaultTransform
 import coil3.compose.AsyncImagePainter.State
 import coil3.compose.internal.AsyncImageState
-import coil3.compose.internal.ConstraintsSizeResolver
 import coil3.compose.internal.ContentPainterElement
 import coil3.compose.internal.contentDescription
 import coil3.compose.internal.onStateOf
