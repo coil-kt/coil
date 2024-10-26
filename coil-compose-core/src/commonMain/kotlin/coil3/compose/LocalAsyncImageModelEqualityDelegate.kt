cab1e5edd9c63551eb9122a0bf33b18e8f0af666
Colin White <colin@colinwhite.me>
2024-10-26 16:29:28 -0700

    Add AllProperties model equality delegate.

--- coil-compose-core/src/commonMain/kotlin/coil3/compose/LocalAsyncImageModelEqualityDelegate.kt
+++ coil-compose-core/src/commonMain/kotlin/coil3/compose/LocalAsyncImageModelEqualityDelegate.kt
@@ -1,5 +1,6 @@
 package coil3.compose
 
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.staticCompositionLocalOf
 import coil3.annotation.ExperimentalCoilApi
@@ -16,7 +17,7 @@ val LocalAsyncImageModelEqualityDelegate = staticCompositionLocalOf {
  * Determines equality between two models.
  *
  * This allows you to control when [rememberAsyncImagePainter], [AsyncImage], and
- * [SubcomposeAsyncImage] execute a new request and recompose due to a `model` change.
+ * [SubcomposeAsyncImage] execute a new request and recompose due to a model change.
  */
 @ExperimentalCoilApi
 @Stable
@@ -25,7 +26,11 @@ interface AsyncImageModelEqualityDelegate {
     fun hashCode(self: Any?): Int
 
     companion object {
-        @JvmField val Default: AsyncImageModelEqualityDelegate = object : AsyncImageModelEqualityDelegate {
+        /**
+         * The default model equality delegate that only compares a subset of [ImageRequest]'s
+         * properties instead of delegating to its own [equals] and [hashCode] implementations.
+         */
+        @JvmField val Default = object : AsyncImageModelEqualityDelegate {
 
             override fun equals(self: Any?, other: Any?): Boolean {
                 if (this === other) return true
@@ -53,6 +58,22 @@ interface AsyncImageModelEqualityDelegate {
                 result = 31 * result + self.precision.hashCode()
                 return result
             }
+
+            override fun toString(): String {
+                return "AsyncImageModelEqualityDelegate.Default"
+            }
+        }
+
+        /**
+         * A model equality delegate that always delegates to the model's [equals] and [hashCode].
+         * If the model is an [ImageRequest] this will compare all of its properties and this may
+         * cause recompositions than necessary. Consider [remember]ing your [ImageRequest]s if you
+         * use this model equality delegate.
+         */
+        @JvmField val AllProperties = object : AsyncImageModelEqualityDelegate {
+            override fun equals(self: Any?, other: Any?) = self == other
+            override fun hashCode(self: Any?) = self.hashCode()
+            override fun toString() = "AsyncImageModelEqualityDelegate.AllProperties"
         }
     }
 }
