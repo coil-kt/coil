3c6521fe9dc886dde63c7f87c58ec23c68410c89
Colin White <colin@colinwhite.me>
2024-11-18 18:00:35 -0800

    Revert.

--- coil-test/src/commonMain/kotlin/coil3/test/FakeImage.kt
+++ coil-test/src/commonMain/kotlin/coil3/test/FakeImage.kt
@@ -8,27 +8,17 @@ import coil3.test.internal.Black
  * A simple [Image] that draws a 100x100 black square by default.
  */
 expect class FakeImage(
-    color: Int = Black,
     width: Int = 100,
     height: Int = 100,
     size: Long = 4L * width * height,
     shareable: Boolean = true,
+    color: Int = Black,
 ) : Image {
-
-    @Deprecated("Kept for binary compatibility.", level = DeprecationLevel.HIDDEN)
-    constructor(
-        width: Int,
-        height: Int,
-        size: Long,
-        shareable: Boolean,
-        color: Int = Black,
-    )
-
-    val color: Int
     override val width: Int
     override val height: Int
     override val size: Long
     override val shareable: Boolean
+    val color: Int
 
     override fun draw(canvas: Canvas)
 }
