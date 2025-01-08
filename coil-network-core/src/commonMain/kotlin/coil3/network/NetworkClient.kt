bf120f3e853956b0834b199761f3e0227c4c55cb
HeroBrine1st Erquilenne <pankov.76.tvink@gmail.com>
2025-01-08 20:46:47 +0300

    Pass Extras to NetworkClient

--- coil-network-core/src/commonMain/kotlin/coil3/network/NetworkClient.kt
+++ coil-network-core/src/commonMain/kotlin/coil3/network/NetworkClient.kt
@@ -1,5 +1,6 @@
 package coil3.network
 
+import coil3.Extras
 import coil3.annotation.Poko
 import coil3.network.internal.HTTP_METHOD_GET
 import coil3.network.internal.HTTP_RESPONSE_OK
@@ -34,7 +35,31 @@ class NetworkRequest(
     val method: String = HTTP_METHOD_GET,
     val headers: NetworkHeaders = NetworkHeaders.EMPTY,
     val body: NetworkRequestBody? = null,
+    val extras: Extras = Extras.EMPTY,
 ) {
+    fun copy(
+        url: String = this.url,
+        method: String = this.method,
+        headers: NetworkHeaders = this.headers,
+        body: NetworkRequestBody? = this.body,
+        extras: Extras = this.extras,
+    ) = NetworkRequest(
+        url = url,
+        method = method,
+        headers = headers,
+        body = body,
+        extras = extras,
+    )
+
+    @Deprecated("Kept for binary compatibility.", level = DeprecationLevel.HIDDEN)
+    constructor(
+        url: String,
+        method: String = HTTP_METHOD_GET,
+        headers: NetworkHeaders = NetworkHeaders.EMPTY,
+        body: NetworkRequestBody? = null,
+    ) : this(url, method, headers, body)
+
+    @Deprecated("Kept for binary compatibility.", level = DeprecationLevel.HIDDEN)
     fun copy(
         url: String = this.url,
         method: String = this.method,
@@ -45,6 +70,7 @@ class NetworkRequest(
         method = method,
         headers = headers,
         body = body,
+        extras = this.extras,
     )
 }
 
