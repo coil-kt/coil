fec6b4899b143e14f972b7409ece6c4f5a3c7bc7
Colin White <colin@colinwhite.me>
2025-01-14 21:36:33 -0800

    Fix docs.

--- coil-test/src/commonMain/kotlin/coil3/test/FakeImageLoaderEngine.kt
+++ coil-test/src/commonMain/kotlin/coil3/test/FakeImageLoaderEngine.kt
@@ -29,9 +29,9 @@ fun FakeImageLoaderEngine(image: Image): FakeImageLoaderEngine {
  *
  * ```
  * val engine = FakeImageLoaderEngine.Builder()
- *     .intercept("https://www.example.com/image.jpg", ColorImage(Color.Red))
- *     .intercept({ it is String && it.endsWith("test.png") }, ColorImage(Color.Green))
- *     .default(ColorImage(Color.Blue))
+ *     .intercept("https://www.example.com/image.jpg", ColorImage(Color.Red.toArgb()))
+ *     .intercept({ it is String && it.endsWith("test.png") }, ColorImage(Color.Green.toArgb()))
+ *     .default(ColorImage(Color.Blue.toArgb()))
  *     .build()
  * val imageLoader = ImageLoader.Builder(context)
  *     .components { add(engine) }
