4d7f136cffbf0ad2bb5f5f5a29a938e3604ba924
Colin White <colin@colinwhite.me>
2025-03-27 00:59:31 -0500

    Use kotlinx.io's new Okio interop module.

--- coil-network-ktor3/src/nonJvmCommonMain/kotlin/coil3/network/ktor3/internal/utils.nonJvmCommon.kt
+++ coil-network-ktor3/src/nonJvmCommonMain/kotlin/coil3/network/ktor3/internal/utils.nonJvmCommon.kt
@@ -1,27 +1,15 @@
 package coil3.network.ktor3.internal
 
 import io.ktor.utils.io.ByteReadChannel
-import io.ktor.utils.io.core.readFully
-import io.ktor.utils.io.core.remaining
-import io.ktor.utils.io.readRemaining
+import io.ktor.utils.io.asByteWriteChannel
+import io.ktor.utils.io.copyAndClose
+import kotlinx.io.okio.asKotlinxIoRawSink
 import okio.BufferedSink
 import okio.FileSystem
 import okio.Path
 
 internal actual suspend fun ByteReadChannel.writeTo(sink: BufferedSink) {
-    val buffer = ByteArray(OKIO_BUFFER_SIZE)
-
-    while (!isClosedForRead) {
-        val packet = readRemaining(buffer.size.toLong())
-        if (packet.exhausted()) break
-
-        // TODO: Figure out how to remove 'buffer' and read directly into 'sink'.
-        val bytesRead = packet.remaining.toInt()
-        packet.readFully(buffer, 0, bytesRead)
-        sink.write(buffer, 0, bytesRead)
-    }
-
-    closedCause?.let { throw it }
+    copyAndClose(sink.asKotlinxIoRawSink().asByteWriteChannel())
 }
 
 internal actual suspend fun ByteReadChannel.writeTo(fileSystem: FileSystem, path: Path) {
@@ -29,6 +17,3 @@ internal actual suspend fun ByteReadChannel.writeTo(fileSystem: FileSystem, path
         writeTo(this)
     }
 }
-
-// Okio uses 8 KB internally.
-private const val OKIO_BUFFER_SIZE = 8 * 1024
