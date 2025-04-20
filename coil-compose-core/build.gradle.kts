025e228e835123bbd2d9007579b6060b268ca0bc
Colin White <colin@colinwhite.me>
2025-04-19 17:51:26 -0700

    Enable Java 11 bytecode for Compose artifacts.

--- coil-compose-core/build.gradle.kts
+++ coil-compose-core/build.gradle.kts
@@ -1,5 +1,8 @@
 import coil3.addAllMultiplatformTargets
 import coil3.androidLibrary
+import org.gradle.kotlin.dsl.withType
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 plugins {
     id("com.android.library")
@@ -65,4 +68,13 @@ baselineProfile {
 
 dependencies {
     baselineProfile(projects.internal.benchmark)
+}
+
+// Compose 1.8.0 requires JVM 11.
+tasks.withType<JavaCompile>().configureEach {
+    sourceCompatibility = JavaVersion.VERSION_11.toString()
+    targetCompatibility = JavaVersion.VERSION_11.toString()
+}
+tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions.jvmTarget = JvmTarget.JVM_11
 }
