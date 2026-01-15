08c483fb9e389aeac04c3e2fd7a76452e85ce15c
Colin White <colin@colinwhite.me>
2026-01-15 17:38:29 -0500

    Inline PlatformContext.applicationImageLoaderFactory. (#3289)

--- coil/src/nonAndroidMain/kotlin/coil3/SingletonImageLoader.nonAndroid.kt
+++ coil/src/nonAndroidMain/kotlin/coil3/SingletonImageLoader.nonAndroid.kt
@@ -1,10 +1,3 @@
 package coil3
 
-internal actual fun PlatformContext.applicationImageLoaderFactory(): SingletonImageLoader.Factory? {
-    return null
-}
-
-/**
- * @return the PlatformContext as is, for non-Android platform.
- */
 internal actual fun PlatformContext.applicationContext(): PlatformContext = this
