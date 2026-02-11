71f064a6623f5b5f85c0dc65f057aae839b4ca34
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2026-02-11 14:35:45 +0100

    Configure web tests to use headless browser with the karma instead of the nodejs
    Compose for Web is not designed to be used in a node environment. Tests used to work because there were no explicit skiko references.
    New changes import "org.jetbrains.skiko.wasm.awaitSkiko" function. The function is declared in "js-reexport-symbols.mjs" module, which is not possible to use in nodejs.

--- buildSrc/src/main/kotlin/coil3/multiplatform.kt
+++ buildSrc/src/main/kotlin/coil3/multiplatform.kt
@@ -33,11 +33,12 @@ fun Project.addAllMultiplatformTargets(
             js {
                 browser {
                     testTask {
-                        enabled = false
+                        useKarma { useChromeHeadless() }
                     }
                 }
                 nodejs {
                     testTask {
+                        enabled = false
                         useMocha {
                             timeout = "60s"
                         }
@@ -57,12 +58,12 @@ fun Project.addAllMultiplatformTargets(
 
                     browser {
                         testTask {
-                            enabled = false
+                            useKarma { useChromeHeadless() }
                         }
                     }
                     nodejs {
                         testTask {
-                            enabled = true
+                            enabled = false
                             doFirst {
                                 val skikoModule = skikoDirProvider.get().resolve("skiko.mjs")
                                 if (skikoModule.isFile) {
