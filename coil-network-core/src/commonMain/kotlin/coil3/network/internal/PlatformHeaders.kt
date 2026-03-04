c7e8847f08fab19c4c239fa3258f8c15dde73dc3
Kamil Bąk <kamil.bak@footballco.com>
2026-03-04 15:41:56 +0100

    Add accept header based on android capabilities for image decoding

--- coil-network-core/src/commonMain/kotlin/coil3/network/internal/PlatformHeaders.kt
+++ coil-network-core/src/commonMain/kotlin/coil3/network/internal/PlatformHeaders.kt
@@ -2,4 +2,4 @@ package coil3.network.internal
 
 import coil3.network.NetworkHeaders
 
-internal fun getPlatformHeaders(): NetworkHeaders = NetworkHeaders.EMPTY
+internal expect fun getPlatformHeaders(): NetworkHeaders
