9717f2b8dc70d6d62e557b0d3dec343bb82f90fe
gajgaje2000 <gajgaje2000@gmail.com>
2026-06-20 14:30:10 +0530

    fix: added 8GB fallback for  on JVM targets

--- coil-core/src/jvmMain/kotlin/coil3/util/contexts.jvm.kt
+++ coil-core/src/jvmMain/kotlin/coil3/util/contexts.jvm.kt
@@ -3,5 +3,10 @@ package coil3.util
 import coil3.PlatformContext
 
 internal actual fun PlatformContext.totalAvailableMemoryBytes(): Long {
-    return Runtime.getRuntime().maxMemory()
+    val maxMemory = Runtime.getRuntime().maxMemory()
+    return if (maxMemory == Long.MAX_VALUE) {
+        8L * 1024 * 1024 * 1024 // 8 GB
+    } else {
+        maxMemory
+    }
 }
