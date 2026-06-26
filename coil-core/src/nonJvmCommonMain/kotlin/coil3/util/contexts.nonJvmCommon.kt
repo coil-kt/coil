97db52d4ad1ab82c0fd0197a0a625685ba715959
gajgaje2000 <gajgaje2000@gmail.com>
2026-06-26 17:27:48 +0530

    fix: Query physical memory on Apple targets for cache sizing

--- coil-core/src/nonJvmCommonMain/kotlin/coil3/util/contexts.nonJvmCommon.kt
+++
@@ -1,8 +0,0 @@
-package coil3.util
-
-import coil3.PlatformContext
-
-internal actual fun PlatformContext.totalAvailableMemoryBytes(): Long {
-    // TODO: Figure out how to compute the total available memory on non-JVM platforms.
-    return 512L * 1024L * 1024L // 512 MB
-}
