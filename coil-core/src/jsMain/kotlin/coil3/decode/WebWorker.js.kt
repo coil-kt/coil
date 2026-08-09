e1b696ebfa4ede84f5026a16d92336f28590932c
Colin White <colin@colinwhite.me>
2026-08-09 16:19:41 -0400

    Avoid internal Skiko APIs on web

--- coil-core/src/jsMain/kotlin/coil3/decode/WebWorker.js.kt
+++ coil-core/src/jsMain/kotlin/coil3/decode/WebWorker.js.kt
@@ -1,8 +1,22 @@
+@file:OptIn(ExperimentalWasmJsInterop::class)
+
 package coil3.decode
 
+import kotlin.js.ExperimentalWasmJsInterop
+import kotlin.js.JsAny
+import kotlin.js.Promise
 import kotlinx.coroutines.await
 
-@OptIn(ExperimentalWasmJsInterop::class)
-@Suppress("INVISIBLE_REFERENCE")
-internal actual suspend fun awaitSkiko(): JsAny =
-    org.jetbrains.skiko.wasm.awaitSkiko.await()
+@JsModule("./js-reexport-symbols.mjs")
+@JsNonModule
+private external val skikoModule: SkikoModule
+
+private external interface SkikoModule {
+    val api: SkikoApi
+}
+
+private external interface SkikoApi {
+    val awaitSkiko: Promise<JsAny>
+}
+
+internal actual suspend fun awaitSkiko(): JsAny = skikoModule.api.awaitSkiko.await()
