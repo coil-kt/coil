95589d2208361e2864ea96c0d631de9e3763c23f
Colin White <colin@colinwhite.me>
2026-08-30 10:39:08 +0700

    Rename method.

--- coil-gif/src/androidMain/kotlin/coil3/gif/AnimatedImageDecoder.android.kt
+++ coil-gif/src/androidMain/kotlin/coil3/gif/AnimatedImageDecoder.android.kt
@@ -1,8 +1,11 @@
+@file:JvmName("AnimatedImageDecoderFactory")
+
 package coil3.gif
 
 import android.os.Build.VERSION.SDK_INT
 import coil3.decode.Decoder
 
+@JvmName("create")
 actual fun AnimatedImageDecoderFactory(): Decoder.Factory =
     if (SDK_INT >= 28) {
         AnimatedImageDecoder.Factory()
--- coil-gif/src/nonAndroidMain/kotlin/coil3/gif/AnimatedImageDecoder.nonAndroid.kt
+++ coil-gif/src/nonAndroidMain/kotlin/coil3/gif/AnimatedImageDecoder.nonAndroid.kt
@@ -1,6 +1,10 @@
+@file:JvmName("AnimatedImageDecoderFactory")
+
 package coil3.gif
 
 import coil3.decode.Decoder
+import kotlin.jvm.JvmName
 
+@JvmName("create")
 actual fun AnimatedImageDecoderFactory(): Decoder.Factory =
     AnimatedSkiaImageDecoder.Factory()
