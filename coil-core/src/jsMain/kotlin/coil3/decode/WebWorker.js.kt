4b14a54836ec4ed2097d3cc8100a7ae8c86fe6d2
Konstantin <konstantin.tskhovrebov@jetbrains.com>
2026-09-03 10:00:29 +0200

    Use InternalSkikoApi awaitSkiko directly in web worker decoding (#3568)
    To avoid wasm linkage error with CMP 1.13

--- coil-core/src/jsMain/kotlin/coil3/decode/WebWorker.js.kt
+++
@@ -1,9 +0,0 @@
-@file:OptIn(ExperimentalWasmJsInterop::class)
-
-package coil3.decode
-
-import kotlin.js.ExperimentalWasmJsInterop
-import kotlin.js.JsAny
-import kotlinx.coroutines.await
-
-internal actual suspend fun awaitSkiko(): JsAny = awaitSkiko.await()
